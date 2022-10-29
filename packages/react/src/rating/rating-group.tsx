1f554e65714098b5b4645656557ea642c46a613d
Tim Kolberger <tim@kolberger.eu>
2022-10-29 06:47:36 +0400

    chore(react): rename props

--- packages/react/src/rating/rating-group.tsx
+++ packages/react/src/rating/rating-group.tsx
@@ -3,12 +3,12 @@ import { forwardRef } from '../forwardRef'
 import { RatingContext, useRatingContext } from './rating-context'
 import { ReactNode } from 'react'
 
-export type RatingItemGroupProps = Omit<HTMLAtlasProps<'div'>, 'children'> & {
+export type RatingGroupProps = Omit<HTMLAtlasProps<'div'>, 'children'> & {
   children: ReactNode | ((context: RatingContext) => ReactNode)
   renderIcon?: never
 }
 
-export const RatingGroup = forwardRef<'div', RatingItemGroupProps>((props, ref) => {
+export const RatingGroup = forwardRef<'div', RatingGroupProps>((props, ref) => {
   const { children, ...htmlProps } = props
   const api = useRatingContext()
   const renderPropResult = typeof children === 'function' ? children(api) : children
