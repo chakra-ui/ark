131fd30a6c803d3be7ab06204f9a409076ccf627
Tim Kolberger <tim@kolberger.eu>
2022-10-29 07:11:10 +0400

    chore(react): explicit exports for pressable

--- packages/react/src/pressable/index.ts
+++ packages/react/src/pressable/index.ts
@@ -1,2 +1,4 @@
-export * from './pressable'
-export * from './use-pressable'
+export { Pressable } from './pressable'
+export type { PressableProps } from './pressable'
+export { usePressable } from './use-pressable'
+export type { UsePressableProps } from './use-pressable'
