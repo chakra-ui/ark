05fb08cbf3ec4bbde1804516348014ff10011c91
Tim Kolberger <tim@kolberger.eu>
2022-10-30 19:21:01 +0100

    feat(react): handle multiple groups in split props

--- packages/react/src/splitProps.ts
+++
@@ -1,9 +0,0 @@
-export const splitProps = <P extends object, K extends keyof P>(props: P, keys: Array<K>) =>
-  keys.reduce<[Pick<P, K>, Omit<P, K>]>(
-    (prev, key) => {
-      const [target, source] = prev
-      const { [key]: value, ...rest } = source
-      return [{ ...target, [key]: value }, rest as Omit<P, K>]
-    },
-    [{} as Pick<P, K>, props],
-  )
