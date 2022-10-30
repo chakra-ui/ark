04eebf428863fef49f2f678f598f4006d6b7ba99
Christian Schröter <dev@cschroeter.net>
2022-10-30 21:18:03 +0100

    fix: fix typecheck

--- packages/react/src/accordion/accordion.tsx
+++ packages/react/src/accordion/accordion.tsx
@@ -2,7 +2,7 @@ import { atlas, HTMLAtlasProps } from '../factory'
 import { forwardRef } from '../forwardRef'
 import { AccordionProvider } from './accordion-context'
 import { useAccordion, UseAccordionProps } from './use-accordion'
-import { Assign } from '../split-props'
+import type { Assign } from '../split-props'
 
 export type AccordionProps = Assign<HTMLAtlasProps<'div'>, UseAccordionProps>
 
