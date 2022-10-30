c09d7bb5de87a97c95aad6f482d842f0eefbf3bd
Tim Kolberger <tim@kolberger.eu>
2022-10-30 22:57:04 +0100

    feat(react): filter undefined menu props

--- packages/react/src/menu/use-menu.ts
+++ packages/react/src/menu/use-menu.ts
@@ -1,9 +1,10 @@
 import * as menu from '@zag-js/menu'
 import { normalizeProps, useMachine } from '@zag-js/react'
 import { useId } from 'react'
-import { splitProps } from '../split-props'
+import { filterUndefinedEntries } from '../filter-undefined-entries'
+import { OptionalId, splitProps } from '../split-props'
 
-export type UseMenuProps = Omit<menu.Context, 'id'>
+export type UseMenuProps = OptionalId<menu.Context> & { isOpen?: boolean }
 
 export const useMenu = <Props extends UseMenuProps>(props: Props) => {
   const [menuProps, htmlProps] = splitProps(props, [
@@ -11,20 +12,22 @@ export const useMenu = <Props extends UseMenuProps>(props: Props) => {
     'anchorPoint',
     'aria-label',
     'closeOnSelect',
+    'dir',
+    'getRootNode',
+    'id',
     'ids',
     'loop',
     'onSelect',
     'onValueChange',
     'positioning',
-    'value',
   ])
 
-  const [state, send, machine] = useMachine(
-    menu.machine({
-      id: useId(),
-      ...menuProps,
-    }),
-  )
+  const initialContext = filterUndefinedEntries({
+    id: useId(),
+    ...menuProps,
+  })
+
+  const [state, send, machine] = useMachine(menu.machine(initialContext))
 
   const api = menu.connect(state, send, normalizeProps)
 
