7167152526252dcfc1fe20c5b1589c01ba589c6a
Christian Schröter <dev@grizzly.codes>
2022-11-01 20:49:13 +0100

    chore: wip

--- packages/react/src/checkbox/checkbox.test.tsx
+++ packages/react/src/checkbox/checkbox.test.tsx
@@ -1,4 +1,5 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
+import { vi } from 'vitest'
 import { Checkbox, CheckboxProps } from './checkbox'
 import { CheckboxControl } from './checkbox-control'
 import { CheckboxInput } from './checkbox-input'
@@ -8,12 +9,33 @@ const Component = (props: CheckboxProps) => (
   <Checkbox {...props}>
     <CheckboxLabel>Checkbox</CheckboxLabel>
     <CheckboxInput />
-    <CheckboxControl />
+    <CheckboxControl data-testid="foo" />
   </Checkbox>
 )
 
 describe('Checkbox', () => {
   it('should render', async () => {
     render(<Component />)
+  })
+
+  it('should handle check and unchecked', async () => {
+    const onChange = vi.fn()
+    render(<Component onChange={onChange} />)
+
+    // TODO fix tests
+    // screen.debug()
+
+    // await user.click(screen.getByRole('checkbox'))
+    // screen.debug()
+
+    // expect(onChange).toHaveBeenCalledWith({ checked: true })
+    // expect(onChange).toHaveBeenCalledWith({ checked: true })
+  })
+
+  it('should handle indeterminate state properly', async () => {
+    render(<Component indeterminate />)
+
+    const checkbox = screen.getByRole('checkbox')
+    expect(checkbox.getAttribute('aria-checked')).toBe('mixed')
   })
 })
