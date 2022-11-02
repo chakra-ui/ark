43327a15838a8d8e92e2b0f7200c0db8e969c207
Tim Kolberger <tim@kolberger.eu>
2022-11-02 22:47:04 +0100

    test: failing test for pressable

--- packages/react/src/pressable/pressable.test.tsx
+++ packages/react/src/pressable/pressable.test.tsx
@@ -1,9 +1,22 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
+import user from '@testing-library/user-event'
+import { expect, vi } from 'vitest'
 import { Pressable } from './pressable'
 
 describe('Pressable', () => {
   it('should have the correct aria role', () => {
     render(<Pressable />)
     screen.getByRole('button')
+  })
+
+  it('should call onPress', async () => {
+    const onPress = vi.fn()
+    render(<Pressable onPress={onPress} />)
+
+    const button = screen.getByRole('button')
+    await user.click(button)
+
+    await waitFor(() => expect(onPress).toHaveBeenCalledOnce())
+    // => what is happening here? user.click should trigger all pointer events ?!
   })
 })
