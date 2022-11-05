3991bfc47b63a298c323df0b6da6a00bf510d0ed
Christian Schröter <dev@grizzly.codes>
2022-11-06 00:04:05 +0100

    chore: simplify prettier setup

--- .prettierrc-ci.js
+++
@@ -1,8 +0,0 @@
-/**
- * This additional config file is used in CI related checks,
- * to skip the annoying auto cleanup of imports while saving files in the IDE
- */
-module.exports = {
-  ...require('./.prettierrc.js'),
-  plugins: ['prettier-plugin-organize-imports'],
-}
--- .prettierrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  printWidth: 100,
-  singleQuote: true,
-  trailingComma: 'all',
-  arrowParens: 'always',
-  semi: false,
-  pluginSearchDirs: ['./node_modules'],
-}
