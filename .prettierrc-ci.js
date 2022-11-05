6eb09d6952b111b84a4450219cc88f95705253f3
Tim Kolberger <tim@kolberger.eu>
2022-11-05 23:33:14 +0100

    docs: add "why" comment to prettier-ci file

--- .prettierrc-ci.js
+++ .prettierrc-ci.js
@@ -1,3 +1,7 @@
+/**
+ * This additional config file is used in CI related checks,
+ * to skip the annoying auto cleanup of imports while saving files in the IDE
+ */
 module.exports = {
   ...require('./.prettierrc.js'),
   plugins: ['prettier-plugin-organize-imports'],
