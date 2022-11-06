6c2b70d033b065d9b001201414699a88b4bdaa29
Christian Schröter <dev@grizzly.codes>
2022-11-06 21:27:12 +0100

    chore: wip

--- packages/react/src/slider/slider.test.tsx
+++ packages/react/src/slider/slider.test.tsx
@@ -1,4 +1,5 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
+import user from '@testing-library/user-event'
 import { JSDOM } from 'jsdom'
 import { Slider, type SliderProps } from './slider'
 import { SliderControl } from './slider-control'
@@ -7,14 +8,20 @@ import { SliderRange } from './slider-range'
 import { SliderThumb } from './slider-thumb'
 import { SliderTrack } from './slider-track'
 
-const Component = (props: SliderProps) => (
-  <Slider {...props} getRootNode={() => new JSDOM().window.document}>
+const ComponentUnderTest = (props: SliderProps) => (
+  <Slider
+    min={-50}
+    max={50}
+    defaultValue={0}
+    getRootNode={() => new JSDOM().window.document}
+    {...props}
+  >
     <SliderControl>
       <SliderTrack>
         <SliderRange />
       </SliderTrack>
       <SliderThumb>
-        <SliderInput />∏
+        <SliderInput />
       </SliderThumb>
     </SliderControl>
   </Slider>
@@ -22,6 +29,64 @@ const Component = (props: SliderProps) => (
 
 describe('Slider', () => {
   it('should render!', async () => {
-    render(<Component />)
+    render(<ComponentUnderTest />)
+  })
+
+  it('should move the thumb correctly when orientated horizontal', async () => {
+    render(<ComponentUnderTest />)
+
+    const thumb = screen.getByRole('slider', { hidden: true })
+
+    thumb.focus()
+
+    await user.keyboard('[ArrowRight]')
+    expect(thumb).toHaveAttribute('aria-valuenow', '1')
+
+    await user.keyboard('[ArrowRight]')
+    expect(thumb).toHaveAttribute('aria-valuenow', '2')
+
+    await user.keyboard('[Home]')
+    expect(thumb).toHaveAttribute('aria-valuenow', '-50')
+
+    await user.keyboard('[End]')
+    expect(thumb).toHaveAttribute('aria-valuenow', '50')
+  })
+
+  it('should move the thumb correctly when orientated vertical', async () => {
+    render(<ComponentUnderTest orientation="vertical" />)
+
+    const thumb = screen.getByRole('slider', { hidden: true })
+    thumb.focus()
+
+    await user.keyboard('[ArrowUp]')
+    expect(thumb).toHaveAttribute('aria-valuenow', '1')
+
+    await user.keyboard('[ArrowDown]')
+    expect(thumb).toHaveAttribute('aria-valuenow', '0')
+
+    await user.keyboard('[Home]')
+    expect(thumb).toHaveAttribute('aria-valuenow', '-50')
+
+    await user.keyboard('[End]')
+    expect(thumb).toHaveAttribute('aria-valuenow', '50')
+  })
+
+  it('should move the thumb correctly under rtl ', async () => {
+    render(<ComponentUnderTest dir="rtl" />)
+
+    const thumb = screen.getByRole('slider', { hidden: true })
+    thumb.focus()
+
+    await user.keyboard('[ArrowRight]')
+    expect(thumb).toHaveAttribute('aria-valuenow', '-1')
+
+    await user.keyboard('[ArrowRight]')
+    expect(thumb).toHaveAttribute('aria-valuenow', '-2')
+
+    await user.keyboard('[Home]')
+    expect(thumb).toHaveAttribute('aria-valuenow', '-50')
+
+    await user.keyboard('[End]')
+    expect(thumb).toHaveAttribute('aria-valuenow', '50')
   })
 })
