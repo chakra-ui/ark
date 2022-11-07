07ce871aeabee028f6989b31ad5342407119b794
Tim Kolberger <tim@kolberger.eu>
2022-11-07 22:43:56 +0100

    test: configure jsdom to mock ResizeObserver on defaultView

--- packages/react/src/setupTest.ts
+++ packages/react/src/setupTest.ts
@@ -1,1 +1,11 @@
 import '@testing-library/jest-dom'
+import { JSDOM } from 'jsdom'
+import ResizeObserver from 'resize-observer-polyfill'
+
+const jsdom = new JSDOM('<!doctype html><html><body></body></html>')
+
+// @ts-expect-error DOMWindow !== globalThis
+window = jsdom.window
+Object.assign(global, { window, document: window.document })
+
+window.ResizeObserver = ResizeObserver
