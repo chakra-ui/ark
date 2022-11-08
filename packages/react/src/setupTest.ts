c89f90ddb3a4a33fb3f3ad817dc81d7b2cbafd7d
Tim Kolberger <tim@kolberger.eu>
2022-11-08 08:57:38 +0100

    refactor: move ResizeObserver assignment before global assignment

--- packages/react/src/setupTest.ts
+++ packages/react/src/setupTest.ts
@@ -6,6 +6,6 @@ const jsdom = new JSDOM('<!doctype html><html><body></body></html>')
 
 // @ts-expect-error DOMWindow !== globalThis
 window = jsdom.window
-Object.assign(global, { window, document: window.document })
-
 window.ResizeObserver = ResizeObserver
+
+Object.assign(global, { window, document: window.document })
