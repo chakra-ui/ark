a64e2140a336a5ed356c65b1e31d34a98c8e7a36
Christian Schröter <dev@grizzly.codes>
2022-11-10 18:10:36 +0100

    refactor(react): refactor trigger components (#93)

--- packages/react/src/dialog/dialog-trigger.tsx
+++ packages/react/src/dialog/dialog-trigger.tsx
@@ -1,16 +1,11 @@
-import { cloneElement, ReactElement } from 'react'
-import { ark } from '../factory'
+import { Children, cloneElement, ReactElement } from 'react'
 import { useDialogContext } from './dialog-context'
 
-export type DialogTriggerProps = { children: ReactElement | string | number }
+export type DialogTriggerProps = { children: ReactElement }
 
 export const DialogTrigger = (props: DialogTriggerProps) => {
-  const { children } = props
   const { triggerProps } = useDialogContext()
 
-  return typeof children === 'string' || typeof children === 'number' ? (
-    <ark.span {...triggerProps}>{children}</ark.span>
-  ) : (
-    cloneElement(children, triggerProps)
-  )
+  const onlyChild = Children.only(props.children)
+  return cloneElement(onlyChild, triggerProps)
 }
