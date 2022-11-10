7bfa0c0ba44c64e72ed1e9f9cd928eea0b305299
Christian Schröter <dev@grizzly.codes>
2022-11-10 13:18:05 +0100

    chore: more tests

--- packages/react/src/tags-input/tags-input.test.tsx
+++ packages/react/src/tags-input/tags-input.test.tsx
@@ -59,6 +59,27 @@ describe('TagsInput', () => {
     expect(screen.queryByText('angular')).toBeNull()
   })
 
+  it('should allow to modify an added item', async () => {
+    render(<ComponentUnderTest />)
+    await user.type(screen.getByPlaceholderText('Add tag'), 'angular')
+    await user.keyboard('[Enter]')
+
+    expect(screen.getByText('angular')).toBeInTheDocument()
+
+    await user.keyboard('[ArrowLeft]')
+    await user.keyboard('[Enter]')
+    await user.keyboard('[Backspace]')
+
+    const input = screen.getByLabelText(
+      'Editing tag angular. Press enter to save or escape to cancel.',
+    )
+    await user.clear(input)
+    await user.type(input, 'svelte')
+    await user.keyboard('[Enter]')
+
+    expect(await screen.findByText('svelte')).toBeInTheDocument()
+  })
+
   it('should clear all item when clear all button is clicked', async () => {
     render(<ComponentUnderTest />)
     expect(screen.getByText('react')).toBeInTheDocument()
