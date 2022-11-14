6c7091c0bb278c88e8c8bef0487a49e37830370c
Christian Schröter <dev@grizzly.codes>
2022-11-14 08:09:29 +0100

    chore: add sitemap

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,8 +1,10 @@
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
 import { defineConfig } from 'astro/config'
 import pandacss from 'css-panda/astro'
-import mdx from '@astrojs/mdx'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [pandacss(), mdx()],
+  site: 'https://ark-docs.vercel.app',
+  integrations: [pandacss(), mdx(), sitemap()],
 })
