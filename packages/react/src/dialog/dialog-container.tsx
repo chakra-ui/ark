efa0f0d98658c0dff9b0b875879161edf60e9914
Christian Schröter <dev@grizzly.codes>
2022-11-14 13:05:08 +0100

    refactor(react): rename dialog underlay to dialog container

--- packages/react/src/dialog/dialog-container.tsx
+++ packages/react/src/dialog/dialog-container.tsx
@@ -2,9 +2,10 @@ import { forwardRef } from '@polymorphic-factory/react'
 import { mergeProps } from '@zag-js/react'
 import { ark, HTMLArkProps } from '../factory'
 import { useDialogContext } from './dialog-context'
-export type DialogUnderlayProps = HTMLArkProps<'div'>
 
-export const DialogUnderlay = forwardRef<'div', DialogUnderlayProps>((props, ref) => {
+export type DialogContainerProps = HTMLArkProps<'div'>
+
+export const DialogContainer = forwardRef<'div', DialogContainerProps>((props, ref) => {
   const { underlayProps } = useDialogContext()
   const mergedProps = mergeProps(underlayProps, props)
 
