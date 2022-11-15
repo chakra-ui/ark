531f1676ebd4a156b17978837b09a9e8eb19da16
Tim Kolberger <tim@kolberger.eu>
2022-11-15 17:26:35 +0100

    chore: add Options story

--- packages/react/src/menu/menu.stories.tsx
+++ packages/react/src/menu/menu.stories.tsx
@@ -1,10 +1,12 @@
 import { Portal } from '@reach/portal'
+import { useState } from 'react'
 import {
   Menu,
   MenuContent,
   MenuGroup,
   MenuGroupLabel,
   MenuItem,
+  MenuItemOption,
   MenuPositioner,
   MenuSeparator,
   MenuTrigger,
@@ -55,6 +57,61 @@ export const Group = () => (
     </Portal>
   </Menu>
 )
+
+export const Options = () => {
+  const [value, setValue] = useState<Record<string, string | string[]>>({
+    framework: '',
+    libraries: [],
+  })
+  return (
+    <Menu
+      value={value}
+      onValueChange={(data) => {
+        setValue((prev) => ({
+          ...prev,
+          [data.name]: data.value,
+        }))
+      }}
+    >
+      <MenuTrigger>
+        <button>Open menu</button>
+      </MenuTrigger>
+      <Portal>
+        <MenuPositioner>
+          <MenuContent>
+            <MenuGroup id="radio-group">
+              <MenuGroupLabel htmlFor="radio-group">Radio Group</MenuGroupLabel>
+              <MenuItemOption name="framework" type="radio" value="react">
+                {({ isActive }) => <>{isActive ? '✅' : ''} React</>}
+              </MenuItemOption>
+              <MenuItemOption name="framework" type="radio" value="solid">
+                {({ isActive }) => <>{isActive ? '✅' : ''} Solid</>}
+              </MenuItemOption>
+              <MenuItemOption name="framework" type="radio" value="vue">
+                {({ isActive }) => <>{isActive ? '✅' : ''} Vue</>}
+              </MenuItemOption>
+            </MenuGroup>
+            <MenuGroup id="checkbox-group">
+              <MenuGroupLabel htmlFor="checkbox-group">Checkbox Group</MenuGroupLabel>
+              <MenuItemOption name="libraries" type="checkbox" value="zag-js">
+                {({ isActive }) => <>{isActive ? '✅' : ''} zag-js</>}
+              </MenuItemOption>
+              <MenuItemOption name="libraries" type="checkbox" value="ark">
+                {({ isActive }) => <>{isActive ? '✅' : ''} ark</>}
+              </MenuItemOption>
+              <MenuItemOption name="libraries" type="checkbox" value="panda">
+                {({ isActive }) => <>{isActive ? '✅' : ''} panda</>}
+              </MenuItemOption>
+              <MenuItemOption name="libraries" type="checkbox" value="chakra">
+                {({ isActive }) => <>{isActive ? '✅' : ''} chakra</>}
+              </MenuItemOption>
+            </MenuGroup>
+          </MenuContent>
+        </MenuPositioner>
+      </Portal>
+    </Menu>
+  )
+}
 
 export const SubMenu = () => (
   <Menu>
