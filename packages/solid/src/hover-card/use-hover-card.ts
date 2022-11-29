6600222895ebb0ba24f2bbb1ed985db8ed3761bb
Tim Kolberger <tim@kolberger.eu>
2022-11-29 10:16:42 +0100

    feat(solid): integrate hover card (#166)
    * feat(solid): add hover card
    
    * docs(solid): mark hover card as done in readme
    
    * chore: use solid mergeProps

---
+++ packages/solid/src/hover-card/use-hover-card.ts
@@ -0,0 +1,15 @@
+import * as hoverCard from '@zag-js/hover-card'
+import { normalizeProps, useMachine } from '@zag-js/solid'
+import { createMemo, createUniqueId, mergeProps } from 'solid-js'
+import type { Optional } from '../types'
+
+export type UseHoverCardProps = Optional<hoverCard.Context, 'id'>
+
+export const useHoverCard = (props: UseHoverCardProps) => {
+  const context = mergeProps({ id: createUniqueId() }, props)
+
+  const [state, send] = useMachine(hoverCard.machine(context), { context })
+  return createMemo(() => hoverCard.connect(state, send, normalizeProps))
+}
+
+export type UseHoverCardReturn = ReturnType<typeof useHoverCard>
