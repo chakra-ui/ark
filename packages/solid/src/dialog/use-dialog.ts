c679c72bf58c9c2ef6ecd1bf84fc4415bb1aec11
Tim Kolberger <tim@kolberger.eu>
2022-11-29 10:08:17 +0100

    chore: use solid mergeProps

--- packages/solid/src/dialog/use-dialog.ts
+++ packages/solid/src/dialog/use-dialog.ts
@@ -1,15 +1,12 @@
 import * as dialog from '@zag-js/dialog'
 import { normalizeProps, useMachine } from '@zag-js/solid'
-import { createMemo, createUniqueId } from 'solid-js'
+import { createMemo, createUniqueId, mergeProps } from 'solid-js'
 import type { Optional } from '../types'
 
 export type UseDialogProps = Optional<dialog.Context, 'id'>
 
 export const useDialog = (props: UseDialogProps) => {
-  const context = {
-    id: createUniqueId(),
-    ...props,
-  }
+  const context = mergeProps({ id: createUniqueId() }, props)
 
   const [state, send] = useMachine(dialog.machine(context), { context })
   return createMemo(() => dialog.connect(state, send, normalizeProps))
