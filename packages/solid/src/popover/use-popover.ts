6e2e4703bc7ceb9a4591f544d193305ce09e2e36
Tim Kolberger <tim@kolberger.eu>
2022-11-29 10:18:17 +0100

    chore: use solid mergeProps

--- packages/solid/src/popover/use-popover.ts
+++ packages/solid/src/popover/use-popover.ts
@@ -1,15 +1,12 @@
 import * as popover from '@zag-js/popover'
 import { normalizeProps, useMachine } from '@zag-js/solid'
-import { createMemo, createUniqueId } from 'solid-js'
+import { createMemo, createUniqueId, mergeProps } from 'solid-js'
 import type { Optional } from '../types'
 
 export type UsePopoverProps = Optional<popover.Context, 'id'>
 
 export const usePopover = (props: UsePopoverProps) => {
-  const context = {
-    id: createUniqueId(),
-    ...props,
-  }
+  const context = mergeProps({ id: createUniqueId() }, props)
 
   const [state, send] = useMachine(popover.machine(context), { context })
   return createMemo(() => popover.connect(state, send, normalizeProps))
