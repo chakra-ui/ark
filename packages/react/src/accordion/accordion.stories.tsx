aa3ba6e762c39a658fa5e12b8c77c75d69063559
Tim Kolberger <tim@kolberger.eu>
2022-12-04 19:23:25 +0100

    docs(react): add accordion docs

--- packages/react/src/accordion/accordion.stories.tsx
+++ packages/react/src/accordion/accordion.stories.tsx
@@ -1,7 +1,5 @@
-import { Accordion } from './accordion'
-import { AccordionButton } from './accordion-button'
-import { AccordionItem } from './accordion-item'
-import { AccordionPanel } from './accordion-panel'
+import { useState } from 'react'
+import { Accordion, AccordionButton, AccordionItem, AccordionPanel } from '.'
 
 export const Basic = () => {
   const items = ['panel-1', 'panel-2', 'panel-3']
@@ -51,6 +49,20 @@ export const Disabled = () => {
     <Accordion multiple>
       {items.map((item, id) => (
         <AccordionItem key={id} value={item} disabled={item === 'panel-2'}>
+          <AccordionButton>{item} trigger</AccordionButton>
+          <AccordionPanel>{item} content</AccordionPanel>
+        </AccordionItem>
+      ))}
+    </Accordion>
+  )
+}
+
+export const Controlled = () => {
+  const [value, setValue] = useState<string | string[] | null>(null)
+  return (
+    <Accordion value={value} onChange={(details) => setValue(details.value)}>
+      {['panel-1', 'panel-2', 'panel-3'].map((item, id) => (
+        <AccordionItem key={id} value={item}>
           <AccordionButton>{item} trigger</AccordionButton>
           <AccordionPanel>{item} content</AccordionPanel>
         </AccordionItem>
