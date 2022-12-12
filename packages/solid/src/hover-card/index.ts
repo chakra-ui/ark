eb37e402ab46f1fa8bdf57a39e50094bae881856
Christian Schröter <dev@grizzly.codes>
2022-12-12 21:08:05 +0100

    chore: typo

--- packages/solid/src/hover-card/index.ts
+++ packages/solid/src/hover-card/index.ts
@@ -1,6 +1,6 @@
 export { HoverCard, type HoverCardProps } from './hover-card'
 export { HoverCardArrow, type HoverCardArrowProps } from './hover-card-arrow'
-export { HoverCardArrowTip, type HoverCardArrowTipProps } from './hover-card-arrrow-tip'
+export { HoverCardArrowTip, type HoverCardArrowTipProps } from './hover-card-arrow-tip'
 export { HoverCardContent, type HoverCardContentProps } from './hover-card-content'
 export { HoverCardPortal, type HoverCardPortalProps } from './hover-card-portal'
 export { HoverCardPositioner, type HoverCardPositionerProps } from './hover-card-positioner'
