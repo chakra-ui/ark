20b075516e48f0a8133949272c6df0da9050f7fc
Christian Schröter <dev@grizzly.codes>
2022-12-13 16:53:04 +0100

    build: use vite to build packages

--- packages/solid/vite.config.ts
+++ packages/solid/vite.config.ts
@@ -2,14 +2,39 @@
 /// <reference types="vite/client" />
 
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 import solid from 'vite-plugin-solid'
+import pkg from './package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [
+    dts({
+      tsConfigFilePath: 'tsconfig.build.json',
+    }),
+    solid(),
+  ],
+  build: {
+    target: 'esnext',
+    lib: {
+      entry: 'src/index.ts',
+      formats: ['es', 'cjs'],
+      fileName: (format) => (format === 'es' ? 'index.esm.js' : 'index.cjs.js'),
+    },
+    rollupOptions: {
+      external: [
+        ...Object.keys(pkg.dependencies),
+        ...Object.keys(pkg.peerDependencies),
+        'solid-js',
+        'solid-js/web',
+        'solid-js/store',
+      ],
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './src/setup-test.ts',
+    setupFiles: 'src/setup-test.ts',
     coverage: {
       all: true,
       reporter: ['lcov', 'text'],
@@ -18,7 +43,6 @@ export default defineConfig({
     },
     css: false,
   },
-  plugins: [solid()],
   resolve: {
     conditions: ['development', 'browser'],
   },
