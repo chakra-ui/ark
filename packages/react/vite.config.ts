cdbd4833f2aa62e6e76ea1a4d073be8f024a2346
Christian Schröter <dev@grizzly.codes>
2022-12-13 20:19:44 +0100

    build: use vite to build packages (#245)

--- packages/react/vite.config.ts
+++ packages/react/vite.config.ts
@@ -3,12 +3,30 @@
 
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+import pkg from './package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    dts({
+      tsConfigFilePath: 'tsconfig.build.json',
+    }),
+    react(),
+  ],
+  build: {
+    target: 'esnext',
+    lib: {
+      entry: 'src/index.ts',
+      formats: ['es', 'cjs'],
+      fileName: (format) => (format === 'es' ? 'index.esm.js' : 'index.cjs.js'),
+    },
+    rollupOptions: {
+      external: [...Object.keys(pkg.dependencies), ...Object.keys(pkg.peerDependencies)],
+    },
+  },
   test: {
-    setupFiles: './src/setup-test.ts',
+    setupFiles: 'src/setup-test.ts',
     coverage: {
       all: true,
       reporter: ['lcov', 'text'],
