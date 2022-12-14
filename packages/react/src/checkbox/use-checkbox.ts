341c7596d575e050a706dc32892168993311bbc9
Christian Schröter <dev@grizzly.codes>
2022-12-14 19:39:13 +0100

    fix: update to latest polymorph version and try to avoid asteriks imports in tabs and checkbox

--- packages/react/src/checkbox/use-checkbox.ts
+++ packages/react/src/checkbox/use-checkbox.ts
@@ -1,10 +1,10 @@
-import * as checkbox from '@zag-js/checkbox'
+import { connect, Context, machine } from '@zag-js/checkbox'
 import { normalizeProps, useMachine } from '@zag-js/react'
 import { useId } from 'react'
 import type { Optional } from '../types'
 
-export type UseCheckboxProps = Optional<checkbox.Context, 'id'> & {
-  defaultValue?: checkbox.Context['value']
+export type UseCheckboxProps = Optional<Context, 'id'> & {
+  defaultValue?: Context['value']
 }
 export type UseCheckboxReturn = ReturnType<typeof useCheckbox>
 
@@ -20,6 +20,6 @@ export const useCheckbox = (props: UseCheckboxProps) => {
     value: props.value,
   }
 
-  const [state, send] = useMachine(checkbox.machine(initialContext), { context })
-  return checkbox.connect(state, send, normalizeProps)
+  const [state, send] = useMachine(machine(initialContext), { context })
+  return connect(state, send, normalizeProps)
 }
