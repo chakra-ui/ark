ec72ce99ed381b240346fe4ba00e4152a76539e9
Christian Schröter <dev@grizzly.codes>
2022-12-15 09:25:30 +0100

    chore: wip

--- packages/solid/src/pagination/pagination.anatomy.ts
+++ packages/solid/src/pagination/pagination.anatomy.ts
@@ -1,3 +1,3 @@
-import { anatomy } from '@zag-js/pagination'
+export { anatomy as paginationAnatomy } from '@zag-js/pagination'
 
-export const paginationAnatomy = anatomy.extendWith('list')
+// export const paginationAnatomy = anatomy.extendWith('list')
--- packages/solid/vite.config.ts
+++ packages/solid/vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     dts({
       rollupTypes: true,
     }),
-    solid({ ssr: true }),
+    solid(),
   ],
   build: {
     target: 'esnext',
