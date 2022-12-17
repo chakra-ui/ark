e5a8517413d6a98cd17a1315b1813f07d498e803
Christian Schröter <dev@grizzly.codes>
2022-12-17 15:17:11 +0100

    chore: spread attrs on list

--- packages/react/src/pagination/pagination-list.tsx
+++ packages/react/src/pagination/pagination-list.tsx
@@ -1,8 +1,9 @@
 import { forwardRef } from '@polymorphic-factory/react'
 import { ark, HTMLArkProps } from '../factory'
+import { parts } from './pagination.anatomy'
 
 export type PaginationListProps = HTMLArkProps<'ul'>
 
 export const PaginationList = forwardRef<'ul', PaginationListProps>((props, ref) => (
-  <ark.ul {...props} ref={ref} />
+  <ark.ul {...parts.list.attrs} {...props} ref={ref} />
 ))
