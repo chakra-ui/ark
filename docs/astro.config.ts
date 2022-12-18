153659f3cc6bf565cd4fc82ab3795f0b870e53b1
Tim Kolberger <tim@kolberger.eu>
2022-12-18 12:45:17 +0100

    chore: use vercel url

--- docs/astro.config.ts
+++ docs/astro.config.ts
@@ -5,6 +5,6 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ark-docs.vercel.app',
+  site: process.env.VERCEL_URL || 'http://localhost:3000',
   integrations: [mdx(), sitemap(), react()],
 })
