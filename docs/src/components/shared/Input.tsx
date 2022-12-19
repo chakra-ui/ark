29d242a98b36d3c8bf82407e46c86a23a46bf607
Tim Kolberger <tim@kolberger.eu>
2022-12-19 15:27:38 +0100

    feat: set default props for input

--- docs/src/components/shared/Input.tsx
+++ docs/src/components/shared/Input.tsx
@@ -4,6 +4,6 @@ import { input, InputVariants } from '../../../panda/recipes'
 export type InputProps = HTMLPandaProps<'input'> & InputVariants
 
 export const Input = (props: InputProps) => {
-  const { variant, size, ...rest } = props
+  const { variant = 'outline', size = 'md', ...rest } = props
   return <panda.input className={input({ variant, size })} {...rest} />
 }
