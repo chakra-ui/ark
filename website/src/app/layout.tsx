aac6fa746b24f9a58484454bab1e11fdc7a92ac0
Christian Schröter <dev@grizzly.codes>
2022-12-22 18:05:05 +0100

    chore: tweak syntax highlightnin

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -1,15 +1,16 @@
 import { Navbar } from '@/components/Navbar'
-import { css } from '@/panda/css'
-import { Inter } from '@next/font/google'
+import { css, cx } from '@/panda/css'
+import { Inter, Roboto_Mono } from '@next/font/google'
 import { PropsWithChildren } from 'react'
 import '../../panda/styles.css'
 // TODO use panda for styling
 import '../../public/code.css'
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
+const roboto = Roboto_Mono({ subsets: ['latin'], variable: '--font-roboto-mono' })
 
 const RootLayout = (props: PropsWithChildren) => (
-  <html lang="en" className={inter.className}>
+  <html lang="en" className={cx(inter.variable, roboto.variable)}>
     <head />
     <body>
       <div className={css({ minHeight: 'full' })}>
