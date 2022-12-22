37b2a987a0b7d29997126547bee3fd067af7b2d3
Christian Schröter <dev@grizzly.codes>
2022-12-22 14:07:25 +0100

    chore: add playground

---
+++ website/src/components/demo/RangeSlider.tsx
@@ -0,0 +1,29 @@
+import {
+  RangeSlider,
+  RangeSliderControl,
+  RangeSliderMarker,
+  RangeSliderMarkerGroup,
+  RangeSliderProps,
+  RangeSliderRange,
+  RangeSliderThumb,
+  RangeSliderTrack,
+} from '@ark-ui/react'
+import { slider } from '../../../panda/recipes'
+
+export const DemoRangeSlider = (props: Partial<RangeSliderProps>) => (
+  <RangeSlider min={0} max={100} defaultValue={[33, 66]} className={slider()} {...props}>
+    <RangeSliderControl>
+      <RangeSliderTrack>
+        <RangeSliderRange />
+      </RangeSliderTrack>
+      {[0, 1].map((i) => (
+        <RangeSliderThumb key={i} index={i} />
+      ))}
+    </RangeSliderControl>
+    <RangeSliderMarkerGroup>
+      <RangeSliderMarker value={25}>25</RangeSliderMarker>
+      <RangeSliderMarker value={50}>50</RangeSliderMarker>
+      <RangeSliderMarker value={75}>75</RangeSliderMarker>
+    </RangeSliderMarkerGroup>
+  </RangeSlider>
+)
---
+++ website/src/components/demo/Tooltip.tsx
@@ -0,0 +1,34 @@
+import {
+  Tooltip,
+  TooltipArrow,
+  TooltipArrowTip,
+  TooltipContent,
+  TooltipPositioner,
+  TooltipProps,
+  TooltipTrigger,
+} from '@ark-ui/react'
+import { tooltip } from '../../../panda/recipes'
+import { Button } from '../shared/Button'
+
+export type DemoTooltipProps = Partial<TooltipProps> & {
+  placement?: NonNullable<TooltipProps['positioning']>['placement']
+}
+
+export const DemoTooltip = (props: DemoTooltipProps) => {
+  const { placement = 'top', ...tooltipProps } = props
+  return (
+    <Tooltip openDelay={0} closeDelay={200} positioning={{ placement }} {...tooltipProps}>
+      <TooltipTrigger>
+        <Button size="sm" variant="tertiary">
+          hover me
+        </Button>
+      </TooltipTrigger>
+      <TooltipPositioner className={tooltip({})}>
+        <TooltipArrow>
+          <TooltipArrowTip />
+        </TooltipArrow>
+        <TooltipContent>My Tooltip</TooltipContent>
+      </TooltipPositioner>
+    </Tooltip>
+  )
+}
