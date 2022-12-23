2a36c5597e58bf1aac215dd3da4d4e8be748bde1
Christian Schröter <dev@grizzly.codes>
2022-12-23 17:07:54 +0100

    chore: remove astro

--- docs/src/components/shared/Text.tsx
+++
@@ -1,6 +0,0 @@
-import { panda } from '../../../panda/jsx'
-import type { JSXStyleProperties } from '../../../panda/types'
-
-export const Text = (props: JSXStyleProperties) => {
-  return <panda.p {...props} />
-}
