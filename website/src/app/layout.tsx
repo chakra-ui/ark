05b1ec7dcb5e0fb07ca88c06a9b1e8087c9dece9
Christian Schröter <dev@grizzly.codes>
2023-01-05 16:43:05 +0100

    feat(website): prepare for review (#362)

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -12,7 +12,9 @@ const roboto = Roboto_Mono({ subsets: ['latin'], variable: '--font-roboto-mono' 
 const RootLayout = (props: PropsWithChildren) => {
   return (
     <html lang="en" className={cx(inter.variable, roboto.variable)}>
-      <head />
+      <head>
+        <script defer data-domain="ark-ui.com" src="https://plausible.io/js/script.js"></script>
+      </head>
       <body>
         <div className={css({ minHeight: 'full' })}>
           <Navbar />
