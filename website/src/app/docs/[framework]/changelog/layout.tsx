ae0bd32b4728dc0121c55507362ac3dc9377c715
Tim Kolberger <tim@kolberger.eu>
2023-01-06 19:25:51 +0100

    fix(website): fix page overflow from too wide Playground

--- website/src/app/docs/[framework]/changelog/layout.tsx
+++ website/src/app/docs/[framework]/changelog/layout.tsx
@@ -17,7 +17,7 @@ const DocsLayout = (props: any) => {
           items={sidebarItems}
           activePath={`/docs/${params.framework}/changelog`}
         />
-        <panda.main flex="1" py="12">
+        <panda.main flex="1" overflow="hidden" py="12">
           {props.children}
         </panda.main>
       </Stack>
