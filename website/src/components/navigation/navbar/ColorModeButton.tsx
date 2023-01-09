cda0393e00f5a90fa05b85dac097efea93131ee2
Tim Kolberger <tim@kolberger.eu>
2023-01-09 20:25:32 +0100

    feat(website): pretty color mode button (#368)

--- website/src/components/navigation/navbar/ColorModeButton.tsx
+++ website/src/components/navigation/navbar/ColorModeButton.tsx
@@ -1,18 +1,30 @@
 'use client'
-import { IconButton } from '@/components/shared/IconButton'
 import { useColorMode } from '@/lib/useColorMode'
-import { FiMoon } from 'react-icons/fi'
+import { css, cx } from '@/panda/css'
+import { button } from '@/panda/recipes'
+import { Expand } from '@theme-toggles/react'
+import { useIsClient } from 'usehooks-ts'
+import '@theme-toggles/react/css/Expand.css'
 
-// TODO use IconButton
 export const ColorModeButton = () => {
-  const { toggle } = useColorMode()
+  const { toggle, colorMode } = useColorMode()
+
+  const isClient = useIsClient()
+  // prevent false positive for server-side rendering
+  if (!isClient) {
+    return null
+  }
+
   return (
-    <IconButton
-      variant="tertiary"
-      size="md"
-      icon={<FiMoon />}
-      aria-label="Toggle color mode"
-      onClick={toggle}
+    <Expand
+      onToggle={toggle}
+      toggled={colorMode === 'light'}
+      className={cx(
+        button({ variant: 'tertiary', size: 'md' }),
+        css({ fontSize: '2xl', px: '0', _hover: { bg: 'bg.subtle !important' } }),
+      )}
+      data-scope="button"
+      data-part="root"
     />
   )
 }
