59533187b4a37ae89e6e54fffd7f9273970d49e3
Christian Schröter <dev@grizzly.codes>
2023-01-09 21:36:55 +0100

    feat(website): add stats section

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -1,9 +1,11 @@
-import { Hero } from '../components/marketing/Hero'
+import { Hero } from '@/components/marketing/Hero'
+import { Stats } from '@/components/marketing/Stats'
 
 export default function Page() {
   return (
     <>
       <Hero />
+      <Stats />
     </>
   )
 }
