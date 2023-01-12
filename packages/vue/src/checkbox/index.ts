0c12e9b76634e1ddf39f7b56f553206d0bd5a510
tylerapfledderer <tyler.pfledderer@gmail.com>
2023-01-11 23:43:26 -0500

    feat(vue/checkbox): add checkbox label

--- packages/vue/src/checkbox/index.ts
+++ packages/vue/src/checkbox/index.ts
@@ -1,3 +1,4 @@
 export { CheckboxControl, type CheckboxControlProps } from './checkbox-control'
 export { CheckboxInput, type CheckboxInputProps } from './checkbox-input'
+export { CheckboxLabel, type CheckboxLabelProps } from './checkbox-label'
 export { checkboxAnatomy } from './checkbox.anatomy'
