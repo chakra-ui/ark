93ddff7b74a84c0b84a6ca50a0354a21b4c76b54
Tim Kolberger <tim@kolberger.eu>
2023-01-14 22:17:58 +0100

    test(react): isOpen prop of popover

--- packages/react/src/popover/popover.test.tsx
+++ packages/react/src/popover/popover.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react'
 import user from '@testing-library/user-event'
+import { useState } from 'react'
 import {
   Popover,
   PopoverArrow,
@@ -13,7 +14,7 @@ import {
   PopoverTrigger,
 } from './'
 
-const Component = (props: PopoverProps) => (
+const ComponentUnderTest = (props: PopoverProps) => (
   <Popover {...props}>
     <PopoverTrigger>
       <button>click me</button>
@@ -35,7 +36,7 @@ const Component = (props: PopoverProps) => (
 
 describe('Popover', () => {
   it('should open and close the popover', async () => {
-    render(<Component />)
+    render(<ComponentUnderTest />)
 
     await user.click(screen.getByText('click me'))
     expect(screen.getByRole('dialog')).toBeInTheDocument()
@@ -45,7 +46,7 @@ describe('Popover', () => {
   })
 
   it('should hide the tooltip when escape is pressed', async () => {
-    render(<Component />)
+    render(<ComponentUnderTest />)
 
     await user.click(screen.getByText('click me'))
     expect(screen.getByRole('dialog')).toBeInTheDocument()
@@ -55,9 +56,31 @@ describe('Popover', () => {
   })
 
   it('should focus the first focusable element', async () => {
-    render(<Component />)
+    render(<ComponentUnderTest />)
 
     await user.click(screen.getByText('click me'))
     expect(screen.getByRole('dialog')).toBeInTheDocument()
+  })
+
+  it('should allow controlled usage', async () => {
+    const ControlledComponentUnderTest = (props: PopoverProps) => {
+      const [isOpen, setOpen] = useState(false)
+      return (
+        <>
+          <button onClick={() => setOpen((prev) => !prev)}>toggle</button>
+          <ComponentUnderTest {...props} isOpen={isOpen} />
+        </>
+      )
+    }
+
+    render(<ControlledComponentUnderTest />)
+    expect(screen.queryByText('title')).not.toBeVisible()
+
+    await user.click(screen.getByRole('button', { name: /toggle/i }))
+    expect(screen.getByRole('dialog')).toBeInTheDocument()
+    expect(screen.queryByText('title')).toBeVisible()
+
+    await user.click(screen.getByRole('button', { name: /toggle/i }))
+    expect(screen.queryByText('title')).not.toBeVisible()
   })
 })
