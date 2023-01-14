a05617a78b360d0ec875d786244b646fed03c897
Tim Kolberger <tim@kolberger.eu>
2023-01-14 22:31:21 +0100

    feat(react): add render prop to Dialog component

--- packages/react/src/dialog/dialog.tsx
+++ packages/react/src/dialog/dialog.tsx
@@ -1,12 +1,19 @@
-import type { PropsWithChildren } from 'react'
-import { DialogProvider } from './dialog-context'
+import type { PropsWithChildren, ReactNode } from 'react'
+import { runIfFn } from '../run-if-fn'
+import type { Assign } from '../types'
+import { DialogContext, DialogProvider } from './dialog-context'
 import { useDialog, UseDialogProps } from './use-dialog'
 
-export type DialogProps = PropsWithChildren<UseDialogProps>
+export type DialogProps = Assign<
+  PropsWithChildren<UseDialogProps>,
+  {
+    children: ReactNode | ((pages: DialogContext) => ReactNode)
+  }
+>
 
 export const Dialog = (props: DialogProps) => {
   const { children, ...useDialogProps } = props
   const dialog = useDialog(useDialogProps)
-
-  return <DialogProvider value={dialog}>{children}</DialogProvider>
+  const view = runIfFn(children, dialog)
+  return <DialogProvider value={dialog}>{view}</DialogProvider>
 }
