ee0af8871b526bcaf193324c746da307b964b8f9
Tim Kolberger <tim@kolberger.eu>
2023-01-14 23:17:16 +0100

    feat(react): add render prop to Slider and SliderOutput

--- packages/react/src/slider/slider-output.tsx
+++ packages/react/src/slider/slider-output.tsx
@@ -1,13 +1,26 @@
 import { forwardRef } from '@polymorphic-factory/react'
 import { mergeProps } from '@zag-js/react'
+import type { ReactNode } from 'react'
 import { ark, HTMLArkProps } from '../factory'
-import { useSliderContext } from './slider-context'
+import { runIfFn } from '../run-if-fn'
+import type { Assign } from '../types'
+import { SliderContext, useSliderContext } from './slider-context'
 
-export type SliderOutputProps = HTMLArkProps<'output'>
+export type SliderOutputProps = Assign<
+  HTMLArkProps<'output'>,
+  {
+    children?: ((context: SliderContext) => ReactNode) | ReactNode
+  }
+>
 
 export const SliderOutput = forwardRef<'output', SliderOutputProps>((props, ref) => {
-  const { outputProps } = useSliderContext()
-  const mergedProps = mergeProps(outputProps, props)
-
-  return <ark.output {...mergedProps} ref={ref} />
+  const { children, ...rest } = props
+  const slider = useSliderContext()
+  const mergedProps = mergeProps(slider.outputProps, rest)
+  const view = runIfFn(children, slider)
+  return (
+    <ark.output {...mergedProps} ref={ref}>
+      {view}
+    </ark.output>
+  )
 })
