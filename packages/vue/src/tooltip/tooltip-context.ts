d008c9efaf7e2e84d626227a31c7597788758f33
tylerapfledderer <tyler.pfledderer@gmail.com>
2023-01-21 11:17:10 -0500

    feat(vue/tooltip): export context type

--- packages/vue/src/tooltip/tooltip-context.ts
+++ packages/vue/src/tooltip/tooltip-context.ts
@@ -1,8 +1,9 @@
 import type { connect } from '@zag-js/tooltip'
 import type { ComputedRef } from 'vue'
 import { createContext } from '../context'
+import type { UseTooltipReturn } from './use-tooltip'
 
 export const [TooltipProvider, useTooltipContext] =
   createContext<ComputedRef<ReturnType<typeof connect>>>('TooltipContext')
 
-// TODO: export type TooltipContext = UseTooltipReturn
+export type TooltipContext = UseTooltipReturn
