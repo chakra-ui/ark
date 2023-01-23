9768fb0613cc0f0625d46ef4961b9b5b24c89a74
tylerapfledderer <tyler.pfledderer@gmail.com>
2023-01-22 23:00:14 -0500

    fix(vue/rating-group): fix context type for rating provider

--- packages/vue/src/rating-group/rating-context.ts
+++ packages/vue/src/rating-group/rating-context.ts
@@ -5,4 +5,6 @@ import { createContext } from '../context'
 export type RatingContext = ReturnType<ReturnType<typeof connect>['getRatingState']>
 
 export const [RatingProvider, useRatingContext] =
-  createContext<ComputedRef<ReturnType<typeof connect>>>('RatingContext')
+  createContext<ComputedRef<ReturnType<ReturnType<typeof connect>['getRatingState']>>>(
+    'RatingContext',
+  )
