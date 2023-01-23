cc0cfc63439166ad8ddf5ca3d76d47140722b440
tylerapfledderer <tyler.pfledderer@gmail.com>
2023-01-22 22:04:12 -0500

    feat(vue/rating-group): add label

--- packages/vue/src/rating-group/index.ts
+++ packages/vue/src/rating-group/index.ts
@@ -1,1 +1,2 @@
+export { RatingGroupLabel, type RatingGroupLabelProps } from './rating-group-label'
 export { ratingGroupAnatomy } from './rating-group.anatomy'
---
+++ packages/vue/src/rating-group/rating-group-label.tsx
@@ -0,0 +1,19 @@
+import { defineComponent } from 'vue'
+import { ark, HTMLArkProps } from '../factory'
+import { ComponentWithProps, getValidChildren } from '../utils'
+import { useRatingGroupContext } from './rating-group-context'
+
+export type RatingGroupLabelProps = HTMLArkProps<'label'>
+
+export const RatingGroupLabel: ComponentWithProps<RatingGroupLabelProps> = defineComponent({
+  name: 'RatingGroupLabel',
+  setup(_, { slots, attrs }) {
+    const api = useRatingGroupContext()
+
+    return () => (
+      <ark.label {...api.value.labelProps} {...attrs}>
+        {() => getValidChildren(slots)}
+      </ark.label>
+    )
+  },
+})
