0a6f8dbe3e7b5b43343e205959b7d7972da67159
tylerapfledderer <tyler.pfledderer@gmail.com>
2023-01-24 19:57:11 -0500

    chore(vue/hover-card): remove unneed type for trigger

--- packages/vue/src/hover-card/hover-card-trigger.tsx
+++ packages/vue/src/hover-card/hover-card-trigger.tsx
@@ -2,12 +2,7 @@ import { defineComponent, h } from 'vue'
 import { useUniqueChild } from '../utils'
 import { useHoverCardContext } from './hover-card-context'
 
-/** This type is here so that the script 'check-exports' passes
- *  because in Vue we don't pass 'children' as props
- */
-export type HoverCardTriggerProps = Record<string, unknown>
-
-export const HoverCardTrigger = defineComponent<HoverCardTriggerProps>({
+export const HoverCardTrigger = defineComponent({
   name: 'HoverCardTrigger',
   setup(_, { slots, attrs }) {
     const api = useHoverCardContext()
