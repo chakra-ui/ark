02c0fbba32bb0e9ce547b02ae13bb74281916048
tylerapfledderer <tyler.pfledderer@gmail.com>
2023-01-29 23:28:48 -0500

    feat(vue/rating-group): add context type

--- packages/vue/src/rating-group/rating-group-context.ts
+++ packages/vue/src/rating-group/rating-group-context.ts
@@ -1,8 +1,9 @@
 import type { connect } from '@zag-js/rating-group'
 import type { ComputedRef } from 'vue'
 import { createContext } from '../context'
+import type { UseRatingGroupReturn } from './use-rating-group'
 
 export const [RatingGroupProvider, useRatingGroupContext] =
   createContext<ComputedRef<ReturnType<typeof connect>>>('RatingGroupContext')
 
-// TODO: export type RatingGroupContext = UseRatingGroupReturn
+export type RatingGroupContext = UseRatingGroupReturn
