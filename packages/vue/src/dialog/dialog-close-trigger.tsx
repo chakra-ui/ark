1afebfe1b7c38407853c977d24c43b7158c5cbf7
tylerapfledderer <tyler.pfledderer@gmail.com>
2023-01-29 22:20:08 -0500

    chore(vue/dialog): remove unneeded trigger type sigs

--- packages/vue/src/dialog/dialog-close-trigger.tsx
+++ packages/vue/src/dialog/dialog-close-trigger.tsx
@@ -2,12 +2,7 @@ import { defineComponent, h } from 'vue'
 import { useUniqueChild } from '../utils'
 import { useDialogContext } from './dialog-context'
 
-/** This type is here so that the script 'check-exports' passes
- *  because in Vue we don't pass 'children' as props
- */
-export type DialogCloseTriggerProps = Record<string, unknown>
-
-export const DialogCloseTrigger = defineComponent<DialogCloseTriggerProps>({
+export const DialogCloseTrigger = defineComponent({
   name: 'DialogCloseTrigger',
   setup(_, { slots, attrs }) {
     const api = useDialogContext()
--- packages/vue/src/dialog/dialog-trigger.tsx
+++ packages/vue/src/dialog/dialog-trigger.tsx
@@ -2,12 +2,7 @@ import { defineComponent, h } from 'vue'
 import { useUniqueChild } from '../utils'
 import { useDialogContext } from './dialog-context'
 
-/** This type is here so that the script 'check-exports' passes
- *  because in Vue we don't pass 'children' as props
- */
-export type DialogTriggerProps = Record<string, unknown>
-
-export const DialogTrigger = defineComponent<DialogTriggerProps>({
+export const DialogTrigger = defineComponent({
   name: 'DialogTrigger',
   setup(_, { slots, attrs }) {
     const api = useDialogContext()
