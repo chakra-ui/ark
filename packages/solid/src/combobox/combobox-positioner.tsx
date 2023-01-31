e02216ba4f4d339940e2079c09b2c45e7c20971b
Tyler Pfledderer <tyler.pfledderer@gmail.com>
2023-01-31 07:11:07 -0500

    feat(solid): add combobox (#444)
    Co-authored-by: Christian Schröter <dev@grizzly.codes>

---
+++ packages/solid/src/combobox/combobox-positioner.tsx
@@ -0,0 +1,10 @@
+import { ark, HTMLArkProps } from '../factory'
+import { useComboboxContext } from './combobox-context'
+
+export type ComboboxPositionerProps = HTMLArkProps<'div'>
+
+export const ComboboxPositioner = (props: ComboboxPositionerProps) => {
+  const combobox = useComboboxContext()
+
+  return <ark.div {...combobox().positionerProps} {...props} />
+}
---
+++ packages/solid/src/combobox/combobox.test.tsx
@@ -0,0 +1,90 @@
+import user from '@testing-library/user-event'
+import { For, Portal } from 'solid-js/web'
+import { render, screen } from 'solid-testing-library'
+import { vi } from 'vitest'
+import {
+  Combobox,
+  ComboboxContent,
+  ComboboxControl,
+  ComboboxInput,
+  ComboboxLabel,
+  ComboboxOption,
+  ComboboxOptionProps,
+  ComboboxPositioner,
+  ComboboxProps,
+  ComboboxTrigger,
+} from '.'
+
+const ComponentUnderTest = (props: Omit<ComboboxProps, 'children'>) => {
+  const options: Pick<ComboboxOptionProps, 'label' | 'value' | 'disabled'>[] = [
+    { label: 'ReactJS', value: 'react' },
+    { label: 'SolidJS', value: 'solid' },
+    { label: 'VueJS', value: 'vue', disabled: true },
+    { label: 'AngularJS', value: 'angular' },
+  ]
+
+  return (
+    <Combobox {...props}>
+      <ComboboxLabel>JS Frameworks</ComboboxLabel>
+      <ComboboxControl>
+        <ComboboxInput />
+        <ComboboxTrigger>
+          <button data-testid="trigger-btn">▼</button>
+        </ComboboxTrigger>
+      </ComboboxControl>
+      <Portal>
+        <ComboboxPositioner>
+          <ComboboxContent>
+            <For each={options}>
+              {(option) => (
+                <ComboboxOption
+                  label={option.label}
+                  value={option.value}
+                  disabled={option?.disabled}
+                >
+                  {option.label}
+                </ComboboxOption>
+              )}
+            </For>
+          </ComboboxContent>
+        </ComboboxPositioner>
+      </Portal>
+    </Combobox>
+  )
+}
+
+describe('Combobox', () => {
+  it('should render', () => {
+    render(() => <ComponentUnderTest />)
+  })
+
+  it('should show options on click', async () => {
+    render(() => <ComponentUnderTest />)
+    expect(screen.getByRole('option', { hidden: true, name: 'ReactJS' })).not.toBeVisible()
+
+    await user.click(screen.getByTestId('trigger-btn'))
+    expect(screen.getByRole('option', { name: 'ReactJS' })).toBeVisible()
+  })
+
+  it('should show a disabled option', async () => {
+    render(() => <ComponentUnderTest />)
+    expect(screen.getByRole('option', { hidden: true, name: 'ReactJS' })).not.toBeVisible()
+
+    await user.click(screen.getByTestId('trigger-btn'))
+    expect(screen.getByRole('option', { name: 'VueJS' })).toHaveAttribute('aria-disabled', 'true')
+  })
+
+  it('should call onInputChange when value changes', async () => {
+    const onInputChange = vi.fn()
+
+    render(() => <ComponentUnderTest onInputChange={onInputChange} />)
+
+    const TEST_VAL = 'react'
+
+    const inputField = screen.getByRole('combobox')
+    await user.type(inputField, TEST_VAL)
+    expect(inputField).toHaveValue(TEST_VAL)
+    expect(onInputChange).toBeCalledWith({ value: TEST_VAL })
+    expect(onInputChange).toBeCalledTimes(TEST_VAL.length)
+  })
+})
