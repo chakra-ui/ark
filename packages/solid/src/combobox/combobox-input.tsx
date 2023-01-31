f95bd8c130adf0cea809d424dcd4ed7bc769d77d
Tyler Pfledderer <tyler.pfledderer@gmail.com>
2023-01-31 07:11:07 -0500

    feat(solid): add combobox (#444)
    Co-authored-by: Christian Schröter <dev@grizzly.codes>

---
+++ packages/solid/src/combobox/combobox-input.tsx
@@ -0,0 +1,10 @@
+import { ark, HTMLArkProps } from '../factory'
+import { useComboboxContext } from './combobox-context'
+
+export type ComboboxInputProps = HTMLArkProps<'input'>
+
+export const ComboboxInput = (props: ComboboxInputProps) => {
+  const combobox = useComboboxContext()
+
+  return <ark.input {...combobox().inputProps} {...props} />
+}
