c3a658f9615df54e0ad6fcfdc7645c6f80208987
Tim Kolberger <tim@kolberger.eu>
2023-02-12 23:13:28 +0100

    feat(website): add splitter demo component

---
+++ website/src/components/docs/demo/Splitter.tsx
@@ -0,0 +1,37 @@
+import { splitter } from '@/panda/recipes'
+import { Splitter, SplitterPanel, SplitterProps, SplitterResizeTrigger } from '@ark-ui/react'
+import { useId } from 'react'
+
+const Basic = (props: Partial<SplitterProps> & { content?: React.ReactNode }) => {
+  const [first, second] = [useId(), useId()].map((id) => id.replace(/:/g, '-'))
+  return (
+    <>
+      <Splitter
+        size={[
+          { id: first, size: 50 },
+          { id: second, size: 50 },
+        ]}
+        {...props}
+        className={splitter()}
+      >
+        <SplitterPanel id={first}>{props.content ?? <p>{first}</p>}</SplitterPanel>
+        <SplitterResizeTrigger id={`${first}:${second}`}>
+          <div />
+        </SplitterResizeTrigger>
+        <SplitterPanel id={second}>{props.children ?? <p>{second}</p>}</SplitterPanel>
+      </Splitter>
+    </>
+  )
+}
+
+export const DemoSplitter = (props: Partial<SplitterProps>) => (
+  <div style={{ height: '26rem', width: '100%' }}>
+    <Basic {...props} orientation="vertical" content="A">
+      <Basic {...props} content="B">
+        <Basic {...props} orientation="vertical" content="C">
+          D
+        </Basic>
+      </Basic>
+    </Basic>
+  </div>
+)
