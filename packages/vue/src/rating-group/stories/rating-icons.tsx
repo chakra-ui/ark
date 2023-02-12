3245b60e5afa7d31444a861bff1187e153a9cd32
codebender828 <jbakebwa@163.com>
2023-02-12 18:25:18 +0800

    test(select): add wait func in between renders

--- packages/vue/src/rating-group/stories/rating-icons.tsx
+++ packages/vue/src/rating-group/stories/rating-icons.tsx
@@ -8,14 +8,14 @@ const IconHalf = () => (
     }}
   >
     <path
-      fillRule="evenodd"
-      clipRule="evenodd"
+      fill-rule="evenodd"
+      clip-rule="evenodd"
       d="M135.977 214.086L52.1294 259.594L69.6031 165.229L0 99.1561L95.1465 86.614L135.977 1.04785V214.086Z"
       fill="currentColor"
     ></path>
     <path
-      fillRule="evenodd"
-      clipRule="evenodd"
+      fill-rule="evenodd"
+      clip-rule="evenodd"
       d="M135.977 213.039L219.826 258.546L202.352 164.181L271.957 98.1082L176.808 85.5661L135.977 0V213.039Z"
       fill="#bdbdbd"
     ></path>
