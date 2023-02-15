762a0795400c3727ce8335acc53169735f992e13
Christian Schröter <dev@grizzly.codes>
2023-02-15 12:56:19 +0100

    chore: clean up spread fn

--- packages/solid/src/spread.ts
+++ packages/solid/src/spread.ts
@@ -5,23 +5,23 @@ const getEventKey = (key: string) => `$$${key.toLowerCase().slice(2)}`
 
 export const spread = (node: HTMLElement, props: any) => {
   const eventListeners = filterObject(props, ([key]) => key.slice(0, 2) === 'on')
-  // @ts-expect-error fix later
+  // @ts-expect-error - TODO iterator type
   const events = filterObject(node, ([key]) => key.slice(0, 2) === '$$')
 
-  const result: Record<string, unknown> = {}
-
-  for (const key in eventListeners) {
-    const prop = getEventKey(key)
-    if (prop in events) {
-      result[key] = function (...args: any[]) {
-        // @ts-expect-error fix later
-        events[prop](...args)
-        eventListeners[key](...args)
-      }
-    } else {
-      result[key] = eventListeners[key]
-    }
-  }
+  const result = Object.fromEntries(
+    Object.entries(eventListeners).map(([key, value]) => {
+      const prop = getEventKey(key)
+      const newValue =
+        prop in events
+          ? function (...args: unknown[]) {
+              // @ts-expect-error - TODO iterator type
+              events[prop](...args)
+              value(...args)
+            }
+          : value
+      return [key, newValue]
+    }),
+  )
 
   solidSpread(node, { ...props, ...result })
 }
