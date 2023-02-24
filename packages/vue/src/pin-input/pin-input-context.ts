3f2f5d7e92a288b0919ab34bcc4a3d17cde7a89b
tylerapfledderer <tyler.pfledderer@gmail.com>
2023-02-24 18:39:13 -0500

    feat(vue): add PinInput to vue

---
+++ packages/vue/src/pin-input/pin-input-context.ts
@@ -0,0 +1,9 @@
+import type { connect } from '@zag-js/pin-input'
+import type { ComputedRef } from 'vue'
+import { createContext } from '../context'
+import type { UsePinInputReturn } from './use-pin-input'
+
+export const [PinInputProvider, usePinInputContext] =
+  createContext<ComputedRef<ReturnType<typeof connect>>>('PinInputContext')
+
+export type PinInputContext = UsePinInputReturn
