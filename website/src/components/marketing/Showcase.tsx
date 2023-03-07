b08348f20ef0f8ba724094d91ccf53d87854493f
Christian Schröter <dev@grizzly.codes>
2023-03-07 13:14:14 +0100

    feat(website): improve landing page showcase

--- website/src/components/marketing/Showcase.tsx
+++ website/src/components/marketing/Showcase.tsx
@@ -1,9 +1,11 @@
 'use client'
-import { css } from '@/panda/css'
+import { css, cx } from '@/panda/css'
 import { Box, Container, HStack, Stack } from '@/panda/jsx'
 import { tabs } from '@/panda/recipes'
 import {
   Carousel,
+  CarouselNextSlideTrigger,
+  CarouselPrevSlideTrigger,
   CarouselSlide,
   CarouselSlideGroup,
   CarouselViewport,
@@ -21,53 +23,54 @@ export const Showcase = () => {
   const [index, setIndex] = useState(0)
   return (
     <Container py={{ base: '12', md: '16' }}>
-      <Stack direction="column" gap="8" width="full" overflow="hidden">
-        <Carousel
-          className={css({ width: 'full' })}
-          index={index}
-          onSlideChange={({ index }) => setIndex(index)}
-          slidesPerView={2}
-        >
-          <CarouselViewport className={css({ overflowX: 'hidden' })}>
-            <CarouselSlideGroup>
-              {components.map((component, id) => (
-                <CarouselSlide key={component} index={id}>
-                  <Box
-                    background="bg.surface"
-                    borderWidth="1px"
-                    borderRadius="lg"
-                    minH="400px"
-                    p="6"
-                  >
-                    {component} Demo
-                  </Box>
-                </CarouselSlide>
-              ))}
-            </CarouselSlideGroup>
-          </CarouselViewport>
+      <Box maxW={{ lg: '4xl' }}>
+        <Carousel index={index} onSlideChange={({ index }) => setIndex(index)}>
+          <Stack gap="8">
+            <CarouselViewport
+              className={css({
+                background: 'bg.surface',
+                borderWidth: '1px',
+                borderRadius: 'lg',
+                minH: '400px',
+                overflowX: 'hidden',
+                width: 'full',
+              })}
+            >
+              <CarouselSlideGroup>
+                {components.map((component, id) => (
+                  <CarouselSlide key={component} index={id}>
+                    <Box p="6">{component} Demo</Box>
+                  </CarouselSlide>
+                ))}
+              </CarouselSlideGroup>
+            </CarouselViewport>
+            <Stack direction="row" justify="space-between" width="full">
+              <Tabs
+                className={cx(tabs({}), css({ display: { base: 'none', sm: 'block' } }))}
+                value={components[index]}
+                onChange={({ value }) => setIndex(components.indexOf(value ?? ''))}
+              >
+                <TabList>
+                  {components.map((component) => (
+                    <TabTrigger key={component} value={component}>
+                      <button>{component}</button>
+                    </TabTrigger>
+                  ))}
+                  <TabIndicator />
+                </TabList>
+              </Tabs>
+              <HStack gap="3" justify="space-between" flex="1">
+                <CarouselPrevSlideTrigger>
+                  <IconButton icon={<FiArrowLeft />} aria-label="Previous component" />
+                </CarouselPrevSlideTrigger>
+                <CarouselNextSlideTrigger>
+                  <IconButton icon={<FiArrowRight />} aria-label="Next component" />
+                </CarouselNextSlideTrigger>
+              </HStack>
+            </Stack>
+          </Stack>
         </Carousel>
-
-        <Stack direction="row" justify="space-between" width="full">
-          <Tabs
-            className={tabs({})}
-            defaultValue="Dialog"
-            onChange={({ value }) => setIndex(components.indexOf(value ?? ''))}
-          >
-            <TabList>
-              {components.map((component) => (
-                <TabTrigger key={component} value={component}>
-                  <button>{component}</button>
-                </TabTrigger>
-              ))}
-              <TabIndicator />
-            </TabList>
-          </Tabs>
-          <HStack gap="3">
-            <IconButton icon={<FiArrowLeft />} aria-label="Previous component" />
-            <IconButton icon={<FiArrowRight />} aria-label="Next component" />
-          </HStack>
-        </Stack>
-      </Stack>
+      </Box>
     </Container>
   )
 }
