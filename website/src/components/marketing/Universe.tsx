5fd1d98d4b8cad800d7ac18a1c43e1f3103b03d1
Christian Schröter <dev@grizzly.codes>
2023-03-08 16:21:14 +0100

    feat(website): improve universe section

--- website/src/components/marketing/Universe.tsx
+++ website/src/components/marketing/Universe.tsx
@@ -4,61 +4,79 @@ import { ReactIcon } from '@/icons/React'
 import { SolidIcon } from '@/icons/Solid'
 import { VueIcon } from '@/icons/Vue'
 import { Box, Container, Flex, FlexProps, panda, Stack } from '@/panda/jsx'
+import { FiArrowRight } from 'react-icons/fi'
 import { match } from 'ts-pattern'
 import { Button } from '../shared/Button'
 import { Heading } from '../shared/Heading'
 import { Text } from '../shared/Text'
 
-export const Universe = () => {
-  return (
-    <Box background="url(/images/pattern.svg) repeat-x">
-      <Container pt="64" pb={{ base: '16', md: '24' }}>
-        <Stack align="center" gap="10">
-          <Heading textAlign="center" textStyle={{ base: '3xl', md: '4xl' }} fontWeight="semibold">
-            Accessible components. <br />
-            Powered by <panda.span color="accent.default">State Machines</panda.span>
-          </Heading>
-          <Stack direction="row" align="center" gap="10" width="full" justify="space-between">
-            {[
-              { framework: 'React', icon: <ReactIcon width="48" /> },
-              { framework: 'Vue', icon: <VueIcon width="48" /> },
-              { framework: 'Ark', icon: <ArkIcon width="48" /> },
-              { framework: 'Solid', icon: <SolidIcon width="48" /> },
-              { framework: 'JS', icon: <JSIcon width="42" /> },
-            ].map((item, id) =>
-              match(item)
-                .with({ framework: 'Ark' }, () => <ArkOrb key={id} />)
-                .otherwise(() => (
-                  <Circle key={id} bg="#220C08" borderColor="orange.900" width="24" height="24">
-                    {item.icon}
-                  </Circle>
-                )),
-            )}
-          </Stack>
-          <Text color="fg.muted" textStyle={{ base: 'lg', md: 'xl' }} textAlign="center" maxW="lg">
-            Ark UI is an open-source library that allows developers to build components faster
-            without sweating on functionality.
-          </Text>
-          <Button href="/docs/react/overview/introduction" size={{ base: 'lg', md: 'xl' }}>
-            Get Started
-          </Button>
+export const Universe = () => (
+  <Box
+    backgroundImage={{ base: 'none', md: 'url(/images/pattern.svg)' }}
+    _dark={{ backgroundImage: { base: 'none', md: 'url(/images/pattern_dark.svg)' } }}
+    backgroundRepeat="repeat-x"
+  >
+    <Container pt={{ base: '16', md: '40' }}>
+      <Heading textAlign="center" textStyle={{ base: '3xl', md: '4xl' }} fontWeight="semibold">
+        Accessible components. <br />
+        Powered by <panda.span color="accent.default">State Machines</panda.span>
+      </Heading>
+    </Container>
+    <Box
+      backgroundImage="url(/images/universe.svg)"
+      _dark={{ backgroundImage: 'url(/images/universe_dark.svg)' }}
+      backgroundPosition="center center"
+      backgroundRepeat="repeat-x"
+    >
+      <Container pt={{ base: '8', md: '10' }} pb={{ base: '8', md: '10' }}>
+        <Stack direction="row" align="center" width="full" justify="space-evenly">
+          {[
+            { framework: 'React', icon: <ReactIcon width="48" /> },
+            { framework: 'Vue', icon: <VueIcon width="48" /> },
+            { framework: 'Ark', icon: <ArkIcon width="48" /> },
+            { framework: 'Solid', icon: <SolidIcon width="48" /> },
+            { framework: 'JS', icon: <JSIcon width="42" /> },
+          ].map((item, id) =>
+            match(item)
+              .with({ framework: 'Ark' }, () => <Box key={id} width="358px" height="358px" />)
+              .otherwise(() => <Orb key={id}>{item.icon}</Orb>),
+          )}
         </Stack>
       </Container>
     </Box>
-  )
-}
-
-const ArkOrb = () => (
-  <Circle bg="orange.800" width="72" height="72" borderColor="orange.800">
-    <Circle bg="orange.700" width="60" height="60" borderColor="rgba(255, 255, 255, 0.03)">
-      <Circle bg="orange.600" width="44" height="44" borderColor="rgba(255, 255, 255, 0.07)">
-        <Circle bg="orange.500" width="24" height="24" borderColor="rgba(255, 255, 255, 0.21)">
-          <ArkIcon width="44" />
-        </Circle>
-      </Circle>
-    </Circle>
-  </Circle>
+    <Container pb={{ base: '16', md: '24' }}>
+      <Stack align="center" gap={{ base: '8', md: '10' }}>
+        <Text color="fg.muted" textStyle={{ base: 'lg', md: 'xl' }} textAlign="center" maxW="lg">
+          Ark UI is an open-source library that allows developers to build components faster without
+          sweating on functionality.
+        </Text>
+        <Button
+          href="/docs/react/overview/introduction"
+          size={{ base: 'lg', md: 'xl' }}
+          rightIcon={<FiArrowRight />}
+        >
+          Get Started
+        </Button>
+      </Stack>
+    </Container>
+  </Box>
 )
-const Circle = (props: FlexProps) => (
-  <Flex borderRadius="full" justify="center" align="center" borderWidth="2px" {...props} />
+
+const Orb = (props: FlexProps) => (
+  <Flex
+    borderRadius="full"
+    justify="center"
+    align="center"
+    borderWidth="2px"
+    bg="orange.900"
+    borderColor="orange.800"
+    _dark={{
+      bg: '#220C08',
+      borderColor: 'orange.800',
+    }}
+    width="94px"
+    height="94px"
+    display={{ base: 'none', md: 'flex' }}
+    {...props}
+  />
 )
