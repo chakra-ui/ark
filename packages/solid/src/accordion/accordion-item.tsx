829b8d9ee1d6607937b4647ce46aa39571b0c1af
Abraham <anubra266@gmail.com>
2023-03-27 14:27:51 +0100

    Accordion item render props (#681)

--- packages/solid/src/accordion/accordion-item.tsx
+++ packages/solid/src/accordion/accordion-item.tsx
@@ -1,26 +1,32 @@
 import { type Assign } from '@polymorphic-factory/solid'
-import { splitProps, type JSX } from 'solid-js'
+import { children, splitProps, type JSX } from 'solid-js'
 import { ark, type HTMLArkProps } from '../factory'
-import { useAccordionContext } from './accordion-context'
+import { runIfFn } from '../run-if-fn'
+import { useAccordionContext, type AccordionContext } from './accordion-context'
 import { AccordionItemProvider } from './accordion-item-context'
+
+export type AccordionItemState = () => ReturnType<ReturnType<AccordionContext>['getItemState']>
 
 export type AccordionItemProps = Assign<
   HTMLArkProps<'div'>,
   {
     value: string
     disabled?: boolean
-    children?: JSX.Element
-    // | ((props: ReturnType<AccordionContext['getItemState']>) => JSX.Element) -> TODO enable render prop fn
+    children?: JSX.Element | ((state: AccordionItemState) => JSX.Element)
   }
 >
 
 export const AccordionItem = (props: AccordionItemProps) => {
   const [itemProps, divProps] = splitProps(props, ['value', 'disabled'])
   const accordion = useAccordionContext()
+  const view = () =>
+    children(() => runIfFn(divProps.children, () => accordion().getItemState(props)))
 
   return (
     <AccordionItemProvider value={itemProps}>
-      <ark.div {...accordion().getItemProps(itemProps)} {...divProps} />
+      <ark.div {...accordion().getItemProps(itemProps)} {...divProps}>
+        {view}
+      </ark.div>
     </AccordionItemProvider>
   )
 }
