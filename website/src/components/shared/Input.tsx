891ba59cf09526a3c0aaa274b4ae235bdeae8598
Christian Schröter <dev@grizzly.codes>
2023-04-25 20:05:43 +0200

    chore: fix types

--- website/src/components/shared/Input.tsx
+++ website/src/components/shared/Input.tsx
@@ -1,7 +1,7 @@
 import { panda, type HTMLPandaProps } from '@/panda/jsx'
-import { input, type InputVariants } from '@/panda/recipes'
+import { input, type InputVariantProps } from '@/panda/recipes'
 
-export type InputProps = Omit<HTMLPandaProps<'input'>, 'size'> & InputVariants
+export type InputProps = Omit<HTMLPandaProps<'input'>, 'size'> & InputVariantProps
 
 export const Input = (props: InputProps) => {
   const { variant = 'outline', size = 'md', ...rest } = props
