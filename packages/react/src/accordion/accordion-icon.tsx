5f01dac4a8d3a701a44f95c981b9c8ce66a099cc
Segun Adebayo <joseshegs@gmail.com>
2023-04-26 20:20:00 +0100

    chore: update accordion icon

--- packages/react/src/accordion/accordion-icon.tsx
+++ packages/react/src/accordion/accordion-icon.tsx
@@ -1,8 +1,18 @@
 import { ark, type HTMLArkProps } from '../factory'
 import { forwardRef } from '../forward-ref'
+import { useAccordionItemContext } from './accordion-item-context'
 
-export type AccordionIconProps = HTMLArkProps<'div'> & { foo: string }
+export type AccordionIconProps = HTMLArkProps<'div'>
 
-export const AccordionIcon = forwardRef<'div', AccordionIconProps>((props, ref) => {
-  return <ark.div {...props} ref={ref} />
+export const AccordionIcon = forwardRef<'div'>((props, ref) => {
+  const { isOpen, isDisabled, isFocused } = useAccordionItemContext()
+  return (
+    <ark.div
+      data-open={isOpen || undefined}
+      data-disabled={isDisabled || undefined}
+      data-focused={isFocused || undefined}
+      {...props}
+      ref={ref}
+    />
+  )
 })
