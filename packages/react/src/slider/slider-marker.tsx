a608fd2e300fb4fd655626e4b6690b6ca89576a2
Segun Adebayo <joseshegs@gmail.com>
2023-04-26 20:14:02 +0100

    refactor: slider

--- packages/react/src/slider/slider-marker.tsx
+++ packages/react/src/slider/slider-marker.tsx
@@ -1,14 +1,17 @@
-import { forwardRef, type Assign } from '@polymorphic-factory/react'
 import { mergeProps } from '@zag-js/react'
 import { ark, type HTMLArkProps } from '../factory'
+import { forwardRef } from '../forward-ref'
 import { useSliderContext } from './slider-context'
 
-export type SliderMarkerProps = Assign<HTMLArkProps<'span'>, { value: number }>
+type MarkerProps = {
+  value: number
+}
 
-export const SliderMarker = forwardRef<'span', SliderMarkerProps>((props, ref) => {
+export type SliderMarkerProps = HTMLArkProps<'span', MarkerProps>
+
+export const SliderMarker = forwardRef<'span', MarkerProps>((props, ref) => {
   const { value, ...spanProps } = props
   const { getMarkerProps } = useSliderContext()
   const mergedProps = mergeProps(getMarkerProps({ value }), spanProps)
-
   return <ark.span {...mergedProps} ref={ref} />
 })
