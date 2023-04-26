2cdd0301a21ce63252eb2e213ace0099050df166
Christian Schröter <dev@grizzly.codes>
2023-04-26 17:02:59 +0200

    feat(react): add new factory

--- packages/react/src/factory.tsx
+++ packages/react/src/factory.tsx
@@ -1,19 +1,68 @@
-/**
- * All html and svg elements for ark components.
- * This is mostly for `ark.<element>` syntax.
- */
-import {
-  polymorphicFactory,
-  type HTMLPolymorphicComponents,
-  type HTMLPolymorphicProps,
-} from '@polymorphic-factory/react'
-import { type ElementType } from 'react'
+import { mergeProps } from '@zag-js/react'
+import { Children, forwardRef as __forwardRef, cloneElement, isValidElement } from 'react'
+import { composeRefs } from './compose-refs'
 
-export type HTMLArkComponents = HTMLPolymorphicComponents
+type AsChildProps<Props extends { children?: unknown }> =
+  | { asChild: true; children: React.ReactElement }
+  | { asChild?: false; children?: Props['children'] }
 
-export type HTMLArkProps<T extends ElementType> = HTMLPolymorphicProps<T>
+export type WithAsChildProps<Props extends { children?: unknown }> = Omit<Props, 'children'> &
+  AsChildProps<Props>
 
-/**
- * The ark factory serves as an object of JSX elements to render React components which accept the `as` prop
- */
-export const ark = polymorphicFactory()
+type ComponentWithAsChildProps<T extends React.ElementType> = React.FC<
+  WithAsChildProps<React.ComponentPropsWithRef<T>>
+>
+
+type JsxFactoryFn<T extends React.ElementType = React.ElementType> = (
+  component: T,
+) => ComponentWithAsChildProps<T>
+
+type JsxElements = {
+  [K in keyof JSX.IntrinsicElements]: ComponentWithAsChildProps<K>
+}
+
+const withAsChild = (Component: React.ElementType) => {
+  const Comp = __forwardRef<unknown, AsChildProps<React.ComponentPropsWithRef<React.ElementType>>>(
+    function ComponentWithAsChild(props, ref) {
+      const { asChild, ...restProps } = props
+
+      if (!asChild) {
+        return <Component {...restProps} ref={ref} />
+      }
+
+      const onlyChild = Children.only(props.children)
+      const forwardedRef = composeRefs(ref, (onlyChild as any).ref)
+
+      return isValidElement(onlyChild)
+        ? cloneElement(onlyChild, {
+            ...mergeProps(restProps, onlyChild.props as any),
+            ref: forwardedRef,
+          })
+        : null
+    },
+  )
+
+  // @ts-expect-error - it exists
+  Comp.displayName = Component.displayName || Component.name
+
+  return Comp
+}
+
+const jsxFactory = () => {
+  const cache = new Map()
+
+  return new Proxy(withAsChild, {
+    apply(_target, _thisArg, argArray) {
+      return withAsChild(argArray[0])
+    },
+    get(_, element) {
+      const asElement = element as React.ElementType
+      if (!cache.has(asElement)) {
+        cache.set(asElement, withAsChild(asElement))
+      }
+      return cache.get(asElement)
+    },
+  }) as unknown as JsxFactoryFn & JsxElements
+}
+
+export const ark = jsxFactory()
