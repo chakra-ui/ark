198625c3ddef0425c697f10b52fbc8995252ca81
tylerapfledderer <tyler.pfledderer@gmail.com>
2023-05-06 18:31:44 -0400

    test(vue/environment): render test component via template

--- packages/vue/src/environment/environment.test.tsx
+++ packages/vue/src/environment/environment.test.tsx
@@ -1,21 +1,19 @@
 import { render } from '@testing-library/vue'
-import { defineComponent } from 'vue'
 import { Environment } from './environment'
 import PrintEnvironment from './stories/print-environment.vue'
 
-const ComponentUnderTest = defineComponent({
-  setup() {
-    return () => (
-      <Environment value={document}>
-        <PrintEnvironment />
-      </Environment>
-    )
-  },
-})
-
 describe('Environment', () => {
   it('should have access to the environment values', () => {
-    const { getByLabelText } = render(ComponentUnderTest)
+    const { getByLabelText, debug, baseElement } = render({
+      components: { Environment, PrintEnvironment },
+      template: `
+        <Environment :value='document'>
+          <PrintEnvironment />
+        </Environment>
+      `,
+    })
+
+    debug(baseElement)
 
     expect(getByLabelText('environment values').innerHTML).not.toBe('""')
   })
