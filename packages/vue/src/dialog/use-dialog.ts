f6b6cd53c1ad37a1cc7c784a2af2fc9174c28ee8
Tyler Pfledderer <tyler.pfledderer@gmail.com>
2023-05-06 22:01:50 -0400

    refactor(vue/dialog): add two-way binding to `open` prop (#898)

--- packages/vue/src/dialog/use-dialog.ts
+++ packages/vue/src/dialog/use-dialog.ts
@@ -1,6 +1,6 @@
 import { connect, machine, type Context } from '@zag-js/dialog'
 import { normalizeProps, useMachine } from '@zag-js/vue'
-import { computed, reactive } from 'vue'
+import { computed, reactive, watch } from 'vue'
 import { useId } from '../utils'
 
 export type UseDialogContext = Omit<Context, 'id'>
@@ -12,8 +12,13 @@ export const useDialog = (emit: CallableFunction, context: UseDialogContext) => 
     machine({
       ...reactiveContext,
       id: useId().value,
+      onOpen() {
+        emit('open')
+        emit('update:open', true)
+      },
       onClose() {
         emit('close')
+        emit('update:open', false)
       },
       onEsc() {
         emit('esc')
@@ -25,6 +30,18 @@ export const useDialog = (emit: CallableFunction, context: UseDialogContext) => 
   )
 
   const api = computed(() => connect(state.value, send, normalizeProps))
+
+  watch(
+    () => reactiveContext.open,
+    (value) => {
+      if (value == undefined) return
+      if (value) {
+        api.value.open()
+      } else {
+        api.value.close()
+      }
+    },
+  )
 
   return api
 }
