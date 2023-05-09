f38753f4c00de7c9f230b671f81b2a5018088cd4
Christian Schröter <dev@grizzly.codes>
2023-05-09 22:00:49 +0200

    feat: improve pagination (#922)

--- packages/solid/src/pagination/pagination.stories.tsx
+++ packages/solid/src/pagination/pagination.stories.tsx
@@ -1,8 +1,9 @@
-import { For } from 'solid-js'
+import { For, createSignal } from 'solid-js'
 import {
   Pagination,
   PaginationEllipsis,
   PaginationList,
+  PaginationListItem,
   PaginationNextPageTrigger,
   PaginationPageTrigger,
   PaginationPrevPageTrigger,
@@ -13,29 +14,71 @@ export const Basic = () => (
   <Pagination count={5000} pageSize={10} siblingCount={2}>
     {({ pages }) => (
       <PaginationList>
-        <PaginationPrevPageTrigger>
-          <button>
-            Previous <span class="visually-hidden">Page</span>
-          </button>
-        </PaginationPrevPageTrigger>
+        <PaginationListItem>
+          <PaginationPrevPageTrigger>
+            <button>
+              Previous <span class="visually-hidden">Page</span>
+            </button>
+          </PaginationPrevPageTrigger>
+        </PaginationListItem>
 
         <For each={pages}>
           {(page, index) =>
             page.type === 'page' ? (
-              <PaginationPageTrigger value={page.value}>
-                <button>{page.value}</button>
-              </PaginationPageTrigger>
+              <PaginationListItem>
+                <PaginationPageTrigger value={page.value}>
+                  <button>{page.value}</button>
+                </PaginationPageTrigger>
+              </PaginationListItem>
             ) : (
-              <PaginationEllipsis index={index()}>&#8230;</PaginationEllipsis>
+              <PaginationListItem>
+                <PaginationEllipsis index={index()}>&#8230;</PaginationEllipsis>
+              </PaginationListItem>
             )
           }
         </For>
-        <PaginationNextPageTrigger>
-          <button>
-            Next <span class="visually-hidden">Page</span>
-          </button>
-        </PaginationNextPageTrigger>
+        <PaginationListItem>
+          <PaginationNextPageTrigger>
+            <button>
+              Next <span class="visually-hidden">Page</span>
+            </button>
+          </PaginationNextPageTrigger>
+        </PaginationListItem>
       </PaginationList>
     )}
   </Pagination>
 )
+
+export const Controlled = () => {
+  const [currentPage, setCurrentPage] = createSignal(1)
+
+  return (
+    <Pagination
+      count={5000}
+      pageSize={10}
+      siblingCount={2}
+      page={currentPage()}
+      onChange={(details) => setCurrentPage(details.page)}
+    >
+      {/* ... */}
+    </Pagination>
+  )
+}
+
+export const Customized = () => {
+  return (
+    <Pagination
+      count={5000}
+      pageSize={20}
+      siblingCount={3}
+      dir="ltr"
+      translations={{
+        nextPageTriggerLabel: 'Next',
+        prevPageTriggerLabel: 'Prev',
+        pageTriggerLabel: (details) => `Page ${details.page}`,
+      }}
+    >
+      {/* ... */}
+    </Pagination>
+  )
+}
--- packages/vue/src/pagination/index.ts
+++ packages/vue/src/pagination/index.ts
@@ -1,6 +1,7 @@
 export { Pagination, type PaginationProps } from './pagination'
 export { PaginationEllipsis, type PaginationEllipsisProps } from './pagination-ellipsis'
 export { PaginationList, type PaginationListProps } from './pagination-list'
+export { PaginationListItem, type PaginationListItemProps } from './pagination-list-item'
 export {
   PaginationNextPageTrigger,
   type PaginationNextPageTriggerProps,
