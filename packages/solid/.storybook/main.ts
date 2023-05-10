d69046d7c3fe6de94124b16477cffe8fd0ddeb57
Segun Adebayo <joseshegs@gmail.com>
2023-05-10 12:48:18 +0100

    chore: update

--- packages/solid/.storybook/main.ts
+++ packages/solid/.storybook/main.ts
@@ -1,6 +1,6 @@
 import type { StorybookConfig } from 'storybook-solidjs-vite'
 const config: StorybookConfig = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
