960c84b7be580f0033eabc1041056981f33b4d54
Christian Schröter <dev@grizzly.codes>
2023-05-11 18:12:20 +0200

    feat(solid): use mergeProps from zag-js (#936)

--- packages/solid/src/menu/use-menu.ts
+++ packages/solid/src/menu/use-menu.ts
@@ -1,6 +1,6 @@
 import * as menu from '@zag-js/menu'
-import { normalizeProps, useMachine } from '@zag-js/solid'
-import { createMemo, createUniqueId, mergeProps } from 'solid-js'
+import { mergeProps, normalizeProps, useMachine } from '@zag-js/solid'
+import { createMemo, createUniqueId } from 'solid-js'
 import { useEnvironmentContext } from '../environment'
 import { type Optional } from '../types'
 
