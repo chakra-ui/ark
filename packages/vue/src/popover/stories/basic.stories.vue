70d57d0ed94f5ab381327cd1baf56025ada0b420
codebender828 <codebender828@gmail.com>
2023-05-13 04:11:17 +0800

    feat: create as-child for vue

--- packages/vue/src/popover/stories/basic.stories.vue
+++ packages/vue/src/popover/stories/basic.stories.vue
@@ -16,7 +16,7 @@ import {
     <PopoverTrigger>
       <button>click me</button>
     </PopoverTrigger>
-    <PopoverPositioner as-child>
+    <PopoverPositioner>
       <PopoverContent>
         <PopoverArrow>
           <PopoverArrowTip />
