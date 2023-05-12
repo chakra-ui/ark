d3547375f6ad63949df67fde88b628f0ce26ad2a
Christian Schröter <dev@grizzly.codes>
2023-05-12 15:50:44 +0200

    chore: wip

--- packages/vue/src/color-picker/color-picker.anatomy.ts
+++ packages/vue/src/color-picker/color-picker.anatomy.ts
@@ -1,21 +1,4 @@
-import { createAnatomy } from '@zag-js/anatomy'
-// TODO replace with anatomy from @zag-js/color-picker
-// import { anatomy } from '@zag-js/color-picker'
-
-const anatomy = createAnatomy('color-picker', [
-  'area',
-  'areaThumb',
-  'areaGradient',
-  'channelSliderTrack',
-  'channelSliderTrackBg',
-  'channelSliderThumb',
-  'channelInput',
-  'swatch',
-  'swatchBg',
-  'content',
-  'label',
-  'eyeDropTrigger',
-])
+import { anatomy } from '@zag-js/color-picker'
 
 export const colorPickerAnatomy = anatomy.extendWith('swatchGroup')
 export const parts = colorPickerAnatomy.build()
