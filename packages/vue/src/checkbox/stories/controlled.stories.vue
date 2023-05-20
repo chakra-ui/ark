e3fa791b3c158aa3c23e4990b6873bb6f5393047
codebender828 <codebender828@gmail.com>
2023-05-20 21:50:17 +0800

    fix(vue): resolve all linting errors

--- packages/vue/src/checkbox/stories/controlled.stories.vue
+++ packages/vue/src/checkbox/stories/controlled.stories.vue
@@ -1,17 +1,9 @@
 <script setup lang="ts">
-import { ref, watch } from 'vue'
+import { ref } from 'vue'
 import { Checkbox, CheckboxControl, CheckboxInput, CheckboxLabel } from '..'
 import '../checkbox.css'
 
 const checked = ref(true)
-
-watch(checked, (newValue) => {
-  console.log('checked:value', newValue)
-})
-
-setInterval(() => {
-  checked.value = !checked.value
-}, 2000)
 </script>
 <template>
   <Checkbox v-model="checked">
