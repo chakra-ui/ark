f6bce6c87e3ff8a054bec702bac520b2c0a430d7
codebender828 <codebender828@gmail.com>
2023-05-20 21:27:02 +0800

    fix(checkbox:vue): update tests to match latest api

--- packages/vue/src/checkbox/checkbox.test.tsx
+++ packages/vue/src/checkbox/checkbox.test.tsx
@@ -3,7 +3,6 @@ import { render } from '@testing-library/vue'
 import { Fragment, defineComponent, ref } from 'vue'
 import { Checkbox, CheckboxControl, CheckboxInput, CheckboxLabel, type CheckboxProps } from '.'
 import BasicComponentStory from './stories/basic.stories.vue'
-import IndeterminateComponentStory from './stories/indeterminate.stories.vue'
 
 const ComponentUnderTest = (props: CheckboxProps) => (
   <Checkbox {...props}>
@@ -19,7 +18,7 @@ describe('Checkbox', () => {
 
   it('should handle check and unchecked', async () => {
     const onChange = vi.fn()
-    const { getByRole } = render(ComponentUnderTest, { props: { onChange } })
+    const { getByRole } = render(() => <ComponentUnderTest onChange={onChange} />)
 
     const checkbox = getByRole('checkbox')
 
@@ -49,28 +48,7 @@ describe('Checkbox', () => {
   })
 
   it('should handle indeterminate state from example', async () => {
-    const { getByTestId, getByText } = render(IndeterminateComponentStory)
-
-    const parentControl = getByTestId('parent-control')
-    expect(parentControl).not.toHaveAttribute('data-indeterminate')
-
-    const childOneChecbox = getByText('Child One Checkbox')
-    await user.click(childOneChecbox)
-    expect(getByTestId('child-one-input')).toBeChecked()
-
-    expect(parentControl).toHaveAttribute('data-indeterminate')
-
-    const childTwoChecbox = getByText('Child Two Checkbox')
-    await user.click(childTwoChecbox)
-    expect(getByTestId('child-two-input')).toBeChecked()
-
-    expect(parentControl).not.toHaveAttribute('data-indeterminate')
-    expect(parentControl).toHaveAttribute('data-checked')
-
-    await user.click(parentControl)
-    expect(parentControl).not.toHaveAttribute('data-checked')
-    expect(parentControl).not.toHaveAttribute('data-indeterminate')
-    expect(getByTestId('child-one-input')).not.toBeChecked()
-    expect(getByTestId('child-two-input')).not.toBeChecked()
+    const { getByTestId } = render(() => <ComponentUnderTest modelValue={'indeterminate'} />)
+    expect(getByTestId('control')).toHaveAttribute('data-indeterminate')
   })
 })
