980df94314b28b51e56246525f12f6fb5ffe2946
Christian Schröter <dev@grizzly.codes>
2023-06-03 23:10:55 +0200

    ci: update slack script

--- scripts/slack.ts
+++ scripts/slack.ts
@@ -2,6 +2,12 @@ import fetch from 'node-fetch'
 import yargs from 'yargs'
 
 const main = async () => {
+  const url = process.env.SLACK_WEBHOOK_URL
+
+  if (!url) {
+    throw new Error('Missing Slack Webhook URL')
+  }
+
   const argv = yargs(process.argv.slice(2))
     .options({
       p: { type: 'string' },
@@ -9,20 +15,18 @@ const main = async () => {
       c: { type: 'string' },
     })
     .parseSync()
-  fetch(
-    'https://hooks.slack.com/workflows/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',
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        package_name: argv.p,
-        version: argv.v,
-        changelog: argv.c,
-      }),
+
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
     },
-  )
+    body: JSON.stringify({
+      package_name: argv.p,
+      version: argv.v,
+      changelog: argv.c,
+    }),
+  })
 }
 
 main()
