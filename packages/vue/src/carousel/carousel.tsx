16b46e6527980a7951177d98d70e2defb47d8faf
tylerapfledderer <tyler.pfledderer@gmail.com>
2023-06-07 13:25:09 -0400

    refactor(vue/carousel): use createVueProps

--- packages/vue/src/carousel/carousel.tsx
+++ packages/vue/src/carousel/carousel.tsx
@@ -1,46 +1,48 @@
 import { defineComponent, type PropType } from 'vue'
 import { ark, type HTMLArkProps } from '../factory'
 import type { Assign } from '../types'
-import type { ComponentWithProps } from '../utils'
+import { createVueProps, type ComponentWithProps } from '../utils'
 import { CarouselProvider } from './carousel-context'
 import { useCarousel, type UseCarouselContext } from './use-carousel'
 
 export type CarouselProps = Assign<HTMLArkProps<'div'>, UseCarouselContext>
 
+const VueCarouselProps = createVueProps<CarouselProps>({
+  align: {
+    type: String as PropType<CarouselProps['align']>,
+  },
+  dir: {
+    type: String as PropType<CarouselProps['dir']>,
+  },
+  getRootNode: {
+    type: Function as PropType<CarouselProps['getRootNode']>,
+  },
+  id: {
+    type: String as PropType<CarouselProps['id']>,
+  },
+  index: {
+    type: Number as PropType<CarouselProps['index']>,
+  },
+  loop: {
+    type: Boolean as PropType<CarouselProps['loop']>,
+  },
+  orientation: {
+    type: String as PropType<CarouselProps['orientation']>,
+  },
+  slidesPerView: {
+    type: Number as PropType<CarouselProps['slidesPerView']>,
+  },
+  spacing: {
+    type: String as PropType<CarouselProps['spacing']>,
+  },
+})
+
 export const Carousel: ComponentWithProps<CarouselProps> = defineComponent({
   name: 'Carousel',
-  props: {
-    align: {
-      type: String as PropType<CarouselProps['align']>,
-    },
-    dir: {
-      type: String as PropType<CarouselProps['dir']>,
-    },
-    getRootNode: {
-      type: Function as PropType<CarouselProps['getRootNode']>,
-    },
-    id: {
-      type: String as PropType<CarouselProps['id']>,
-    },
-    index: {
-      type: Number as PropType<CarouselProps['index']>,
-    },
-    loop: {
-      type: Boolean as PropType<CarouselProps['loop']>,
-    },
-    orientation: {
-      type: String as PropType<CarouselProps['orientation']>,
-    },
-    slidesPerView: {
-      type: Number as PropType<CarouselProps['slidesPerView']>,
-    },
-    spacing: {
-      type: String as PropType<CarouselProps['spacing']>,
-    },
-  },
+  props: VueCarouselProps,
   emits: ['slide-change'],
   setup(props, { slots, attrs, emit }) {
-    const api = useCarousel(emit, props)
+    const api = useCarousel(emit, props as CarouselProps)
 
     CarouselProvider(api)
 
