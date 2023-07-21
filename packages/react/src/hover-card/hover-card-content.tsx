6b62d5417fdfff70ad164886d9f8d14dcbabf823
Christian Schröter <dev@grizzly.codes>
2023-07-21 20:29:37 +0200

    feat(react): add lazyMount and unmountOnExit to HoverCardContent

--- packages/react/src/hover-card/hover-card-content.tsx
+++ packages/react/src/hover-card/hover-card-content.tsx
@@ -1,12 +1,24 @@
 import { mergeProps } from '@zag-js/react'
 import { ark, type HTMLArkProps } from '../factory'
 import { forwardRef } from '../forward-ref'
+import { splitPresenceProps } from '../presence'
 import { useHoverCardContext } from './hover-card-context'
+import { HoverCardPresence, type HoverCardPresenceProps } from './hover-card-presence'
 
-export type HoverCardContentProps = HTMLArkProps<'div'>
+export type HoverCardContentProps = HTMLArkProps<'div'> & Omit<HoverCardPresenceProps, 'children'>
 
-export const HoverCardContent = forwardRef<'div'>((props, ref) => {
+export const HoverCardContent = forwardRef<'div', HoverCardContentProps>((props, ref) => {
+  const [presenceProps, hoverCardContentProps] = splitPresenceProps(props)
+  return (
+    <HoverCardPresence {...presenceProps}>
+      <InnerHoverCardContent ref={ref} {...hoverCardContentProps} />
+    </HoverCardPresence>
+  )
+})
+
+const InnerHoverCardContent = forwardRef<'div', HTMLArkProps<'div'>>((props, ref) => {
   const { contentProps } = useHoverCardContext()
   const mergedProps = mergeProps(contentProps, props)
+
   return <ark.div {...mergedProps} ref={ref} />
 })
