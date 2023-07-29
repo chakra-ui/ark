0a8967db49e07218dddece69a1763f521724de5a
Christian Schröter <dev@grizzly.codes>
2023-07-29 20:23:09 +0200

    chore: fix typecheck in react slider test

--- packages/react/src/slider/slider-output.tsx
+++ packages/react/src/slider/slider-output.tsx
@@ -2,11 +2,13 @@ import { mergeProps } from '@zag-js/react'
 import { forwardRef, type ComponentPropsWithoutRef } from 'react'
 import { ark } from '../factory'
 import { runIfFn } from '../run-if-fn'
+import type { Assign } from '../types'
 import { useSliderContext, type SliderContext } from './slider-context'
 
-export type SliderOutputProps = {
-  children?: ((context: SliderContext) => React.ReactNode) | React.ReactNode
-} & ComponentPropsWithoutRef<typeof ark.output>
+export type SliderOutputProps = Assign<
+  ComponentPropsWithoutRef<typeof ark.output>,
+  { children?: ((context: SliderContext) => React.ReactNode) | React.ReactNode }
+>
 
 export const SliderOutput = forwardRef<HTMLOutputElement, SliderOutputProps>((props, ref) => {
   const { children, ...restProps } = props
