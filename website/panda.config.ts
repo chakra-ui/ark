5cd0229d98daf608ad1c1275b983de58f1db6404
Christian Schröter <dev@grizzly.codes>
2023-08-08 20:15:35 +0200

    fix(website): resolve an issue with container padding

--- website/panda.config.ts
+++ website/panda.config.ts
@@ -168,20 +168,15 @@ export default defineConfig({
   patterns: {
     extend: {
       container: {
-        transform(props: any) {
-          // TODO descructuring props.centerContent is not working @panda
-          delete props.centerContent
-
-          return Object.assign(
-            {
-              position: 'relative',
-              width: '100%',
-              maxWidth: '7xl',
-              mx: 'auto',
-              paddingX: { base: '4', md: '6' },
-            },
-            props,
-          )
+        transform(props: ContainerProperties) {
+          return {
+            position: 'relative',
+            width: '100%',
+            maxWidth: '7xl',
+            mx: 'auto',
+            paddingX: { base: '4', md: '6' },
+            ...props,
+          }
         },
       },
     },
