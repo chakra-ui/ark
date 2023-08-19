dc67cbd8bb5ac35cb61ae9dcd8c17acc9dd0cc5d
Christian Schröter <dev@grizzly.codes>
2023-08-19 20:01:44 +0200

    feat(web): seo improvements

--- web/astro.config.ts
+++ web/astro.config.ts
@@ -1,11 +1,13 @@
 import mdx from '@astrojs/mdx'
 import react from '@astrojs/react'
+import sitemap from '@astrojs/sitemap'
 import pandacss from '@pandacss/astro'
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), pandacss(), mdx()],
+  site: 'https://ark-ui.com',
+  integrations: [react(), pandacss(), mdx(), sitemap()],
   experimental: {
     viewTransitions: true,
   },
