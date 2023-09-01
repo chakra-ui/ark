894570bc7d1bab5028c3804cd6813e9d2a8ad759
Christian Schröter <dev@grizzly.codes>
2023-09-01 19:47:03 +0200

    feat(vue): add avatar (#1347)

---
+++ packages/vue/src/avatar/avatar.story.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { Avatar, AvatarFallback, AvatarImage } from './'
+import './avatar.css'
+</script>
+
+<template>
+  <Story>
+    <Variant title="Basic">
+      <Avatar>
+        <AvatarFallback>PA</AvatarFallback>
+        <AvatarImage src="https://i.pravatar.cc/3000" alt="avatar" />
+      </Avatar>
+    </Variant>
+    <Variant title="Events">
+      <Avatar @load="() => console.log('loaded')" @error="() => console.log('error')">
+        <AvatarFallback>PA</AvatarFallback>
+        <AvatarImage src="https://i.pravatar.cc/3000" alt="avatar" />
+      </Avatar>
+    </Variant>
+  </Story>
+</template>
---
+++ packages/vue/src/avatar/avatar.tsx
@@ -0,0 +1,24 @@
+import { defineComponent } from 'vue'
+import { ark, type HTMLArkProps } from '../factory'
+import type { Assign } from '../types'
+import { AvatarProvider } from './avatar-context'
+import { emits, props } from './avatar.props'
+import { useAvatar, type UseAvatarProps } from './use-avatar'
+
+export type AvatarProps = Assign<HTMLArkProps<'div'>, UseAvatarProps>
+
+export const Avatar = defineComponent({
+  name: 'Avatar',
+  props,
+  emits,
+  setup(props, { slots, attrs, emit }) {
+    const api = useAvatar(props, emit)
+    AvatarProvider(api)
+
+    return () => (
+      <ark.div {...api.value.rootProps} {...attrs}>
+        {slots?.default?.(api.value)}
+      </ark.div>
+    )
+  },
+})
