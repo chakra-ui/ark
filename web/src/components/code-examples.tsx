ebbec7ba1c046f564ededb4e7034b9d5e082399a
Christian Schröter <dev@grizzly.codes>
2023-09-03 19:51:37 +0200

    feat(web): display stories in tabs

---
+++ web/src/components/code-examples.tsx
@@ -0,0 +1,37 @@
+import { Box } from 'styled-system/jsx'
+import {
+  TabContent,
+  TabIndicator,
+  TabList,
+  TabTrigger,
+  Tabs,
+  type TabsProps,
+} from '~/components/ui/tabs'
+
+type Props = TabsProps & {
+  react?: JSX.Element
+  solid?: JSX.Element
+  vue?: JSX.Element
+}
+
+export const CodeExamples = (props: Props) => {
+  return (
+    <Tabs size="sm" defaultValue="react" {...props}>
+      <TabList bg="bg.muted" px="4" pt="3" borderTopRadius="l3">
+        <TabTrigger value="react">React</TabTrigger>
+        <TabTrigger value="solid" disabled={!Boolean(props.solid)}>
+          Solid
+        </TabTrigger>
+        <TabTrigger value="vue" disabled={!Boolean(props.vue)}>
+          Vue
+        </TabTrigger>
+        <TabIndicator />
+      </TabList>
+      <Box borderBottomRadius="l3" mt="-6" bg="grayPalette.900">
+        <TabContent value="react">{props.react}</TabContent>
+        <TabContent value="solid">{props.solid}</TabContent>
+        <TabContent value="vue">{props.vue}</TabContent>
+      </Box>
+    </Tabs>
+  )
+}
