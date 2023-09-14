6fa9f2dce4354de4cb6ef31a641ef71e34dbc36a
Christian Schröter <dev@grizzly.codes>
2023-09-14 18:54:20 +0200

    feat(toggle-group): export anatomy

---
+++ packages/react/src/toggle-group/toggle-group.anatomy.ts
@@ -0,0 +1,4 @@
+import { createAnatomy } from '@zag-js/anatomy'
+
+// TODO replace with zagjs version
+export const toggleGroupAnatomy = createAnatomy('toggle-group').parts('root', 'toggle')
--- packages/solid/src/toggle-group/index.ts
+++ packages/solid/src/toggle-group/index.ts
@@ -1,11 +1,13 @@
 import { Toggle, type ToggleProps } from './toggle'
 import { ToggleGroup as ToggleGroupRoot, type ToggleGroupProps } from './toggle-group'
+import { useToggleGroupContext, type ToggleGroupContext } from './toggle-group-context'
+import { toggleGroupAnatomy } from './toggle-group.anatomy'
 
 const ToggleGroup = Object.assign(ToggleGroupRoot, {
   Root: ToggleGroupRoot,
   Toggle: Toggle,
 })
 
-export { Toggle, ToggleGroup }
+export { Toggle, ToggleGroup, toggleGroupAnatomy, useToggleGroupContext }
 
-export type { ToggleGroupProps, ToggleProps }
+export type { ToggleGroupContext, ToggleGroupProps, ToggleProps }
