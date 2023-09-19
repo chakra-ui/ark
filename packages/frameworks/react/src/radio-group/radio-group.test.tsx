fe47d97037bd3a419dfc6965b2a42991e37450cc
Christian Schröter <dev@grizzly.codes>
2023-09-19 22:37:56 +0200

    chore: fix tests in react

--- packages/frameworks/react/src/radio-group/radio-group.test.tsx
+++ packages/frameworks/react/src/radio-group/radio-group.test.tsx
@@ -4,12 +4,12 @@ import { vi } from 'vitest'
 import { RadioGroup, type RadioGroupProps } from '.'
 
 const Component = (props: RadioGroupProps) => {
-  const frameworks = ['React', 'Solid', 'Vue']
+  const frameworks = ['React', 'Solid', 'Svelte', 'Vue']
   return (
     <RadioGroup.Root {...props}>
       <RadioGroup.Label>Framework</RadioGroup.Label>
       {frameworks.map((framework) => (
-        <RadioGroup.Item key={framework} value={framework}>
+        <RadioGroup.Item key={framework} value={framework} disabled={framework === 'Svelte'}>
           <RadioGroup.ItemText>{framework}</RadioGroup.ItemText>
           <RadioGroup.ItemControl />
         </RadioGroup.Item>
@@ -24,8 +24,8 @@ describe('Radio Group', () => {
 
     render(<Component onValueChange={onValueChange} />)
 
-    await user.click(screen.getByLabelText('Grapes'))
-    expect(onValueChange).toHaveBeenCalledWith({ value: 'grape' })
+    await user.click(screen.getByLabelText('Solid'))
+    expect(onValueChange).toHaveBeenCalledWith({ value: 'Solid' })
   })
 
   it('should not invoke onValueChange if option is disabled', async () => {
@@ -33,7 +33,7 @@ describe('Radio Group', () => {
 
     render(<Component onValueChange={onValueChange} />)
 
-    await user.click(screen.getByLabelText('Mangoes'))
+    await user.click(screen.getByLabelText('Svelte'))
     expect(onValueChange).not.toHaveBeenCalled()
   })
 })
--- packages/frameworks/react/src/segment-group/segment-group.test.tsx
+++ packages/frameworks/react/src/segment-group/segment-group.test.tsx
@@ -4,12 +4,12 @@ import { vi } from 'vitest'
 import { SegmentGroup, type SegmentGroupProps } from './'
 
 const ComponentUnderTest = (props: SegmentGroupProps) => {
-  const frameworks = ['React', 'Solid', 'Vue']
+  const frameworks = ['React', 'Solid', 'Svelte', 'Vue']
   return (
     <SegmentGroup.Root {...props}>
       <SegmentGroup.Indicator />
       {frameworks.map((framework) => (
-        <SegmentGroup.Item key={framework} value={framework}>
+        <SegmentGroup.Item key={framework} value={framework} disabled={framework === 'Svelte'}>
           <SegmentGroup.ItemText>{framework}</SegmentGroup.ItemText>
           <SegmentGroup.ItemControl />
         </SegmentGroup.Item>
@@ -21,14 +21,14 @@ describe('Segment Group', () => {
   it('should invoke onValueChange if another value has selected', async () => {
     const onValueChange = vi.fn()
     render(<ComponentUnderTest onValueChange={onValueChange} />)
-    await user.click(screen.getByLabelText('Grapes'))
-    expect(onValueChange).toHaveBeenCalledWith({ value: 'grape' })
+    await user.click(screen.getByLabelText('Solid'))
+    expect(onValueChange).toHaveBeenCalledWith({ value: 'Solid' })
   })
 
   it('should not invoke onValueChange if option is disabled', async () => {
     const onValueChange = vi.fn()
     render(<ComponentUnderTest onValueChange={onValueChange} />)
-    await user.click(screen.getByLabelText('Mangoes'))
+    await user.click(screen.getByLabelText('Svelte'))
     expect(onValueChange).not.toHaveBeenCalled()
   })
 })
