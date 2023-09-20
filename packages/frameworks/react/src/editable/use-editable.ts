ce13bf5d2e51089e05c13370415122cfa1190c8a
junghyeonsu <jung660317@naver.com>
2023-09-20 17:28:01 +0900

    fix(editable): flushsync onChange function

--- packages/frameworks/react/src/editable/use-editable.ts
+++ packages/frameworks/react/src/editable/use-editable.ts
@@ -3,6 +3,7 @@ import { normalizeProps, useMachine } from '@zag-js/react'
 import { useId } from 'react'
 import { useEnvironmentContext } from '../environment'
 import { type Optional } from '../types'
+import { useEvent } from '../use-event'
 
 export interface UseEditableProps extends Optional<editable.Context, 'id'> {
   defaultValue?: editable.Context['value']
@@ -21,6 +22,7 @@ export const useEditable = (props: UseEditableProps): UseEditableReturn => {
   const context = {
     ...initialContext,
     value: props.value,
+    onChange: useEvent(props.onValueChange, { sync: true }),
   }
 
   const [state, send] = useMachine(editable.machine(initialContext), { context })
