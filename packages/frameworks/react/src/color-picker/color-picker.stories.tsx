8bf0c5fc65d5b4df007dc84a97fe349306e3aa20
Michał Korczak <michal.k2001@gmail.com>
2023-10-26 11:19:30 +0200

    chore: fix after merge

--- packages/frameworks/react/src/color-picker/color-picker.stories.tsx
+++ packages/frameworks/react/src/color-picker/color-picker.stories.tsx
@@ -79,64 +79,7 @@ export const Controlled = () => {
       <ColorPicker.Control>
         <ColorPicker.ChannelInput channel="hex" />
         <ColorPicker.ChannelInput channel="alpha" />
-        <ColorPicker.Trigger>
-          <ColorPicker.Swatch value="red">
-            <ColorPicker.TransparencyGrid size="10px" />
-          </ColorPicker.Swatch>
-        </ColorPicker.Trigger>
-      </ColorPicker.Control>
-      <ColorPicker.Positioner>
-        <ColorPicker.Content>
-          <ColorPicker.Area>
-            <ColorPicker.AreaBackground />
-            <ColorPicker.AreaThumb />
-          </ColorPicker.Area>
-          <ColorPicker.ChannelSlider channel="hue">
-            <ColorPicker.ChannelSliderTrack />
-            <ColorPicker.ChannelSliderThumb />
-          </ColorPicker.ChannelSlider>
-          <ColorPicker.ChannelSlider channel="alpha">
-            <ColorPicker.TransparencyGrid size="16px" />
-            <ColorPicker.ChannelSliderTrack />
-            <ColorPicker.ChannelSliderThumb />
-          </ColorPicker.ChannelSlider>
-          <ColorPicker.SwatchGroup>
-            <ColorPicker.SwatchTrigger value="red">
-              <ColorPicker.Swatch value="red" />
-            </ColorPicker.SwatchTrigger>
-            <ColorPicker.SwatchTrigger value="blue">
-              <ColorPicker.Swatch value="blue" />
-            </ColorPicker.SwatchTrigger>
-            <ColorPicker.SwatchTrigger value="green">
-              <ColorPicker.Swatch value="green" />
-            </ColorPicker.SwatchTrigger>
-          </ColorPicker.SwatchGroup>
-          <ColorPicker.ChannelInput channel="hex" />
-          <ColorPicker.ChannelInput channel="alpha" />
-          <ColorPicker.EyeDropperTrigger>Pick color</ColorPicker.EyeDropperTrigger>
-        </ColorPicker.Content>
-      </ColorPicker.Positioner>
-    </ColorPicker.Root>
-  )
-}
-
-export const Controlled = () => {
-  const [currentValue, setCurrentValue] = useState('hsl(0, 100%, 50%)')
-
-  const handleValueChange = (details: { value: Color; valueAsString: string }) => {
-    setCurrentValue(details.valueAsString)
-  }
-
-  return (
-    <ColorPicker.Root
-      value={currentValue}
-      onValueChange={handleValueChange}
-      defaultValue="hsl(0, 100%, 50%)"
-    >
-      <ColorPicker.Label>Color</ColorPicker.Label>
-      <ColorPicker.Control>
-        <ColorPicker.ChannelInput channel="hex" />
-        <ColorPicker.ChannelInput channel="alpha" />
+        <ColorPicker.ValueText />
         <ColorPicker.Trigger>
           <ColorPicker.Swatch value="red">
             <ColorPicker.TransparencyGrid size="10px" />
