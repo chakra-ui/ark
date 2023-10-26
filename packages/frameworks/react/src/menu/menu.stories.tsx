1eac667a79e7fd7dd1b48018b16fa44572b6c788
Michał Korczak <michal.k2001@gmail.com>
2023-10-26 12:59:31 +0200

    chore: wip

--- packages/frameworks/react/src/menu/menu.stories.tsx
+++ packages/frameworks/react/src/menu/menu.stories.tsx
@@ -31,6 +31,24 @@ export const Basic = () => (
   </Menu.Root>
 )
 
+export const Controlled = () => (
+  <Menu.Root onSelect={(id) => console.log(id)}>
+    <Menu.Trigger>Open menu</Menu.Trigger>
+    <Portal>
+      <Menu.Positioner>
+        <Menu.Content>
+          <Menu.Item id="search">Search</Menu.Item>
+          <Menu.Item id="undo">Undo</Menu.Item>
+          <Menu.Item id="delivery" disabled>
+            Delivery
+          </Menu.Item>
+          <Menu.Item id="unlink">Unlink</Menu.Item>
+        </Menu.Content>
+      </Menu.Positioner>
+    </Portal>
+  </Menu.Root>
+)
+
 export const Group = () => (
   <Menu.Root>
     <Menu.Trigger>Open menu</Menu.Trigger>
@@ -47,6 +65,73 @@ export const Group = () => (
             <Menu.Item id="rename">Rename...</Menu.Item>
             <Menu.Item id="delete">Delete...</Menu.Item>
           </Menu.ItemGroup>
+        </Menu.Content>
+      </Menu.Positioner>
+    </Portal>
+  </Menu.Root>
+)
+
+export const Separator = () => (
+  <Menu.Root>
+    <Menu.Trigger>Open menu</Menu.Trigger>
+    <Portal>
+      <Menu.Positioner>
+        <Menu.Content>
+          <Menu.Item id="search">Search</Menu.Item>
+          <Menu.Item id="undo">Undo</Menu.Item>
+          <Menu.Separator />
+          <Menu.Item id="delivery" disabled>
+            Delivery
+          </Menu.Item>
+          <Menu.Item id="unlink">Unlink</Menu.Item>
+        </Menu.Content>
+      </Menu.Positioner>
+    </Portal>
+  </Menu.Root>
+)
+
+export const ContextMenu = () => (
+  <Menu.Root>
+    <Menu.ContextTrigger>
+      <div style={{ width: '100%', height: '20rem', border: '1px solid lightgray' }}>
+        Some content
+      </div>
+    </Menu.ContextTrigger>
+    <Portal>
+      <Menu.Positioner>
+        <Menu.Content>
+          <Menu.Item id="search">Search</Menu.Item>
+          <Menu.Item id="undo">Undo</Menu.Item>
+          <Menu.Item id="delivery" disabled>
+            Delivery
+          </Menu.Item>
+          <Menu.Item id="unlink">Unlink</Menu.Item>
+        </Menu.Content>
+      </Menu.Positioner>
+    </Portal>
+  </Menu.Root>
+)
+
+export const SubMenu = () => (
+  <Menu.Root>
+    <Menu.Trigger>Open menu</Menu.Trigger>
+    <Portal>
+      <Menu.Positioner>
+        <Menu.Content>
+          <Menu.Item id="new-tab">New Tab...</Menu.Item>
+          <Menu.Item id="new-win">New Window...</Menu.Item>
+          <Menu.Separator />
+          <Menu>
+            <Menu.TriggerItem>Share &gt;</Menu.TriggerItem>
+            <Portal>
+              <Menu.Positioner>
+                <Menu.Content>
+                  <Menu.Item id="twitter">Twitter</Menu.Item>
+                  <Menu.Item id="message">Message</Menu.Item>
+                </Menu.Content>
+              </Menu.Positioner>
+            </Portal>
+          </Menu>
         </Menu.Content>
       </Menu.Positioner>
     </Portal>
@@ -105,32 +190,6 @@ export const Options = () => {
     </Menu.Root>
   )
 }
-
-export const SubMenu = () => (
-  <Menu.Root>
-    <Menu.Trigger>Open menu</Menu.Trigger>
-    <Portal>
-      <Menu.Positioner>
-        <Menu.Content>
-          <Menu.Item id="new-tab">New Tab...</Menu.Item>
-          <Menu.Item id="new-win">New Window...</Menu.Item>
-          <Menu.Separator />
-          <Menu>
-            <Menu.TriggerItem>Share &gt;</Menu.TriggerItem>
-            <Portal>
-              <Menu.Positioner>
-                <Menu.Content>
-                  <Menu.Item id="twitter">Twitter</Menu.Item>
-                  <Menu.Item id="message">Message</Menu.Item>
-                </Menu.Content>
-              </Menu.Positioner>
-            </Portal>
-          </Menu>
-        </Menu.Content>
-      </Menu.Positioner>
-    </Portal>
-  </Menu.Root>
-)
 
 export const ComplexSubMenu = () => {
   const [value, setValue] = useState<Record<string, string | string[]>>({
--- packages/frameworks/react/src/number-input/number-input.stories.tsx
+++ packages/frameworks/react/src/number-input/number-input.stories.tsx
@@ -5,13 +5,51 @@ import './number-input.css'
 type NumberInputType = typeof NumberInput.Root
 
 const meta: Meta<NumberInputType> = {
-  title: 'NumberInput.',
+  title: 'NumberInput',
   component: NumberInput.Root,
 }
 
 export default meta
 
 export const Basic = () => (
+  <NumberInput.Root>
+    <NumberInput.Label>Label</NumberInput.Label>
+    <NumberInput.Input />
+    <NumberInput.Control>
+      <NumberInput.DecrementTrigger>-1</NumberInput.DecrementTrigger>
+      <NumberInput.IncrementTrigger>+1</NumberInput.IncrementTrigger>
+    </NumberInput.Control>
+  </NumberInput.Root>
+)
+
+export const MinMax = () => (
+  <NumberInput.Root min={0} max={10}>
+    <NumberInput.Scrubber />
+    <NumberInput.Label>Label</NumberInput.Label>
+    <NumberInput.Input />
+    <NumberInput.Control>
+      <NumberInput.DecrementTrigger>-1</NumberInput.DecrementTrigger>
+      <NumberInput.IncrementTrigger>+1</NumberInput.IncrementTrigger>
+    </NumberInput.Control>
+  </NumberInput.Root>
+)
+
+export const FractionDigits = () => (
+  <NumberInput.Root
+    formatOptions={{ minimumFractionDigits: 2, maximumFractionDigits: 4 }}
+    defaultValue="1.00"
+  >
+    <NumberInput.Scrubber />
+    <NumberInput.Label>Label</NumberInput.Label>
+    <NumberInput.Input />
+    <NumberInput.Control>
+      <NumberInput.DecrementTrigger>-1</NumberInput.DecrementTrigger>
+      <NumberInput.IncrementTrigger>+1</NumberInput.IncrementTrigger>
+    </NumberInput.Control>
+  </NumberInput.Root>
+)
+
+export const Scrubber = () => (
   <NumberInput.Root>
     <NumberInput.Scrubber />
     <NumberInput.Label>Label</NumberInput.Label>
@@ -35,10 +73,36 @@ export const MouseWheel = () => (
   </NumberInput.Root>
 )
 
-export const FractionDigits = () => (
+export const NoClamp = () => (
+  <NumberInput.Root clampValueOnBlur={false}>
+    <NumberInput.Scrubber />
+    <NumberInput.Label>Label</NumberInput.Label>
+    <NumberInput.Input />
+    <NumberInput.Control>
+      <NumberInput.DecrementTrigger>-1</NumberInput.DecrementTrigger>
+      <NumberInput.IncrementTrigger>+1</NumberInput.IncrementTrigger>
+    </NumberInput.Control>
+  </NumberInput.Root>
+)
+
+export const FormUsage = () => (
+  <NumberInput.Root name="quantity">
+    <NumberInput.Scrubber />
+    <NumberInput.Label>Label</NumberInput.Label>
+    <NumberInput.Input />
+    <NumberInput.Control>
+      <NumberInput.DecrementTrigger>-1</NumberInput.DecrementTrigger>
+      <NumberInput.IncrementTrigger>+1</NumberInput.IncrementTrigger>
+    </NumberInput.Control>
+  </NumberInput.Root>
+)
+
+export const Formatted = () => (
   <NumberInput.Root
-    formatOptions={{ minimumFractionDigits: 2, maximumFractionDigits: 4 }}
-    defaultValue="1.00"
+    formatOptions={{
+      style: 'currency',
+      currency: 'USD',
+    }}
   >
     <NumberInput.Scrubber />
     <NumberInput.Label>Label</NumberInput.Label>
