48e442ead5c6187678a45107aa31b34ffd156ded
Christian Schröter <dev@grizzly.codes>
2023-10-30 19:55:42 +0100

    chore: wip

--- packages/frameworks/vue/src/slider/slider-value-text.tsx
+++ packages/frameworks/vue/src/slider/slider-value-text.tsx
@@ -3,17 +3,17 @@ import { ark, type HTMLArkProps } from '../factory'
 import { getValidChildren, type ComponentWithProps } from '../utils'
 import { useSliderContext } from './slider-context'
 
-export type SliderOutputProps = HTMLArkProps<'output'>
+export type SliderValueTextProps = HTMLArkProps<'div'>
 
-export const SliderOutput: ComponentWithProps<SliderOutputProps> = defineComponent({
-  name: 'SliderOutput',
+export const SliderValueText: ComponentWithProps<SliderValueTextProps> = defineComponent({
+  name: 'SliderValueText',
   setup(_, { slots, attrs }) {
     const api = useSliderContext()
 
     return () => (
-      <ark.output {...api.value.outputProps} {...attrs}>
+      <ark.div {...api.value.valueTextProps} {...attrs}>
         {() => getValidChildren(slots)}
-      </ark.output>
+      </ark.div>
     )
   },
 })
