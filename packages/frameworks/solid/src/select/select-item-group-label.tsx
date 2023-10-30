c9b3cac808b75fd231e672348679b816eb1eaa83
Christian Schröter <dev@grizzly.codes>
2023-10-30 13:55:32 +0100

    chore: wip

--- packages/frameworks/solid/src/select/select-item-group-label.tsx
+++ packages/frameworks/solid/src/select/select-item-group-label.tsx
@@ -1,16 +1,23 @@
-import type { ItemGroupLabelProps } from '@zag-js/select'
 import { mergeProps } from '@zag-js/solid'
 import { createSplitProps } from '../create-split-props'
 import { ark, type HTMLArkProps } from '../factory'
 import type { Assign } from '../types'
 import { useSelectContext } from './select-context'
 
-export type SelectItemGroupLabelProps = Assign<HTMLArkProps<'div'>, ItemGroupLabelProps>
+interface ItemGroupLabelProps {
+  for: string
+}
+
+export interface SelectItemGroupLabelProps
+  extends Assign<HTMLArkProps<'div'>, ItemGroupLabelProps> {}
 
 export const SelectItemGroupLabel = (props: SelectItemGroupLabelProps) => {
   const [labelProps, localProps] = createSplitProps<ItemGroupLabelProps>()(props, ['for'])
   const api = useSelectContext()
-  const mergedProps = mergeProps(() => api().getItemGroupLabelProps(labelProps), localProps)
+  const mergedProps = mergeProps(
+    () => api().getItemGroupLabelProps({ htmlFor: labelProps.for }),
+    localProps,
+  )
 
   return <ark.div {...mergedProps} />
 }
