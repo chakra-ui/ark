7fecd526ef21fe1e999bc92e5b2ec6adfb2da6f5
Christian Schröter <dev@grizzly.codes>
2023-11-02 20:14:49 +0100

    chore: wip

--- packages/frameworks/react/src/popover/popover-trigger.tsx
+++ packages/frameworks/react/src/popover/popover-trigger.tsx
@@ -1,13 +1,20 @@
 import { mergeProps } from '@zag-js/react'
 import { forwardRef } from 'react'
 import { ark, type HTMLArkProps } from '../factory'
+import { usePresenceContext } from '../presence'
 import { usePopoverContext } from './popover-context'
 
 export interface PopoverTriggerProps extends HTMLArkProps<'button'> {}
 
 export const PopoverTrigger = forwardRef<HTMLButtonElement, PopoverTriggerProps>((props, ref) => {
   const api = usePopoverContext()
-  const mergedProps = mergeProps(api.triggerProps, props)
+  const presence = usePresenceContext()
+
+  const mergedProps = mergeProps(
+    api.triggerProps,
+    props,
+    presence.isUnmounted ? { 'aria-controls': undefined } : {},
+  )
 
   return <ark.button {...mergedProps} ref={ref} />
 })
