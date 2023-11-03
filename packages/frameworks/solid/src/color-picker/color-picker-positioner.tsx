08066aca0b902831cf1736d2f9db69eb629b3540
Christian Schröter <dev@grizzly.codes>
2023-11-04 00:14:06 +0100

    chore: color picker

--- packages/frameworks/solid/src/color-picker/color-picker-positioner.tsx
+++ packages/frameworks/solid/src/color-picker/color-picker-positioner.tsx
@@ -1,12 +1,19 @@
 import { mergeProps } from '@zag-js/solid'
+import { Show } from 'solid-js'
 import { ark, type HTMLArkProps } from '../factory'
+import { usePresenceContext } from '../presence'
 import { useColorPickerContext } from './color-picker-context'
 
 export interface ColorPickerPositionerProps extends HTMLArkProps<'div'> {}
 
 export const ColorPickerPositioner = (props: ColorPickerPositionerProps) => {
   const api = useColorPickerContext()
+  const presenceApi = usePresenceContext()
   const mergedProps = mergeProps(() => api().positionerProps, props)
 
-  return <ark.div {...mergedProps} />
+  return (
+    <Show when={!presenceApi().isUnmounted}>
+      <ark.div {...mergedProps} />
+    </Show>
+  )
 }
