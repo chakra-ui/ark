8e72cea52b336bf2e74d110c3c2e026a84ed6607
Michał Korczak <michal.k2001@gmail.com>
2023-11-07 04:54:02 +0100

    feat(vue): revise checkbox

---
+++ packages/frameworks/vue/src/checkbox/tests/checkbox.test.tsx
@@ -0,0 +1,53 @@
+import { checkboxAnatomy } from '@ark-ui/anatomy'
+import user from '@testing-library/user-event'
+import { render } from '@testing-library/vue'
+import { Fragment, defineComponent, ref } from 'vue'
+import { getParts } from '../../setup-test'
+import ComponentUnderTest from './checkbox.test.vue'
+
+describe('Checkbox', () => {
+  it.each(getParts(checkboxAnatomy))('should render part %s', async (part) => {
+    render(ComponentUnderTest)
+    expect(document.querySelector(part)).toBeInTheDocument()
+  })
+
+  it('should render', async () => {
+    render(ComponentUnderTest)
+  })
+
+  it('should handle check and unchecked', async () => {
+    const onCheckedChange = vi.fn()
+    const { getByRole } = render(() => <ComponentUnderTest onCheckedChange={onCheckedChange} />)
+
+    const checkbox = getByRole('checkbox')
+
+    await user.click(checkbox)
+    expect(checkbox).toBeChecked()
+  })
+
+  it('should allow controlled usage', async () => {
+    const ControlledComponentUnderTest = defineComponent({
+      setup() {
+        const checked = ref(false)
+
+        return () => (
+          <Fragment>
+            <button onClick={() => (checked.value = true)}>set checked</button>
+            <ComponentUnderTest checked={checked.value} />
+          </Fragment>
+        )
+      },
+    })
+
+    const { getByRole, getByText } = render(ControlledComponentUnderTest)
+
+    expect(getByRole('checkbox')).not.toBeChecked()
+    await user.click(getByText('set checked'))
+    expect(getByRole('checkbox')).toBeChecked()
+  })
+
+  it('should handle indeterminate state from example', async () => {
+    const { getByTestId } = render(() => <ComponentUnderTest modelValue={'indeterminate'} />)
+    expect(getByTestId('control')).toHaveAttribute('data-state', 'indeterminate')
+  })
+})
---
+++ packages/frameworks/vue/src/checkbox/tests/checkbox.test.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { Checkbox } from '../'
+</script>
+
+<template>
+  <Checkbox.Root>
+    <Checkbox.Label>Checkbox</Checkbox.Label>
+    <Checkbox.Control data-testid="control" />
+    <Checkbox.Indicator />
+  </Checkbox.Root>
+</template>
