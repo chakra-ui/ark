4dcfb1b52f064e1b15f519d50d7d286994593146
Michał Korczak <michal.k2001@gmail.com>
2023-11-07 05:43:56 +0100

    fix: tests

--- packages/frameworks/vue/src/checkbox/tests/checkbox.test.tsx
+++ packages/frameworks/vue/src/checkbox/tests/checkbox.test.tsx
@@ -1,9 +1,9 @@
 import { checkboxAnatomy } from '@ark-ui/anatomy'
 import user from '@testing-library/user-event'
 import { render } from '@testing-library/vue'
-import { Fragment, defineComponent, ref } from 'vue'
 import { getParts } from '../../setup-test'
 import ComponentUnderTest from './checkbox.test.vue'
+import ControlledComponentUnderTest from './controlled-checkbox.test.vue'
 
 describe('Checkbox', () => {
   it.each(getParts(checkboxAnatomy))('should render part %s', async (part) => {
@@ -26,19 +26,6 @@ describe('Checkbox', () => {
   })
 
   it('should allow controlled usage', async () => {
-    const ControlledComponentUnderTest = defineComponent({
-      setup() {
-        const checked = ref(false)
-
-        return () => (
-          <Fragment>
-            <button onClick={() => (checked.value = true)}>set checked</button>
-            <ComponentUnderTest checked={checked.value} />
-          </Fragment>
-        )
-      },
-    })
-
     const { getByRole, getByText } = render(ControlledComponentUnderTest)
 
     expect(getByRole('checkbox')).not.toBeChecked()
---
+++ packages/frameworks/vue/src/checkbox/tests/controlled-checkbox.test.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { Checkbox, type CheckedState } from '../'
+
+const checked = ref<CheckedState>(false)
+</script>
+
+<template>
+  <Fragment>
+    <button @click="() => (checked = true)">set checked</button>
+    <Checkbox.Root :checked="checked">
+      <Checkbox.Label>Checkbox</Checkbox.Label>
+      <Checkbox.Control data-testid="control" />
+      <Checkbox.Indicator />
+    </Checkbox.Root>
+  </Fragment>
+</template>
