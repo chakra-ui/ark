68b19f06c01752298325906d1acb814687c3eb4a
Christian Schröter <dev@grizzly.codes>
2023-11-13 18:03:44 +0100

    chore: wip

--- packages/website/src/components/docs/info-banner.tsx
+++ packages/website/src/components/docs/info-banner.tsx
@@ -1,14 +1,14 @@
 import { InfoIcon } from 'lucide-react'
-import type { PropsWithChildren, ReactNode } from 'react'
+import type { PropsWithChildren } from 'react'
 import { Alert } from '~/components/ui'
 
 interface Props {
   title?: string
-  description: ReactNode
+  description?: string
 }
 
 export const InfoBanner = (props: PropsWithChildren<Props>) => {
-  const { title = 'Info', description, children } = props
+  const { title = 'Info', children, description } = props
   return (
     <Alert.Root className="not-prose" mb="6">
       <Alert.Icon asChild>
@@ -16,7 +16,11 @@ export const InfoBanner = (props: PropsWithChildren<Props>) => {
       </Alert.Icon>
       <Alert.Content textStyle="sm!">
         <Alert.Title>{title}</Alert.Title>
-        <Alert.Description asChild>{<p>{description}</p> ?? children}</Alert.Description>
+        {description ? (
+          <Alert.Description>{description}</Alert.Description>
+        ) : (
+          <Alert.Description asChild>{children}</Alert.Description>
+        )}
       </Alert.Content>
     </Alert.Root>
   )
