a3b1caefc9624fe36f8ab9ffaf36e441933d2d19
Michał Korczak <michal.k2001@gmail.com>
2023-11-17 01:57:15 +0100

    feat(number-input): add render function

--- packages/frameworks/react/src/number-input/number-input.tsx
+++ packages/frameworks/react/src/number-input/number-input.tsx
@@ -1,49 +1,70 @@
 import { mergeProps } from '@zag-js/react'
-import { forwardRef } from 'react'
+import { forwardRef, type ReactNode } from 'react'
 import { createSplitProps } from '../create-split-props'
 import { ark, type HTMLArkProps } from '../factory'
+import { runIfFn } from '../run-if-fn'
 import { type Assign } from '../types'
 import { NumberInputProvider } from './number-input-context'
-import { useNumberInput, type UseNumberInputProps } from './use-number-input'
+import {
+  useNumberInput,
+  type UseNumberInputProps,
+  type UseNumberInputReturn,
+} from './use-number-input'
 
-export interface NumberInputProps extends Assign<HTMLArkProps<'div'>, UseNumberInputProps> {}
+export interface NumberInputProps
+  extends Assign<
+    Assign<
+      HTMLArkProps<'div'>,
+      {
+        children?: ReactNode | ((api: UseNumberInputReturn) => ReactNode)
+      }
+    >,
+    UseNumberInputProps
+  > {}
 
 export const NumberInput = forwardRef<HTMLDivElement, NumberInputProps>((props, ref) => {
-  const [useNumberInputProps, divProps] = createSplitProps<UseNumberInputProps>()(props, [
-    'allowMouseWheel',
-    'allowOverflow',
-    'clampValueOnBlur',
-    'defaultValue',
-    'dir',
-    'disabled',
-    'focusInputOnChange',
-    'form',
-    'formatOptions',
-    'getRootNode',
-    'id',
-    'ids',
-    'inputMode',
-    'invalid',
-    'locale',
-    'max',
-    'min',
-    'name',
-    'onFocusChange',
-    'onValueChange',
-    'onValueInvalid',
-    'pattern',
-    'readOnly',
-    'spinOnPress',
-    'step',
-    'translations',
-    'value',
-  ])
+  const [useNumberInputProps, { children, ...divProps }] = createSplitProps<UseNumberInputProps>()(
+    props,
+    [
+      'allowMouseWheel',
+      'allowOverflow',
+      'clampValueOnBlur',
+      'defaultValue',
+      'dir',
+      'disabled',
+      'focusInputOnChange',
+      'form',
+      'formatOptions',
+      'getRootNode',
+      'id',
+      'ids',
+      'inputMode',
+      'invalid',
+      'locale',
+      'max',
+      'min',
+      'name',
+      'onFocusChange',
+      'onValueChange',
+      'onValueInvalid',
+      'pattern',
+      'readOnly',
+      'spinOnPress',
+      'step',
+      'translations',
+      'value',
+    ],
+  )
   const api = useNumberInput(useNumberInputProps)
   const mergedProps = mergeProps(api.rootProps, divProps)
 
+  const view = runIfFn(children, api)
+
   return (
     <NumberInputProvider value={api}>
-      <ark.div {...mergedProps} ref={ref} />
+      <ark.div {...mergedProps} ref={ref}>
+        {view}
+      </ark.div>
     </NumberInputProvider>
   )
 })
