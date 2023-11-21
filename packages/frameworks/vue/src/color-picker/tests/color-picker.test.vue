4f8567d66b7c2bed58e41b10139b132e83a652e9
Michał Korczak <michal.k2001@gmail.com>
2023-11-21 04:44:27 +0100

    test: update

--- packages/frameworks/vue/src/color-picker/tests/color-picker.test.vue
+++ packages/frameworks/vue/src/color-picker/tests/color-picker.test.vue
@@ -3,19 +3,21 @@ import { ColorPicker } from '../'
 </script>
 
 <template>
-  <ColorPicker.Root>
+  <ColorPicker.Root #default="api" :modelValue="'#eb5e41'">
     <ColorPicker.Label>Color</ColorPicker.Label>
     <ColorPicker.Control>
       <ColorPicker.ChannelInput channel="hex" />
       <ColorPicker.ChannelInput channel="alpha" />
+      <ColorPicker.ValueText />
       <ColorPicker.Trigger>
-        <ColorPicker.Swatch value="red">
-          <ColorPicker.TransparencyGrid size="10px" />
-        </ColorPicker.Swatch>
+        <ColorPicker.TransparencyGrid />
+        <ColorPicker.Swatch :value="api.value" />
       </ColorPicker.Trigger>
     </ColorPicker.Control>
     <ColorPicker.Positioner>
       <ColorPicker.Content>
+        <ColorPicker.FormatTrigger>Toggle ColorFormat</ColorPicker.FormatTrigger>
+        <ColorPicker.FormatSelect />
         <ColorPicker.Area>
           <ColorPicker.AreaBackground />
           <ColorPicker.AreaThumb />
@@ -25,23 +27,36 @@ import { ColorPicker } from '../'
           <ColorPicker.ChannelSliderThumb />
         </ColorPicker.ChannelSlider>
         <ColorPicker.ChannelSlider channel="alpha">
-          <ColorPicker.TransparencyGrid size="16px" />
+          <ColorPicker.TransparencyGrid />
           <ColorPicker.ChannelSliderTrack />
           <ColorPicker.ChannelSliderThumb />
         </ColorPicker.ChannelSlider>
         <ColorPicker.SwatchGroup>
           <ColorPicker.SwatchTrigger value="red">
-            <ColorPicker.Swatch value="red" />
+            <ColorPicker.Swatch value="red">
+              <ColorPicker.SwatchIndicator>✓</ColorPicker.SwatchIndicator>
+            </ColorPicker.Swatch>
           </ColorPicker.SwatchTrigger>
           <ColorPicker.SwatchTrigger value="blue">
-            <ColorPicker.Swatch value="blue" />
+            <ColorPicker.Swatch value="blue">
+              <ColorPicker.SwatchIndicator>✓</ColorPicker.SwatchIndicator>
+            </ColorPicker.Swatch>
           </ColorPicker.SwatchTrigger>
           <ColorPicker.SwatchTrigger value="green">
-            <ColorPicker.Swatch value="green" />
+            <ColorPicker.Swatch value="green">
+              <ColorPicker.SwatchIndicator>✓</ColorPicker.SwatchIndicator>
+            </ColorPicker.Swatch>
           </ColorPicker.SwatchTrigger>
         </ColorPicker.SwatchGroup>
-        <ColorPicker.ChannelInput channel="hex" />
-        <ColorPicker.ChannelInput channel="alpha" />
+        <ColorPicker.View format="rgba">
+          <ColorPicker.ChannelInput channel="hex" />
+          <ColorPicker.ChannelInput channel="alpha" />
+        </ColorPicker.View>
+        <ColorPicker.View format="hsla">
+          <ColorPicker.ChannelInput channel="hue" />
+          <ColorPicker.ChannelInput channel="saturation" />
+          <ColorPicker.ChannelInput channel="lightness" />
+        </ColorPicker.View>
         <ColorPicker.EyeDropperTrigger>Pick color</ColorPicker.EyeDropperTrigger>
       </ColorPicker.Content>
     </ColorPicker.Positioner>
