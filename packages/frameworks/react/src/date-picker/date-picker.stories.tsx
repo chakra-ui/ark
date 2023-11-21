9b52199f40a462de793497fba7769f64e4ee6fc8
Christian Schröter <dev@grizzly.codes>
2023-11-21 14:21:59 +0100

    chore: add datepicker standalone example

--- packages/frameworks/react/src/date-picker/date-picker.stories.tsx
+++ packages/frameworks/react/src/date-picker/date-picker.stories.tsx
@@ -231,3 +231,71 @@ export const RangeWithSingleGrid = () => {
     </DatePicker.Root>
   )
 }
+
+export const Standalone = () => {
+  return (
+    <DatePicker.Root open={true} numOfMonths={2} selectionMode="range" closeOnSelect={false}>
+      {(api) => {
+        const offset = api.getOffset({ months: 1 })
+        return (
+          <DatePicker.View view="day">
+            <DatePicker.ViewControl>
+              <DatePicker.PrevTrigger>Prev</DatePicker.PrevTrigger>
+              <DatePicker.ViewTrigger>
+                <DatePicker.RangeText />
+              </DatePicker.ViewTrigger>
+              <DatePicker.NextTrigger>Next</DatePicker.NextTrigger>
+            </DatePicker.ViewControl>
+            <div style={{ display: 'flex', gap: '24px' }}>
+              <DatePicker.Table>
+                <DatePicker.TableHead>
+                  <DatePicker.TableRow>
+                    {api.weekDays.map((weekDay, id) => (
+                      <DatePicker.TableHeader key={id}>{weekDay.short}</DatePicker.TableHeader>
+                    ))}
+                  </DatePicker.TableRow>
+                </DatePicker.TableHead>
+                <DatePicker.TableBody>
+                  {api.weeks.map((week, id) => (
+                    <DatePicker.TableRow key={id}>
+                      {week.map((day, id) => (
+                        <DatePicker.TableCell key={id} value={day}>
+                          <DatePicker.TableCellTrigger>{day.day}</DatePicker.TableCellTrigger>
+                        </DatePicker.TableCell>
+                      ))}
+                    </DatePicker.TableRow>
+                  ))}
+                </DatePicker.TableBody>
+              </DatePicker.Table>
+              {/* 2nd month */}
+              <DatePicker.Table>
+                <DatePicker.TableHead>
+                  <DatePicker.TableRow>
+                    {api.weekDays.map((weekDay, id) => (
+                      <DatePicker.TableHeader key={id}>{weekDay.short}</DatePicker.TableHeader>
+                    ))}
+                  </DatePicker.TableRow>
+                </DatePicker.TableHead>
+                <DatePicker.TableBody>
+                  {offset.weeks.map((week, id) => (
+                    <DatePicker.TableRow key={id}>
+                      {week.map((day, id) => (
+                        <DatePicker.TableCell
+                          key={id}
+                          value={day}
+                          visibleRange={offset.visibleRange}
+                        >
+                          <DatePicker.TableCellTrigger>{day.day}</DatePicker.TableCellTrigger>
+                        </DatePicker.TableCell>
+                      ))}
+                    </DatePicker.TableRow>
+                  ))}
+                </DatePicker.TableBody>
+              </DatePicker.Table>
+            </div>
+          </DatePicker.View>
+        )
+      }}
+    </DatePicker.Root>
+  )
+}
