0c1f2a80a1f6746e7d6c988cb7501bb297fca335
Michał Korczak <michal.k2001@gmail.com>
2023-11-23 02:04:54 +0100

    feat(vue): segment group (#1761)
    * feat(vue): add segment group
    
    * chore: docs
    
    * chore: prepare package
    
    * docs: update changelog

---
+++ packages/frameworks/vue/src/segment-group/segment-group-indicator.tsx
@@ -0,0 +1,23 @@
+import { segmentGroupAnatomy } from '@ark-ui/anatomy'
+import { defineComponent } from 'vue'
+import { ark, type HTMLArkProps } from '../factory'
+import { useSegmentGroupContext } from './segment-group-context'
+
+export interface SegmentGroupIndicatorProps extends HTMLArkProps<'div'> {}
+
+export const SegmentGroupIndicator = defineComponent({
+  name: 'SegmentGroupIndicator',
+  setup(_, { slots, attrs }) {
+    const api = useSegmentGroupContext()
+
+    return () => (
+      <ark.div
+        {...api.value.indicatorProps}
+        {...segmentGroupAnatomy.build().indicator.attrs}
+        {...attrs}
+      >
+        {slots.default?.()}
+      </ark.div>
+    )
+  },
+})
