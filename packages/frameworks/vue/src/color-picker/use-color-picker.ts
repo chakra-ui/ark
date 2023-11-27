d889e31c9054ac6f4fe3758f7aea28157f2f3a7d
Michał Korczak <michal.k2001@gmail.com>
2023-11-27 01:31:14 +0100

    chore: clean

--- packages/frameworks/vue/src/color-picker/use-color-picker.ts
+++ packages/frameworks/vue/src/color-picker/use-color-picker.ts
@@ -18,15 +18,11 @@ export const useColorPicker = (
 
   const context = computed(() => {
     const { modelValue, ...rest } = props
-    console.log('context', modelValue)
-    modelValue ? console.log('parsed', colorPicker.parse(modelValue)) : null
     return {
       ...rest,
       value: modelValue ? colorPicker.parse(modelValue) : undefined,
     }
   })
-
-  console.log('context value', context.value)
 
   const [state, send] = useMachine(
     colorPicker.machine({
@@ -40,7 +36,6 @@ export const useColorPicker = (
         emit('open-change', details)
       },
       onValueChange(details) {
-        console.log('value change', details)
         emit('value-change', details)
         emit('update:modelValue', details.valueAsString)
       },
