0846b4a4f1ed0800d644c8157ef35d31da291c3b
Michał Korczak <michal.k2001@gmail.com>
2023-12-02 00:02:55 +0100

    feat(vue): revise pin input (#1809)
    * feat(vue): revise pin input
    
    * test: revise tests
    
    * docs: gen

---
+++ packages/frameworks/vue/src/pin-input/pin-input.story.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+import { PinInput } from './'
+</script>
+
+<template>
+  <Story title="Pin Input">
+    <Variant title="Basic">
+      <PinInput.Root @value-complete="(e) => console.log(e.valueAsString)">
+        <PinInput.Label>Label</PinInput.Label>
+        <PinInput.Control>
+          <PinInput.Input v-for="id in [0, 1, 2]" :key="id" :index="id" />
+        </PinInput.Control>
+      </PinInput.Root>
+    </Variant>
+    <Variant title="InitialValue">
+      <PinInput.Root :model-value="['1', '2', '3']">
+        <PinInput.Label>Label</PinInput.Label>
+        <PinInput.Control>
+          <PinInput.Input v-for="id in [0, 1, 2]" :key="id" :index="id" />
+        </PinInput.Control>
+      </PinInput.Root>
+    </Variant>
+    <Variant title="Customized">
+      <PinInput.Root placeholder="*">
+        <PinInput.Label>Label</PinInput.Label>
+        <PinInput.Control>
+          <PinInput.Input v-for="id in [0, 1, 2]" :key="id" :index="id" />
+        </PinInput.Control>
+      </PinInput.Root>
+    </Variant>
+    <Variant title="Blurred">
+      <PinInput.Root blurOnComplete>
+        <PinInput.Label>Label</PinInput.Label>
+        <PinInput.Control>
+          <PinInput.Input v-for="id in [0, 1, 2]" :key="id" :index="id" />
+        </PinInput.Control>
+      </PinInput.Root>
+    </Variant>
+    <Variant title="OTPMode">
+      <PinInput.Root otp>
+        <PinInput.Label>Label</PinInput.Label>
+        <PinInput.Control>
+          <PinInput.Input v-for="id in [0, 1, 2]" :key="id" :index="id" />
+        </PinInput.Control>
+      </PinInput.Root>
+    </Variant>
+    <Variant title="WithMask">
+      <PinInput.Root mask>
+        <PinInput.Label>Label</PinInput.Label>
+        <PinInput.Control>
+          <PinInput.Input v-for="id in [0, 1, 2]" :key="id" :index="id" />
+        </PinInput.Control>
+      </PinInput.Root>
+    </Variant>
+  </Story>
+</template>
