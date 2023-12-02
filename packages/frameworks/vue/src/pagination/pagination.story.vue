d71d48ec82869877fb8cabfc8c333ac85cd04b48
Michał Korczak <michal.k2001@gmail.com>
2023-12-02 04:39:27 +0100

    feat(vue): revise pagination

--- packages/frameworks/vue/src/pagination/pagination.story.vue
+++ packages/frameworks/vue/src/pagination/pagination.story.vue
@@ -1,27 +1,44 @@
 <script setup lang="ts">
-import {
-  Pagination,
-  PaginationEllipsis,
-  PaginationNextTrigger,
-  PaginationItem,
-  PaginationPrevTrigger,
-} from './'
-
-import './pagination.css'
+import { Pagination } from './';
+import './pagination.css';
 </script>
+
 <template>
-  <Story>
+  <Story title="Pagination">
     <Variant title="Basic">
-      <Pagination v-slot="{ pages }" :count="5000" :page-size="10" :sibling-count="2">
-        <PaginationPrevTrigger>Previous </PaginationPrevTrigger>
+      <Pagination.Root v-slot="{ pages }" :count="5000" :page-size="10" :sibling-count="2">
+        <Pagination.PrevTrigger>Previous Page</Pagination.PrevTrigger>
         <template v-for="(page, index) in pages">
-          <PaginationItem v-if="page.type === 'page'" :key="page" :value="page.value">
+          <Pagination.Item v-if="page.type === 'page'" :key="page" :value="page.value">
             {{ page.value }}
-          </PaginationItem>
-          <PaginationEllipsis v-else :key="index" :index="index"> &#8230; </PaginationEllipsis>
+          </Pagination.Item>
+          <Pagination.Ellipsis v-else :key="index" :index="index"> &#8230; </Pagination.Ellipsis>
         </template>
-        <PaginationNextTrigger>Next </PaginationNextTrigger>
-      </Pagination>
+        <Pagination.NextTrigger>Next Page</Pagination.NextTrigger>
+      </Pagination.Root>
+    </Variant>
+    <Variant title="Customized">
+      <Pagination.Root
+        v-slot="{ pages }"
+        :count="5000"
+        :page-size="20"
+        :sibling-count="3"
+        dir="ltr"
+        :translations="{
+          nextTriggerLabel: 'Next',
+          prevTriggerLabel: 'Prev',
+          itemLabel: (details) => `Page ${details.page}`,
+        }"
+      >
+        <Pagination.PrevTrigger>Previous</Pagination.PrevTrigger>
+        <template v-for="(page, index) in pages">
+          <Pagination.Item v-if="page.type === 'page'" :key="page" :value="page.value">
+            {{ page.value }}
+          </Pagination.Item>
+          <Pagination.Ellipsis v-else :key="index" :index="index"> &#8230; </Pagination.Ellipsis>
+        </template>
+        <Pagination.NextTrigger>Next Page</Pagination.NextTrigger>
+      </Pagination.Root>
     </Variant>
   </Story>
 </template>
--- packages/frameworks/vue/src/pagination/tests/pagination.test.tsx
+++ packages/frameworks/vue/src/pagination/tests/pagination.test.tsx
@@ -1,13 +1,21 @@
+import { paginationAnatomy } from '@ark-ui/anatomy'
 import user from '@testing-library/user-event'
 import { render, screen } from '@testing-library/vue'
+import { getParts } from '../../setup-test'
 import ComponentUnderTest from './pagination.test.vue'
 
 describe('Pagination', () => {
+  it.each(getParts(paginationAnatomy))('should render part! %s', async (part) => {
+    render(ComponentUnderTest)
+    expect(document.querySelector(part)).toBeInTheDocument()
+  })
+
   it('should update page when item is clicked', async () => {
     render(ComponentUnderTest)
 
     const pageTwoLink = screen.getByLabelText('page 2')
     expect(pageTwoLink).not.toHaveAttribute('aria-current', 'page')
+
     await user.click(pageTwoLink)
     expect(screen.getByLabelText('page 2')).toHaveAttribute('aria-current', 'page')
   })
@@ -18,6 +26,7 @@ describe('Pagination', () => {
     const pageOneLink = screen.getByLabelText('page 1')
     expect(pageOneLink).toHaveAttribute('aria-current', 'page')
     const nextPageLink = screen.getByText(/next/i)
+
     await user.click(nextPageLink)
     const pageTwoLink = screen.getByLabelText('page 2')
     expect(pageTwoLink).toHaveAttribute('aria-current', 'page')
@@ -28,7 +37,6 @@ describe('Pagination', () => {
 
     const pageTwoLink = screen.getByLabelText('page 2')
     await user.click(pageTwoLink)
-
     expect(screen.getByLabelText('page 2')).toHaveAttribute('aria-current', 'page')
 
     const prevPageLink = screen.getByText(/prev/i)
