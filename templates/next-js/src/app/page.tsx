70dbbbe51466731dbb0f62dccbc3c1a63a2e4d7e
Christian Schröter <dev@grizzly.codes>
2023-12-05 09:28:34 +0100

    chore: add nextjs template

---
+++ templates/next-js/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { ark } from '@ark-ui/react'
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Welcome to Ark UI</h1>
+      <ark.button>Hello</ark.button>
+    </main>
+  )
+}
