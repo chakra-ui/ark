a7055884b0b824a0ec24287f21a815695ab8ff25
Michał Korczak <michal.k2001@gmail.com>
2023-12-13 20:50:25 +0100

    chore: silence lint

--- packages/frameworks/vue/src/date-picker/tests/date-picker.test.tsx
+++ packages/frameworks/vue/src/date-picker/tests/date-picker.test.tsx
@@ -6,6 +6,7 @@ import ComponentUnderTest from './date-picker.test.vue'
 describe('Date Picker', () => {
   it.each(getParts(datePickerAnatomy))('should render part %s', async (part) => {
     render(ComponentUnderTest)
+    // eslint-disable-next-line testing-library/no-node-access
     expect(document.querySelector(part)).toBeInTheDocument()
   })
 })
