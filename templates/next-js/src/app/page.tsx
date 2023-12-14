485acb5f500dca891b6080d59240ecc442d469d8
Christian Schröter <dev@grizzly.codes>
2023-12-14 20:38:09 +0100

    fix: make sure imports work in nextjs

--- templates/next-js/src/app/page.tsx
+++ templates/next-js/src/app/page.tsx
@@ -1,10 +1,13 @@
-import { MyMenu } from '~/components/my-menu'
+import { Avatar } from '@ark-ui/react/src/avatar'
 
 export default function Home() {
   return (
     <main>
       <h1>Welcome to Ark UI</h1>
-      <MyMenu />
+      <Avatar.Root>
+        <Avatar.Fallback>PA</Avatar.Fallback>
+        <Avatar.Image src="https://i.pravatar.cc/300" alt="avatar" />
+      </Avatar.Root>
     </main>
   )
 }
