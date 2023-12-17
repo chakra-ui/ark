dfe4fa055e53eca082c30950f5cd1b33709d2dd2
Omikorin <Omikorin@users.noreply.github.com>
2023-12-17 00:18:10 +0000

    refactor(react): no destructuring of api

--- packages/frameworks/react/src/tags-input/tags-input-input.tsx
+++ packages/frameworks/react/src/tags-input/tags-input-input.tsx
@@ -6,8 +6,8 @@ import { useTagsInputContext } from './tags-input-context'
 export interface TagsInputInputProps extends HTMLArkProps<'input'> {}
 
 export const TagsInputInput = forwardRef<HTMLInputElement, TagsInputInputProps>((props, ref) => {
-  const { inputProps } = useTagsInputContext()
-  const mergedProps = mergeProps(inputProps, props)
+  const api = useTagsInputContext()
+  const mergedProps = mergeProps(api.inputProps, props)
 
   return <ark.input {...mergedProps} ref={ref} />
 })
