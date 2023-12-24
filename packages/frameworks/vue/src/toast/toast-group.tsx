611a41abea712eeb6a3dadab8ea1ed84ed53ea31
Michał Korczak <michal.k2001@gmail.com>
2023-12-24 08:49:53 +0100

    refactor: toast

--- packages/frameworks/vue/src/toast/toast-group.tsx
+++ packages/frameworks/vue/src/toast/toast-group.tsx
@@ -3,9 +3,11 @@ import { ark, type HTMLArkProps } from '../factory'
 
 export interface ToastGroupProps extends HTMLArkProps<'ol'> {}
 
-export const ToastGroup = defineComponent({
-  name: 'ToastGroup',
-  setup(_, { attrs, slots }) {
+export const ToastGroup = defineComponent<ToastGroupProps>(
+  (_, { attrs, slots }) => {
     return () => <ark.ol {...attrs}>{slots.default?.()}</ark.ol>
   },
-})
+  {
+    name: 'ToastGroup',
+  },
+)
