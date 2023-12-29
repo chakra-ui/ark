4b9519c2528615aa24a550c8fd391708fa8c2bc6
Michał Korczak <michal.k2001@gmail.com>
2023-12-29 21:06:06 +0100

    chore: final clean

--- packages/frameworks/react/src/progress/progress-circle-range.tsx
+++ packages/frameworks/react/src/progress/progress-circle-range.tsx
@@ -6,14 +6,7 @@ export interface ProgressCircleRangeProps extends HTMLArkProps<'circle'> {}
 
 export const ProgressCircleRange = (props: ProgressCircleRangeProps) => {
   const api = useProgressContext()
-  const mergedProps = mergeProps(api.circleRangeProps, props, {
-    style: {
-      // @ts-expect-error missing until new zag release
-      cx: '50px',
-      cy: '50px',
-      r: '42px',
-    },
-  })
+  const mergedProps = mergeProps(api.circleRangeProps, props)
 
   return <ark.circle {...mergedProps} />
 }
--- packages/frameworks/react/src/progress/progress-circle-track.tsx
+++ packages/frameworks/react/src/progress/progress-circle-track.tsx
@@ -6,14 +6,7 @@ export interface ProgressCircleTrackProps extends HTMLArkProps<'circle'> {}
 
 export const ProgressCircleTrack = (props: ProgressCircleTrackProps) => {
   const api = useProgressContext()
-  const mergedProps = mergeProps(api.circleTrackProps, props, {
-    style: {
-      // @ts-expect-error missing until new zag release
-      cx: '50px',
-      cy: '50px',
-      r: '42px',
-    },
-  })
+  const mergedProps = mergeProps(api.circleTrackProps, props)
 
   return <ark.circle {...mergedProps} />
 }
