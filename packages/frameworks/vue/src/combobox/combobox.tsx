b5e025723d71364a75d873a4af710c587d949348
Michał Korczak <michal.k2001@gmail.com>
2023-12-30 22:38:37 +0100

    fix(vue): combobox presence

--- packages/frameworks/vue/src/combobox/combobox.tsx
+++ packages/frameworks/vue/src/combobox/combobox.tsx
@@ -1,6 +1,6 @@
-import { defineComponent, type PropType } from 'vue'
+import { computed, defineComponent, type PropType } from 'vue'
 import { ark, type HTMLArkProps } from '../factory'
-import { type UsePresenceProps } from '../presence'
+import { PresenceProvider, usePresence, type UsePresenceProps } from '../presence'
 import type { Assign, CollectionItem } from '../types'
 import { ComboboxProvider } from './combobox-context'
 import { emits, props } from './combobox.props'
@@ -27,11 +27,34 @@ export const Combobox = defineComponent({
     isItemDisabled: {
       type: Function as PropType<UseComboboxProps<any>['isItemDisabled']>,
     },
+    present: {
+      type: Boolean as PropType<UsePresenceProps['present']>,
+      default: undefined,
+    },
+    lazyMount: {
+      type: Boolean as PropType<UsePresenceProps['lazyMount']>,
+      default: false,
+    },
+    unmountOnExit: {
+      type: Boolean as PropType<UsePresenceProps['unmountOnExit']>,
+      default: false,
+    },
   },
   emits,
   setup(props, { slots, attrs, emit }) {
     const api = useCombobox(props, emit)
+
+    const isOpen = computed(() => api.value.isOpen)
+
+    const presenceProps = computed(() => ({
+      present: props.present || isOpen.value,
+      lazyMount: props.lazyMount,
+      unmountOnExit: props.unmountOnExit,
+    }))
+    const presenceApi = usePresence(presenceProps as any, emit)
+
     ComboboxProvider(api)
+    PresenceProvider(presenceApi)
 
     return () => (
       <ark.div {...api.value.rootProps} {...attrs}>
