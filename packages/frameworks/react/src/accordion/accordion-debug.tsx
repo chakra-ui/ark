064b5c24aced9b91432ef11d83747f60abe4b782
Michał Korczak <michal.k2001@gmail.com>
2023-12-30 02:26:53 +0100

    chore: example debug

--- packages/frameworks/react/src/accordion/accordion-debug.tsx
+++ packages/frameworks/react/src/accordion/accordion-debug.tsx
@@ -1,20 +1,22 @@
 import { forwardRef } from 'react'
 import { ark, type HTMLArkProps } from '../factory'
-import { useAccordionContext } from './accordion-context'
+import { useAccordionContext, useAccordionMachineContext } from './accordion-context'
 import type { UseAccordionReturn } from './use-accordion'
 
 export interface AccordionDebugProps extends HTMLArkProps<'div'> {}
 
 export const AccordionDebug = forwardRef<HTMLDivElement, AccordionDebugProps>((_, ref) => {
   const api = useAccordionContext() as UseAccordionReturn['api']
-  const machine = useAccordionContext() as UseAccordionReturn['machine']
+  const machine = useAccordionMachineContext() as UseAccordionReturn['machine']
 
+  console.log(machine)
   return (
     <ark.div ref={ref} data-scope="accordion" data-part="debug">
       <ark.pre dir="ltr">
         <ark.details open>
           <ark.summary>Accordion Debug</ark.summary>
           <ark.div>Value: {api.value}</ark.div>
+          <ark.div dangerouslySetInnerHTML={{ __html: stringifyState(machine.state) }} />
         </ark.details>
       </ark.pre>
     </ark.div>
@@ -22,3 +24,55 @@ export const AccordionDebug = forwardRef<HTMLDivElement, AccordionDebugProps>((_
 })
 
 AccordionDebug.displayName = 'AccordionDebug'
+
+const pick = (obj: Record<string, any>, keys: string[]) =>
+  Object.fromEntries(keys.filter((key) => key in obj).map((key) => [key, obj[key]]))
+
+export function stringifyState(state: Record<string, any>, omit?: string[]) {
+  const code = JSON.stringify(
+    state,
+    (key, v) => {
+      try {
+        if (v.hasOwnProperty('target') && v.hasOwnProperty('timeStamp'))
+          return pick(v, ['type', 'target', 'currentTarget', 'relatedTarget'])
+
+        if (omit?.includes(key)) {
+          return undefined
+        }
+
+        if (v.hasOwnProperty('calendar')) {
+          return v.toString()
+        }
+
+        if (Number.isNaN(v)) {
+          return 'NaN'
+        }
+
+        if (v instanceof File) {
+          return v.name
+        }
+
+        switch (v?.toString()) {
+          case '[object Machine]':
+            const id = v.state.context.id ?? v.id
+            return `Machine: ${id}`
+          case '[object ShadowRoot]':
+            return '#shadow-root'
+          case '[object HTMLDocument]':
+            return '#document'
+          case '[object Window]':
+            return '#window'
+          case '[object AbortController]':
+            return '#abort-cntroller'
+          default:
+            return v !== null && typeof v === 'object' && v.nodeType === 1 ? v.tagName : v
+        }
+      } catch {
+        return v
+      }
+    },
+    4,
+  )
+  // return formatHighlight(code)
+  return code
+}
