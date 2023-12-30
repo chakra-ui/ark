8c05acc89a14ff3c9a5896c0bab224a2e17fa117
Michał Korczak <michal.k2001@gmail.com>
2023-12-30 23:04:27 +0100

    fix(vue): hover card presence

--- packages/frameworks/vue/src/hover-card/hover-card.tsx
+++ packages/frameworks/vue/src/hover-card/hover-card.tsx
@@ -1,5 +1,6 @@
-import { defineComponent } from 'vue'
+import { computed, defineComponent, type PropType } from 'vue'
 import { type HTMLArkProps } from '../factory'
+import { PresenceProvider, usePresence, type UsePresenceProps } from '../presence'
 import type { Assign } from '../types'
 import { HoverCardProvider } from './hover-card-context'
 import { emits, props } from './hover-card.props'
@@ -10,13 +11,38 @@ export interface HoverCardProps extends Assign<HTMLArkProps<'div'>, UseHoverCard
 export const HoverCard = defineComponent<HoverCardProps>(
   (props, { slots, emit }) => {
     const api = useHoverCard(props, emit)
+
+    const isOpen = computed(() => api.value.isOpen)
+
+    const presenceProps = computed(() => ({
+      present: props.present || isOpen.value,
+      lazyMount: props.lazyMount,
+      unmountOnExit: props.unmountOnExit,
+    }))
+    const presenceApi = usePresence(presenceProps as any, emit)
+
     HoverCardProvider(api)
+    PresenceProvider(presenceApi)
 
     return () => slots.default?.(api.value)
   },
   {
     name: 'HoverCard',
-    props,
+    props: {
+      ...props,
+      present: {
+        type: Boolean as PropType<UsePresenceProps['present']>,
+        default: undefined,
+      },
+      lazyMount: {
+        type: Boolean as PropType<UsePresenceProps['lazyMount']>,
+        default: false,
+      },
+      unmountOnExit: {
+        type: Boolean as PropType<UsePresenceProps['unmountOnExit']>,
+        default: false,
+      },
+    },
     emits,
   },
 )
