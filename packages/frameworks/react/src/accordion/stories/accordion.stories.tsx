26b480e10764bff32402bffd9a3f308a5c7005b2
Michał Korczak <michal.k2001@gmail.com>
2023-12-30 01:56:53 +0100

    chore: story test

--- packages/frameworks/react/src/accordion/stories/accordion.stories.tsx
+++ packages/frameworks/react/src/accordion/stories/accordion.stories.tsx
@@ -16,6 +16,7 @@ export default meta
 export const Basic = () => {
   return (
     <Accordion.Root defaultValue={['React']}>
+      <Accordion.Debug />
       {['React', 'Solid', 'Vue'].map((item, id) => (
         <Accordion.Item key={id} value={item}>
           <Accordion.ItemTrigger>
