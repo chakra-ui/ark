6a4343fc00c22522fb89786ab44e8e020ac817f7
Michał Korczak <michal.k2001@gmail.com>
2023-12-31 01:47:19 +0100

    chore: clean

--- packages/frameworks/vue/src/date-picker/tests/date-picker.test.tsx
+++ packages/frameworks/vue/src/date-picker/tests/date-picker.test.tsx
@@ -6,13 +6,13 @@ import { getExports, getParts } from '../../setup-test'
 import ComponentUnderTest from './date-picker.test.vue'
 
 describe('Date Picker', () => {
-  it.skip.each(getParts(datePickerAnatomy))('should render part %s', async (part) => {
+  it.each(getParts(datePickerAnatomy))('should render part %s', async (part) => {
     render(ComponentUnderTest)
     // eslint-disable-next-line testing-library/no-node-access
     expect(document.querySelector(part)).toBeInTheDocument()
   })
 
-  it.skip.each(getExports(datePickerAnatomy))('should export %s', async (part) => {
+  it.each(getExports(datePickerAnatomy))('should export %s', async (part) => {
     expect(DatePicker[part]).toBeDefined()
   })
 
