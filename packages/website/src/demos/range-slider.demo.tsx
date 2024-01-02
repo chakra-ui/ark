c64643fbf5dd363d80da2293149f8eb5d568cf41
Michał Korczak <michal.k2001@gmail.com>
2024-01-03 00:30:46 +0100

    chore: demo

--- packages/website/src/demos/range-slider.demo.tsx
+++ packages/website/src/demos/range-slider.demo.tsx
@@ -2,12 +2,13 @@ import { Slider, type SliderProps } from '~/components/ui'
 
 export const Demo = (props: SliderProps) => {
   return (
-    <Slider.Root min={0} max={100} defaultValue={[33]} {...props}>
+    <Slider.Root min={0} max={100} defaultValue={[33, 66]} {...props}>
       <Slider.Control>
         <Slider.Track>
           <Slider.Range />
         </Slider.Track>
         <Slider.Thumb key={0} index={0} />
+        <Slider.Thumb key={1} index={1} />
       </Slider.Control>
       <Slider.MarkerGroup>
         <Slider.Marker value={25}>25</Slider.Marker>
