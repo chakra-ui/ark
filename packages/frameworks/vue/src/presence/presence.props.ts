0f2819a2eee34adaca133cf8794e177e319d41d5
Michał Korczak <michal.k2001@gmail.com>
2024-01-02 16:22:32 +0100

    revert: presence props

--- packages/frameworks/vue/src/presence/presence.props.ts
+++ packages/frameworks/vue/src/presence/presence.props.ts
@@ -1,12 +1,21 @@
+import type { Context } from '@zag-js/presence'
+import type { PropType } from 'vue'
 import { declareEmits } from '../utils'
+import type { UsePresenceProps } from './use-presence'
 
 export const props = {
   present: {
-    type: Boolean,
+    type: Boolean as PropType<Context['present']>,
     default: undefined,
   },
-  lazyMount: Boolean,
-  unmountOnExit: Boolean,
+  lazyMount: {
+    type: Boolean as PropType<UsePresenceProps['lazyMount']>,
+    default: false,
+  },
+  unmountOnExit: {
+    type: Boolean as PropType<UsePresenceProps['unmountOnExit']>,
+    default: false,
+  },
 }
 
 export const emits = declareEmits(['exit-complete'])
