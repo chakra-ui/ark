e1946935a8bcfc3d7a985e9233275d51a485f859
Michał Korczak <michal.k2001@gmail.com>
2024-01-02 23:06:19 +0100

    chore: demo

--- packages/website/src/demos/nested-menu.demo.tsx
+++ packages/website/src/demos/nested-menu.demo.tsx
@@ -1,4 +1,15 @@
-import { CreditCardIcon, LogOutIcon, SettingsIcon, UserIcon } from 'lucide-react'
+import { Portal } from '@ark-ui/react'
+import {
+  ChevronRightIcon,
+  CreditCardIcon,
+  LogOutIcon,
+  MailIcon,
+  MessageSquareIcon,
+  PlusCircleIcon,
+  SettingsIcon,
+  UserIcon,
+  UserPlusIcon,
+} from 'lucide-react'
 import { HStack } from 'styled-system/jsx'
 import { Button, Menu, Text, type MenuProps } from '~/components/ui'
 
@@ -43,6 +54,40 @@ export const Demo = (props: MenuProps) => {
                 </Text>
               </HStack>
             </Menu.Item>
+            <Menu.Root positioning={{ placement: 'right-start', gutter: -2 }} {...props}>
+              <Menu.TriggerItem justifyContent="space-between">
+                <HStack gap="2">
+                  <UserPlusIcon />
+                  Invite member
+                </HStack>
+                <ChevronRightIcon />
+              </Menu.TriggerItem>
+              <Portal>
+                <Menu.Positioner>
+                  <Menu.Content>
+                    <Menu.Item id="email">
+                      <HStack gap="2">
+                        <MailIcon />
+                        Email
+                      </HStack>
+                    </Menu.Item>
+                    <Menu.Item id="message">
+                      <HStack gap="2">
+                        <MessageSquareIcon />
+                        Message
+                      </HStack>
+                    </Menu.Item>
+                    <Menu.Separator />
+                    <Menu.Item id="other">
+                      <HStack gap="2">
+                        <PlusCircleIcon />
+                        More Options...
+                      </HStack>
+                    </Menu.Item>
+                  </Menu.Content>
+                </Menu.Positioner>
+              </Portal>
+            </Menu.Root>
             <Menu.Separator />
             <Menu.Item id="logout">
               <HStack gap="2">
