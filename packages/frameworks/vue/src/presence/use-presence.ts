d3645c430167ab8cb11ce57fa4d7c7ac0ec29ce1
Michał Korczak <michal.k2001@gmail.com>
2024-01-02 16:24:45 +0100

    revert: use presence

--- packages/frameworks/vue/src/presence/use-presence.ts
+++ packages/frameworks/vue/src/presence/use-presence.ts
@@ -1,6 +1,6 @@
 import * as presence from '@zag-js/presence'
 import { normalizeProps, useMachine } from '@zag-js/vue'
-import { computed, ref, toValue, watch, type MaybeRef, type VNodeRef } from 'vue'
+import { computed, ref, watch, type VNodeRef } from 'vue'
 import type { Optional } from '../types'
 
 export interface UsePresenceProps extends Optional<presence.Context, 'present'> {
@@ -18,19 +18,20 @@ export interface UsePresenceProps extends Optional<presence.Context, 'present'> 
 
 export type UsePresenceReturn = ReturnType<typeof usePresence>
 
-export const usePresence = (props: MaybeRef<UsePresenceProps>, emit: CallableFunction) => {
-  const context = computed(() => ({
-    ...toValue(props),
-    // FIXME: we have to declare here, because zag did not set `onExitComplete` during initialization.
-    // FIX PR: https://github.com/chakra-ui/zag/pull/1124, maybe we can move `onExitComplete` to init later.
-    onExitComplete: () => {
-      emit('exit-complete')
-    },
-  }))
+export const usePresence = (props: UsePresenceProps, emit: CallableFunction) => {
+  const context = ref(props)
   const wasEverPresent = ref(false)
   const nodeRef = ref<VNodeRef | null>(null)
 
-  const [state, send] = useMachine(presence.machine(context.value), { context })
+  const [state, send] = useMachine(
+    presence.machine({
+      ...context.value,
+      onExitComplete: () => {
+        emit('exit-complete')
+      },
+    }),
+    { context },
+  )
   const api = computed(() => presence.connect(state.value, send, normalizeProps))
 
   watch(
