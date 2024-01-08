d4df0753b60a21538746470fb3ad6920ea243b0f
Din <iamdinq@gmail.com>
2024-01-08 23:13:27 +0800

    refactor(vue/dialog): use v-model:open for two-way binding (#1982)
    * refactor(vue/dialog): use v-model:open for two-way binding
    
    * chore: docs
    
    * Update packages/frameworks/vue/src/dialog/stories/dialog.story.vue
    
    ---------
    
    Co-authored-by: Michał Korczak <michal.k2001@gmail.com>

--- packages/frameworks/vue/src/dialog/stories/dialog.story.vue
+++ packages/frameworks/vue/src/dialog/stories/dialog.story.vue
@@ -26,7 +26,7 @@ const open = ref(false)
 
     <Variant title="Controlled">
       <button @click="() => (open = true)">Open Dialog</button>
-      <Dialog.Root v-model="open">
+      <Dialog.Root v-model:open="open">
         <Teleport to="body">
           <Dialog.Backdrop />
           <Dialog.Positioner>
