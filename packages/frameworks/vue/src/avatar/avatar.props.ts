3074824c29fe91b820ef19d599db6bc605c95ac9
carwack <sybren.willemot@gmail.com>
2024-01-14 00:46:02 +0100

    feat(avatar): add jsdocs for avatar props

--- packages/frameworks/vue/src/avatar/avatar.props.ts
+++ packages/frameworks/vue/src/avatar/avatar.props.ts
@@ -2,12 +2,15 @@ import type { Context, StatusChangeDetails } from '@zag-js/avatar'
 import type { PropType } from 'vue'
 
 export const avatarProps = {
+  /** The document's text/writing direction. */
   dir: {
     type: String as PropType<Context['dir']>,
   },
+  /** A root node to correctly resolve document in custom environments. E.x.: Iframes, Electron. */
   getRootNode: {
     type: Function as PropType<Context['getRootNode']>,
   },
+  /** The unique identifier of the machine. */
   id: {
     type: String as PropType<Context['id']>,
   },
