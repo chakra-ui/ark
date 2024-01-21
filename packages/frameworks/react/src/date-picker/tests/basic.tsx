56aab1903a1a966e8556d2b7de7a9e6696a8ed1e
Michał Korczak <michal.k2001@gmail.com>
2024-01-21 02:27:41 +0100

    chore: wip

--- packages/frameworks/react/src/date-picker/tests/basic.tsx
+++ packages/frameworks/react/src/date-picker/tests/basic.tsx
@@ -1,12 +1,7 @@
-import { datePickerAnatomy } from '@ark-ui/anatomy'
-// eslint-disable-next-line testing-library/no-manual-cleanup
-import { cleanup, render, screen } from '@testing-library/react/pure'
-import user from '@testing-library/user-event'
-import { Portal } from '../portal'
-import { getExports, getParts } from '../setup-test'
-import { DatePicker, type DatePickerProps } from './'
+import { DatePicker, type DatePickerProps } from '../'
+import { Portal } from '../../portal'
 
-const ComponentUnderTest = (props: DatePickerProps) => (
+export const ComponentUnderTest = (props: DatePickerProps) => (
   <DatePicker.Root {...props}>
     <DatePicker.Label>Label</DatePicker.Label>
     <DatePicker.Control>
@@ -109,50 +104,3 @@ const ComponentUnderTest = (props: DatePickerProps) => (
     </Portal>
   </DatePicker.Root>
 )
-
-describe('Date Picker / Parts & Exports', () => {
-  afterAll(() => {
-    cleanup()
-  })
-
-  render(<ComponentUnderTest />)
-
-  it.each(getParts(datePickerAnatomy))('should render part %s', async (part) => {
-    // eslint-disable-next-line testing-library/no-node-access
-    expect(document.querySelector(part)).toBeInTheDocument()
-  })
-
-  it.each(getExports(datePickerAnatomy))('should export %s', async (part) => {
-    expect(DatePicker[part]).toBeDefined()
-  })
-})
-
-describe('Date Picker', () => {
-  afterEach(() => {
-    cleanup()
-  })
-
-  it('should be able to lazy mount', async () => {
-    render(<ComponentUnderTest lazyMount />)
-
-    expect(screen.queryByTestId('positioner')).not.toBeInTheDocument()
-
-    await user.click(screen.getByRole('button', { name: 'Open calendar' }))
-    expect(screen.getByTestId('positioner')).toBeInTheDocument()
-
-    await user.click(screen.getByRole('button', { name: 'Close calendar' }))
-    expect(screen.getByTestId('positioner')).toBeInTheDocument()
-  }, 7000)
-
-  it('should lazy mount and unmount on exit', async () => {
-    render(<ComponentUnderTest lazyMount unmountOnExit />)
-
-    expect(screen.queryByTestId('positioner')).not.toBeInTheDocument()
-
-    await user.click(screen.getByRole('button', { name: 'Open calendar' }))
-    expect(screen.getByTestId('positioner')).toBeInTheDocument()
-
-    await user.click(screen.getByRole('button', { name: 'Close calendar' }))
-    expect(screen.queryByTestId('positioner')).not.toBeInTheDocument()
-  }, 7000)
-})
