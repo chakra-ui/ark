bf2bc02c09c0243dbf4773f9219077cdbcab9a30
Michał Korczak <michal.k2001@gmail.com>
2024-01-21 02:02:57 +0100

    chore: wip

--- packages/frameworks/react/src/accordion/tests/accordion.test.tsx
+++ packages/frameworks/react/src/accordion/tests/accordion.test.tsx
@@ -3,30 +3,9 @@ import { accordionAnatomy } from '@ark-ui/anatomy'
 import { cleanup, render, screen } from '@testing-library/react/pure'
 import user from '@testing-library/user-event'
 import { describe, vi } from 'vitest'
-import { getExports, getParts } from '../setup-test'
-import { Accordion, type AccordionProps } from './'
-
-const ComponentUnderTest = (props: AccordionProps) => {
-  const items = [
-    { value: 'React' },
-    { value: 'Solid' },
-    { value: 'Svelte', disabled: true },
-    { value: 'Vue' },
-  ]
-  return (
-    <Accordion.Root {...props}>
-      {items.map((item, id) => (
-        <Accordion.Item key={id} value={item.value} disabled={item.disabled}>
-          <Accordion.ItemTrigger>
-            {item.value} Trigger
-            <Accordion.ItemIndicator>{'>'}</Accordion.ItemIndicator>
-          </Accordion.ItemTrigger>
-          <Accordion.ItemContent>{item.value} Content</Accordion.ItemContent>
-        </Accordion.Item>
-      ))}
-    </Accordion.Root>
-  )
-}
+import { Accordion } from '../'
+import { getExports, getParts } from '../../setup-test'
+import { ComponentUnderTest } from './basic'
 
 describe('Accordion / Parts & Exports', () => {
   afterAll(() => {
