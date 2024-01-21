73718adb7f554b88c475d8fa04b5dfe1206a4878
Michał Korczak <michal.k2001@gmail.com>
2024-01-21 02:24:26 +0100

    chore: wip

--- packages/frameworks/react/src/color-picker/tests/basic.tsx
+++ packages/frameworks/react/src/color-picker/tests/basic.tsx
@@ -1,10 +1,6 @@
-import { colorPickerAnatomy } from '@ark-ui/anatomy'
-import { render, screen } from '@testing-library/react'
-import user from '@testing-library/user-event'
-import { getExports, getParts } from '../setup-test'
-import { ColorPicker, type ColorPickerProps } from './'
+import { ColorPicker, type ColorPickerProps } from '../'
 
-const ComponentUnderTest = (props: ColorPickerProps) => (
+export const ComponentUnderTest = (props: ColorPickerProps) => (
   <ColorPicker.Root defaultValue="#eb5e41" {...props}>
     {(api) => (
       <>
@@ -68,39 +64,3 @@ const ComponentUnderTest = (props: ColorPickerProps) => (
     )}
   </ColorPicker.Root>
 )
-
-describe('ColorPicker', () => {
-  it.each(getParts(colorPickerAnatomy))('should render part %s', async (part) => {
-    render(<ComponentUnderTest />)
-    // eslint-disable-next-line testing-library/no-node-access
-    expect(document.querySelector(part)).toBeInTheDocument()
-  })
-
-  it.each(getExports(colorPickerAnatomy))('should export %s', async (part) => {
-    expect(ColorPicker[part]).toBeDefined()
-  })
-
-  it('should be able to lazy mount', async () => {
-    render(<ComponentUnderTest lazyMount />)
-
-    expect(screen.queryByTestId('positioner')).not.toBeInTheDocument()
-
-    await user.click(screen.getByTestId('trigger'))
-    expect(screen.getByTestId('positioner')).toBeInTheDocument()
-
-    await user.click(screen.getByTestId('trigger'))
-    expect(screen.getByTestId('positioner')).toBeInTheDocument()
-  }, 7000)
-
-  it('should lazy mount and unmount on exit', async () => {
-    render(<ComponentUnderTest lazyMount unmountOnExit />)
-
-    expect(screen.queryByTestId('positioner')).not.toBeInTheDocument()
-
-    await user.click(screen.getByTestId('trigger'))
-    expect(screen.getByTestId('positioner')).toBeInTheDocument()
-
-    await user.click(screen.getByTestId('trigger'))
-    expect(screen.queryByTestId('positioner')).not.toBeInTheDocument()
-  }, 7000)
-})
