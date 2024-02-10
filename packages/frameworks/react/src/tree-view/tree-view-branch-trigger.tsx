7f39332034d5b38967a2cac38ab5e3161e66c863
Michał Korczak <michal.k2001@gmail.com>
2024-02-10 18:01:55 +0100

    fix(react): tree view accesibility

--- packages/frameworks/react/src/tree-view/tree-view-branch-trigger.tsx
+++ packages/frameworks/react/src/tree-view/tree-view-branch-trigger.tsx
@@ -6,9 +6,9 @@ import { ark, type HTMLArkProps } from '../factory'
 import type { Assign } from '../types'
 import { useTreeViewContext, type BranchProps } from './tree-view-context'
 
-export interface TreeViewBranchTriggerProps extends Assign<HTMLArkProps<'button'>, BranchProps> {}
+export interface TreeViewBranchTriggerProps extends Assign<HTMLArkProps<'div'>, BranchProps> {}
 
-export const TreeViewBranchTrigger = forwardRef<HTMLButtonElement, TreeViewBranchTriggerProps>(
+export const TreeViewBranchTrigger = forwardRef<HTMLDivElement, TreeViewBranchTriggerProps>(
   (props, ref) => {
     const [branchProps, { ...localProps }] = createSplitProps<BranchProps>()(props, [
       'depth',
@@ -18,7 +18,7 @@ export const TreeViewBranchTrigger = forwardRef<HTMLButtonElement, TreeViewBranc
     const api = useTreeViewContext()
     const mergedProps = mergeProps(api.getBranchTriggerProps(branchProps), localProps)
 
-    return <ark.button {...mergedProps} ref={ref} />
+    return <ark.div {...mergedProps} ref={ref} />
   },
 )
 
