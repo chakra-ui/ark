4a482b3443d229c813045b0f92ef5613fcae34e1
carwack <sybren.willemot@gmail.com>
2024-02-11 02:34:05 +0100

    feat(store): add nanostore presistent to use localStorage

--- packages/website/src/stores/frameworkSelect.store.ts
+++ packages/website/src/stores/frameworkSelect.store.ts
@@ -1,3 +1,3 @@
-import { atom } from 'nanostores'
+import { persistentAtom } from '@nanostores/persistent'
 
-export const selectedFramework = atom('vue')
+export const selectedFramework = persistentAtom<string>('ark-ui-framework', 'react')
