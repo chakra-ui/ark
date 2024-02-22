c00e0b5c1210ede46d3f37485ce8e64c1af9b608
iamdin <iamdinq@gmail.com>
2024-02-23 00:46:35 +0800

    perf(date-picker): standalone date-picker examples

--- packages/frameworks/vue/src/date-picker/stories/date-picker.story.vue
+++ packages/frameworks/vue/src/date-picker/stories/date-picker.story.vue
@@ -126,6 +126,48 @@ import './date-picker.css'
             </DatePicker.TableBody>
           </DatePicker.Table>
         </DatePicker.View>
+        <DatePicker.View view="month" #default="api">
+          <DatePicker.ViewControl>
+            <DatePicker.PrevTrigger>Prev</DatePicker.PrevTrigger>
+            <DatePicker.ViewTrigger>
+              <DatePicker.RangeText />
+            </DatePicker.ViewTrigger>
+            <DatePicker.NextTrigger>Next</DatePicker.NextTrigger>
+          </DatePicker.ViewControl>
+          <DatePicker.Table>
+            <DatePicker.TableBody>
+              <DatePicker.TableRow
+                v-for="(months, id) in api.getMonthsGrid({ columns: 4, format: 'short' })"
+                :key="id"
+              >
+                <DatePicker.TableCell v-for="(month, id) in months" :key="id" :value="month.value">
+                  <DatePicker.TableCellTrigger>{{ month.label }}</DatePicker.TableCellTrigger>
+                </DatePicker.TableCell>
+              </DatePicker.TableRow>
+            </DatePicker.TableBody>
+          </DatePicker.Table>
+        </DatePicker.View>
+        <DatePicker.View view="year" #default="api">
+          <DatePicker.ViewControl>
+            <DatePicker.PrevTrigger>Prev</DatePicker.PrevTrigger>
+            <DatePicker.ViewTrigger>
+              <DatePicker.RangeText />
+            </DatePicker.ViewTrigger>
+            <DatePicker.NextTrigger>Next</DatePicker.NextTrigger>
+          </DatePicker.ViewControl>
+          <DatePicker.Table>
+            <DatePicker.TableBody>
+              <DatePicker.TableRow
+                v-for="(years, id) in api.getYearsGrid({ columns: 4 })"
+                :key="id"
+              >
+                <DatePicker.TableCell v-for="(year, id) in years" :key="id" :value="year.value">
+                  <DatePicker.TableCellTrigger>{{ year.label }}</DatePicker.TableCellTrigger>
+                </DatePicker.TableCell>
+              </DatePicker.TableRow>
+            </DatePicker.TableBody>
+          </DatePicker.Table>
+        </DatePicker.View>
       </DatePicker.Root>
     </Variant>
   </Story>
