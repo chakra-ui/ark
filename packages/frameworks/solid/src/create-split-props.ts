d1fcab26c1e72251447d2595b056d48ba449b4ae
Minsu <52266597+Gaic4o@users.noreply.github.com>
2024-03-14 23:05:10 +0900

    refactor(react): simplify ensureKeys type definition (#2276)

--- packages/frameworks/solid/src/create-split-props.ts
+++ packages/frameworks/solid/src/create-split-props.ts
@@ -3,11 +3,9 @@ import { splitProps } from 'solid-js'
 type EnsureKeys<
   ExpectedKeys extends (keyof Target)[],
   Target,
-> = ExpectedKeys extends (keyof Target)[]
-  ? [keyof Target] extends [ExpectedKeys[number]]
-    ? unknown
-    : `Missing required keys: ${Exclude<keyof Target, ExpectedKeys[number]>}`
-  : never
+> = keyof Target extends ExpectedKeys[number]
+  ? unknown
+  : `Missing required keys: ${Exclude<keyof Target, ExpectedKeys[number]> & string}`
 
 export const createSplitProps =
   <Target extends Record<never, never>>() =>
