e8a1c7bb50a3bdf0f0cd27ad5c2dd15aada7d9d7
Christian Schröter <dev@grizzly.codes>
2024-03-29 14:30:40 +0100

    chore: wip

--- scripts/src/release-prepare.ts
+++ scripts/src/release-prepare.ts
@@ -4,6 +4,11 @@ import { globby } from 'globby'
 import { basename, dirname, join, parse } from 'path'
 import { match } from 'ts-pattern'
 
+/**
+ * This script generates will update the package.json files of all frameworks and add the following:
+ * - Keywords: The name of the components in the framework
+ * - Exports: The exports of the framework
+ */
 const generateExports = async (pkgJsonPath: string) => {
   const paths = await globby(
     ['src/*/index.ts', 'src/index.ts', 'src/portal.tsx', 'src/factory.{tsx,ts}'],
@@ -53,13 +58,14 @@ const main = async () => {
   const packages = await globby([join(root, 'frameworks/*/package.json')])
 
   for (const pkg of packages) {
+    console.log(`Updating keywords and exports for ${basename(dirname(pkg))}`)
     const packageJson = await fs.readJson(pkg)
 
     const keywords = await generateKeywords(pkg)
     const exports = await generateExports(pkg)
 
     packageJson.keywords = keywords
-    packageJson.exports = exports
+    packageJson.exports = pkg.includes('solid/package.json') ? packageJson.exports : exports
 
     await fs.writeJson(pkg, packageJson, { spaces: 2 })
   }
