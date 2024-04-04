9658554197a40a51f5bc07ed0e986172b03d48d1
Segun Adebayo <joseshegs@gmail.com>
2024-04-04 20:48:02 +0100

    refactor: avatar

--- frameworks/solid/src/avatar/use-avatar.ts
+++ frameworks/solid/src/avatar/use-avatar.ts
@@ -1,7 +1,8 @@
 import * as avatar from '@zag-js/avatar'
-import { mergeProps, normalizeProps, useMachine, type PropTypes } from '@zag-js/solid'
+import { normalizeProps, useMachine, type PropTypes } from '@zag-js/solid'
 import { createMemo, createUniqueId, type Accessor } from 'solid-js'
 import { useEnvironmentContext } from '../environment'
+import { mergeProps } from '../merge-props'
 import { type Optional } from '../types'
 
 export interface UseAvatarProps extends Optional<avatar.Context, 'id'> {}
@@ -10,7 +11,7 @@ export interface UseAvatarReturn extends Accessor<avatar.Api<PropTypes>> {}
 export const useAvatar = (props: UseAvatarProps): UseAvatarReturn => {
   const getRootNode = useEnvironmentContext()
   const context = mergeProps({ id: createUniqueId(), getRootNode }, props)
-  const [state, send] = useMachine(avatar.machine(context()), { context })
+  const [state, send] = useMachine(avatar.machine(context), { context })
 
   return createMemo(() => avatar.connect(state, send, normalizeProps))
 }
