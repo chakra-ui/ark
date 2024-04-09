f28c8351507e79d3074af78037c7df5eca2de876
Christian Schröter <dev@grizzly.codes>
2024-04-09 10:30:21 +0200

    feat(website): show root props first

--- website/src/components/docs/component-props-table.tsx
+++ website/src/components/docs/component-props-table.tsx
@@ -16,66 +16,70 @@ export const ComponentPropsTable = (props: Props) => {
 
   return types ? (
     <Stack gap="6" className="not-prose">
-      {Object.entries(types.data).map(([key, properties]) => (
-        <Stack key={key} gap="4" className="not-prose">
-          <Heading textStyle="xl" fontWeight="semibold" color="fg.default">
-            {key}
-          </Heading>
-          <Stack overflowX="auto">
-            <Table.Root variant="outline" size="sm">
-              <Table.Head>
-                <Table.Row>
-                  <Table.Header>Prop</Table.Header>
-                  <Table.Header>Type</Table.Header>
-                  <Table.Header>Default</Table.Header>
-                </Table.Row>
-              </Table.Head>
-              <Table.Body>
-                {Object.entries(properties).map(([name, property]) => (
-                  <Table.Row key={name}>
-                    <Table.Cell width="40">
-                      <HStack>
-                        <Code size="sm" color="accent.default">
-                          {name}
-                          {property.isRequired && <AsteriskIcon size="10" />}
-                        </Code>
-                        {property.description && (
-                          <Popover.Root portalled>
-                            <Popover.Trigger asChild>
-                              <Button size="xs" px="0" variant="link" color="fg.subtle">
-                                <HelpCircle />
-                              </Button>
-                            </Popover.Trigger>
-                            <Portal>
-                              <Popover.Positioner>
-                                <Popover.Content>
-                                  <Popover.Description>{property.description}</Popover.Description>
-                                </Popover.Content>
-                              </Popover.Positioner>
-                            </Portal>
-                          </Popover.Root>
-                        )}
-                      </HStack>
-                    </Table.Cell>
-                    <Table.Cell height="auto">
-                      <Code size="sm">{property.type}</Code>
-                    </Table.Cell>
-                    <Table.Cell width="12" textAlign="center">
-                      {property.defaultValue ? (
-                        <Code size="sm">{property.defaultValue}</Code>
-                      ) : (
-                        <Icon size="xs" color="fg.muted">
-                          <MinusIcon />
-                        </Icon>
-                      )}
-                    </Table.Cell>
+      {Object.entries(types.data)
+        .sort(([key]) => (key === 'Root' ? -1 : 1))
+        .map(([key, properties]) => (
+          <Stack key={key} gap="4" className="not-prose">
+            <Heading textStyle="xl" fontWeight="semibold" color="fg.default">
+              {key}
+            </Heading>
+            <Stack overflowX="auto">
+              <Table.Root variant="outline" size="sm">
+                <Table.Head>
+                  <Table.Row>
+                    <Table.Header>Prop</Table.Header>
+                    <Table.Header>Type</Table.Header>
+                    <Table.Header>Default</Table.Header>
                   </Table.Row>
-                ))}
-              </Table.Body>
-            </Table.Root>
+                </Table.Head>
+                <Table.Body>
+                  {Object.entries(properties).map(([name, property]) => (
+                    <Table.Row key={name}>
+                      <Table.Cell width="40">
+                        <HStack>
+                          <Code size="sm" color="accent.default">
+                            {name}
+                            {property.isRequired && <AsteriskIcon size="10" />}
+                          </Code>
+                          {property.description && (
+                            <Popover.Root portalled>
+                              <Popover.Trigger asChild>
+                                <Button size="xs" px="0" variant="link" color="fg.subtle">
+                                  <HelpCircle />
+                                </Button>
+                              </Popover.Trigger>
+                              <Portal>
+                                <Popover.Positioner>
+                                  <Popover.Content>
+                                    <Popover.Description>
+                                      {property.description}
+                                    </Popover.Description>
+                                  </Popover.Content>
+                                </Popover.Positioner>
+                              </Portal>
+                            </Popover.Root>
+                          )}
+                        </HStack>
+                      </Table.Cell>
+                      <Table.Cell height="auto">
+                        <Code size="sm">{property.type}</Code>
+                      </Table.Cell>
+                      <Table.Cell width="12" textAlign="center">
+                        {property.defaultValue ? (
+                          <Code size="sm">{property.defaultValue}</Code>
+                        ) : (
+                          <Icon size="xs" color="fg.muted">
+                            <MinusIcon />
+                          </Icon>
+                        )}
+                      </Table.Cell>
+                    </Table.Row>
+                  ))}
+                </Table.Body>
+              </Table.Root>
+            </Stack>
           </Stack>
-        </Stack>
-      ))}
+        ))}
     </Stack>
   ) : null
 }
