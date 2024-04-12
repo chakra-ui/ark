3d7616049c1d0af3cee320c4cc3cafd36300639d
Christian Schröter <dev@grizzly.codes>
2024-04-12 13:38:49 +0200

    chore: wip

---
+++ frameworks/react/src/environment/environment.tsx
@@ -0,0 +1,28 @@
+import { type ReactNode, useMemo, useRef } from 'react'
+import { runIfFn } from '../run-if-fn'
+import { EnvironmentProvider } from './use-environment-context'
+
+export type RootNode = ShadowRoot | Document | Node
+
+export interface EnvironmentProps {
+  children?: ReactNode
+  value?: RootNode | (() => RootNode)
+}
+
+export const Environment = (props: EnvironmentProps) => {
+  const { value, children } = props
+  const ref = useRef<HTMLSpanElement>(null)
+
+  const getRootNode = useMemo(() => {
+    return () => runIfFn(value) ?? ref.current?.ownerDocument ?? document
+  }, [value])
+
+  const showSpan = !value
+
+  return (
+    <EnvironmentProvider value={getRootNode}>
+      {children}
+      {showSpan && <span data-ark-env="" hidden ref={ref} />}
+    </EnvironmentProvider>
+  )
+}
