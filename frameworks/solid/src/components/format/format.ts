4a9afeada088dc30188ae9d632cb537cd43ebceb
Christian Schröter <dev@grizzly.codes>
2024-04-16 14:51:09 +0200

    fix: resolve new lint rules

--- frameworks/solid/src/components/format/format.ts
+++ frameworks/solid/src/components/format/format.ts
@@ -1,3 +1,2 @@
 export { FormatByte as Byte, type FormatByteProps as ByteProps } from './format-byte'
-// biome-ignore lint/suspicious/noShadowRestrictedNames: <explanation>
 export { FormatNumber as Number, type FormatNumberProps as NumberProps } from './format-number'
