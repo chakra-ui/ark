54c7879c2ac731ea8728c1697ea2656a0f32624d
Christian Schröter <dev@grizzly.codes>
2024-04-17 18:29:30 +0200

    chore: wip

--- frameworks/vue-sfc/vite.config.ts
+++ frameworks/vue-sfc/vite.config.ts
@@ -1,6 +1,54 @@
+import { copyFileSync } from 'node:fs'
 import vue from '@vitejs/plugin-vue'
+import { globbySync } from 'globby'
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+import pkg from './package.json'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    dts({
+      entryRoot: 'src',
+      staticImport: true,
+      exclude: ['**/*.stories.vue', '**/examples/*', '**/setup-test.ts'],
+      afterBuild: () => {
+        globbySync(['dist/**/*.d.ts', 'dist/**.d.ts']).map((file) => {
+          copyFileSync(file, file.replace(/\.d\.ts$/, '.d.cts'))
+        })
+      },
+    }),
+    vue(),
+  ],
+  build: {
+    target: 'esnext',
+    minify: false,
+    lib: {
+      entry: globbySync('src/**/index.ts'),
+      formats: ['es', 'cjs'],
+      fileName: (format) => (format === 'es' ? 'index.js' : 'index.cjs'),
+    },
+    rollupOptions: {
+      external: [
+        ...Object.keys(pkg.dependencies ?? {}),
+        ...Object.keys(pkg.peerDependencies ?? {}),
+        'vue',
+      ],
+      output: [
+        {
+          format: 'cjs',
+          preserveModules: true,
+          preserveModulesRoot: 'src',
+          exports: 'named',
+          entryFileNames: '[name].cjs',
+        },
+        {
+          format: 'es',
+          preserveModules: true,
+          preserveModulesRoot: 'src',
+          exports: 'named',
+          entryFileNames: '[name].js',
+        },
+      ],
+    },
+  },
 })
