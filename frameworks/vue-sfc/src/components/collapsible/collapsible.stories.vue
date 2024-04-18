7127adf0e70f579756d1e0dd81efb5f4ffe438e8
Christian Schröter <dev@grizzly.codes>
2024-04-18 09:23:52 +0200

    chore: wip

--- frameworks/vue-sfc/src/components/collapsible/collapsible.stories.vue
+++ frameworks/vue-sfc/src/components/collapsible/collapsible.stories.vue
@@ -1,11 +1,11 @@
 <script setup>
-import Basic from './examples/basic.vue'
+// import Basic from './examples/basic.vue'
 </script>
 
 <template>
   <Story title="Collapsible">
     <Variant title="Basic">
-      <Basic />
+    <h1>Hello World</h1>
     </Variant>
   </Story>
 </template>
