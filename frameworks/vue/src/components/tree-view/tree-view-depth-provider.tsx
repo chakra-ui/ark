5143dd7f269459b7300592d99e7365a4a7b94c81
Christian Schröter <dev@grizzly.codes>
2024-04-19 17:04:05 +0200

    chore: wip

--- frameworks/vue/src/components/tree-view/tree-view-depth-provider.tsx
+++
@@ -1,6 +0,0 @@
-import { createContext } from '../../utils'
-
-export type TreeViewDepthContext = number
-
-export const [TreeViewDepthProvider, useTreeViewDepthContext] =
-  createContext<TreeViewDepthContext>('TreeViewDepthContext')
