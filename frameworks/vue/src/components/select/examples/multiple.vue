a5e2f28d8da624c46b6fad4bb5622afdfcfa7cce
Sybren W <sybren.willemot@gmail.com>
2024-04-22 18:04:04 +0200

    feat(select): add missing import for chevron down icon

--- frameworks/vue/src/components/select/examples/multiple.vue
+++ frameworks/vue/src/components/select/examples/multiple.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import { Select } from '../..'
+import { ChevronDownIcon } from './icons'
 
 const items = ref([
   { label: 'React', value: 'react' },
