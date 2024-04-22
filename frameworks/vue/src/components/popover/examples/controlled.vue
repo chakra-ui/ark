a498e4fef8e11eff6d5410c1c1422da3f050e683
Sybren W <sybren.willemot@gmail.com>
2024-04-22 17:54:49 +0200

    fix(popover): remove Fragment from story since vue 3 support multi root elements

--- frameworks/vue/src/components/popover/examples/controlled.vue
+++ frameworks/vue/src/components/popover/examples/controlled.vue
@@ -6,17 +6,15 @@ const open = ref(false)
 </script>
 
 <template>
-  <Fragment>
-    <button @click="() => (open = !open)">toggle</button>
-    <Popover.Root v-model:open="open">
-      <Popover.Anchor>Anchor</Popover.Anchor>
-      <Popover.Positioner>
-        <Popover.Content>
-          <Popover.Title>Title</Popover.Title>
-          <Popover.Description>Description</Popover.Description>
-          <Popover.CloseTrigger>Close</Popover.CloseTrigger>
-        </Popover.Content>
-      </Popover.Positioner>
-    </Popover.Root>
-  </Fragment>
+  <button @click="() => (open = !open)">toggle</button>
+  <Popover.Root v-model:open="open">
+    <Popover.Anchor>Anchor</Popover.Anchor>
+    <Popover.Positioner>
+      <Popover.Content>
+        <Popover.Title>Title</Popover.Title>
+        <Popover.Description>Description</Popover.Description>
+        <Popover.CloseTrigger>Close</Popover.CloseTrigger>
+      </Popover.Content>
+    </Popover.Positioner>
+  </Popover.Root>
 </template>
