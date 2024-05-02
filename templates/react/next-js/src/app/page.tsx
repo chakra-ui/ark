5143594e29c1fb727d65e1d3c438945b61fd52c9
Christian Schröter <dev@grizzly.codes>
2024-05-02 22:41:43 +0200

    chore: update template

--- templates/react/next-js/src/app/page.tsx
+++ templates/react/next-js/src/app/page.tsx
@@ -6,11 +6,10 @@ export default function Home() {
     <main>
       <h1>Welcome to Ark UI</h1>
       <Avatar
-        onStatusChange={(status) => console.log(status)}
         name="Christian Schröter"
         src="https://avatars.githubusercontent.com/u/1846056?s=400&u=bc2821d6154517e6f62795b11ffe0e8e001764a5&v=4"
       />
-      <Checkbox onCheckedChange={(e) => console.log(e.checked)} />
+      <Checkbox />
     </main>
   )
 }
