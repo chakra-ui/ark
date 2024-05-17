b1868619cfbc3fb9e38ad0e20d9a83b8cf4facb9
Christian Schröter <dev@grizzly.codes>
2024-05-17 16:17:51 +0200

    feat(solid): revise asChild factory

--- packages/solid/src/components/checkbox/checkbox-hidden-input.tsx
+++ packages/solid/src/components/checkbox/checkbox-hidden-input.tsx
@@ -2,7 +2,7 @@ import { mergeProps } from '@zag-js/solid'
 import { type HTMLArkProps, ark } from '../factory'
 import { useCheckboxContext } from './use-checkbox-context'
 
-export interface CheckboxHiddenInputProps extends HTMLArkProps<'div'> {}
+export interface CheckboxHiddenInputProps extends HTMLArkProps<'input'> {}
 
 export const CheckboxHiddenInput = (props: CheckboxHiddenInputProps) => {
   const checkbox = useCheckboxContext()
