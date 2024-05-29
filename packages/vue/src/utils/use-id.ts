d7c8f82ad910428a3bb9a51e681a8d8de0713e87
Christian Schröter <dev@grizzly.codes>
2024-05-29 19:01:04 +0200

    docs: document useId fn

--- packages/vue/src/utils/use-id.ts
+++ packages/vue/src/utils/use-id.ts
@@ -1,3 +1,8 @@
+/**
+ * Simple custom hook to generate unique ids
+ * Workaround until Vue 3.5.0 is released
+ * @see https://x.com/youyuxi/status/1745379112456429688
+ */
 let count = 0
 
 export const useId = (id?: string | null | undefined, prefix = 'ark') =>
