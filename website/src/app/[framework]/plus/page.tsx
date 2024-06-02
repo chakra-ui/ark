2f9f85544e9e72d9968502deedd1be4a9895fe7a
Christian Schröter <dev@grizzly.codes>
2024-06-02 13:04:39 +0200

    feat(website): add faq and contact section

--- website/src/app/[framework]/plus/page.tsx
+++ website/src/app/[framework]/plus/page.tsx
@@ -1,21 +1,25 @@
 import type { Metadata } from 'next'
 import { Container, Stack } from 'styled-system/jsx'
 import { PageHeader } from '~/components/page-header'
+import { Faqs } from '~/components/plus/faqs'
+import { GetInTouch } from '~/components/plus/get-in-touch'
 import { PricingCard } from '~/components/plus/pricing-card'
 
 export default function Page() {
   return (
-    <Container py="12" maxW="4xl">
-      <Stack gap={{ base: '16', md: '24' }}>
+    <Container py="12" maxW="7xl">
+      <Stack gap={{ base: '16', md: '24' }} alignItems="center">
         <PageHeader
           heading="Ark UI Plus"
           subHeading="Pricing"
           description="Unlock exclusive examples and support the development of Ark UI"
         />
-        <Stack direction={{ base: 'column', sm: 'row' }} gap="8" align="start">
+        <Stack direction={{ base: 'column', sm: 'row' }} gap="8" align="start" maxW="4xl">
           <PricingCard variant="personal" />
           <PricingCard variant="team" />
         </Stack>
+        <Faqs />
+        <GetInTouch />
       </Stack>
     </Container>
   )
---
+++ website/src/components/plus/get-in-touch.tsx
@@ -0,0 +1,43 @@
+import { Container, Stack } from 'styled-system/jsx'
+import { Text } from '~/components/ui'
+import { AvatarGroup } from '../ui/avatar-group'
+import { MailToSupportButton } from './mail-to-support'
+
+export const GetInTouch = () => {
+  return (
+    <Stack
+      gap={{ base: '6', lg: '8' }}
+      px={{ base: '5', lg: '8' }}
+      py="8"
+      justify="center"
+      align="center"
+      borderRadius="2xl"
+      bg="bg.subtle"
+      borderWidth="1px"
+      width="full"
+    >
+      <AvatarGroup
+        avatars={[
+          { name: 'Segun Adebayo', src: 'https://avatars2.githubusercontent.com/u/6916170' },
+          {
+            name: 'Christian Schröter',
+            src: 'https://avatars3.githubusercontent.com/u/1846056',
+          },
+        ]}
+      />
+
+      <Stack textAlign="center">
+        <Text textStyle={{ base: 'lg', md: 'xl' }} fontWeight="semibold">
+          Still have questions?
+        </Text>
+        <Text textStyle="lg" color="fg.muted">
+          Can't find the answer you're looking for? Our team is happy to answer your questions.
+        </Text>
+      </Stack>
+
+      <MailToSupportButton subject="Question about Ark Plus" size="xl" alignSelf="center">
+        Send us an email
+      </MailToSupportButton>
+    </Stack>
+  )
+}
