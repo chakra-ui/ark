2e64b6efc62e48fbc682b23a4e6295448bc7291a
Carlo <hola.carlodominguez@gmail.com>
2024-06-12 22:23:03 +0200

    fix: fixes inconsistent React `Dialog.Backdrop` `presence` prop

--- packages/react/src/components/dialog/dialog-backdrop.tsx
+++ packages/react/src/components/dialog/dialog-backdrop.tsx
@@ -1,16 +1,14 @@
 import { mergeProps } from '@zag-js/react'
 import { forwardRef } from 'react'
-import { useRenderStrategyPropsContext } from '../../utils/render-strategy'
 import { type HTMLArkProps, ark } from '../factory'
-import { usePresence } from '../presence'
+import { usePresenceContext } from '../presence'
 import { useDialogContext } from './use-dialog-context'
 
 export interface DialogBackdropProps extends HTMLArkProps<'div'> {}
 
 export const DialogBackdrop = forwardRef<HTMLDivElement, DialogBackdropProps>((props, ref) => {
   const dialog = useDialogContext()
-  const renderStrategyProps = useRenderStrategyPropsContext()
-  const presence = usePresence({ ...renderStrategyProps, present: dialog.open })
+  const presence = usePresenceContext()
   const mergedProps = mergeProps(dialog.getBackdropProps(), presence.getPresenceProps(ref), props)
 
   if (presence.unmounted) {
