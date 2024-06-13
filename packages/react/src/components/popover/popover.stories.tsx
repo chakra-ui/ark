e5d987422762d988809e457beaa27d931c5df390
Segun Adebayo <joseshegs@gmail.com>
2024-06-13 14:02:52 +0100

    chore(react): add popover + dialog composition

--- packages/react/src/components/popover/popover.stories.tsx
+++ packages/react/src/components/popover/popover.stories.tsx
@@ -6,12 +6,13 @@ const meta: Meta = {
 
 export default meta
 
-export { Basic } from './examples/basic'
-export { Portalled } from './examples/portalled'
-export { OnOpenChange } from './examples/on-open-change'
-export { Controlled } from './examples/controlled'
-export { RenderFn } from './examples/render-fn'
 export { Arrow } from './examples/arrow'
+export { Basic } from './examples/basic'
 export { CloseBehavior } from './examples/close-behavior'
-export { Positioning } from './examples/positioning'
+export { Controlled } from './examples/controlled'
 export { Modal } from './examples/modal'
+export { OnOpenChange } from './examples/on-open-change'
+export { Portalled } from './examples/portalled'
+export { Positioning } from './examples/positioning'
+export { RenderFn } from './examples/render-fn'
+export { WithDialog } from './examples/with-dialog'
