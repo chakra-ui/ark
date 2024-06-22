23f4c6d4e71e026808c075c3f211697b3827d856
Christian Schröter <dev@grizzly.codes>
2024-06-22 21:41:54 +0200

    chore: vue checkbox

---
+++ packages/vue/src/components/checkbox/examples/with-field.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { Checkbox, Field } from '../..'
+import { CheckIcon } from './icons'
+</script>
+
+<template>
+  <Field.Root>
+    <Checkbox.Root>
+      <Checkbox.Label>Label</Checkbox.Label>
+      <Checkbox.Control>
+        <Checkbox.Indicator>
+          <CheckIcon />
+        </Checkbox.Indicator>
+        <Checkbox.Indicator indeterminate>
+          <MinusIcon />
+        </Checkbox.Indicator>
+      </Checkbox.Control>
+      <Checkbox.HiddenInput />
+    </Checkbox.Root>
+    <Field.HelperText>Additional Info</Field.HelperText>
+    <Field.ErrorText>Error Info</Field.ErrorText>
+  </Field.Root>
+</template>
--- packages/vue/src/components/checkbox/tests/checkbox.test.ts
+++ packages/vue/src/components/checkbox/tests/checkbox.test.ts
@@ -3,6 +3,7 @@ import user from '@testing-library/user-event'
 import { render, screen } from '@testing-library/vue'
 import { Checkbox } from '../'
 import { getExports, getParts } from '../../../setup-test'
+import WithField from '../examples/with-field.vue'
 import ComponentUnderTest from './checkbox.test.vue'
 import ControlledComponentUnderTest from './controlled-checkbox.test.vue'
 
@@ -38,5 +39,43 @@ describe('Checkbox', () => {
   it('should handle indeterminate state from example', async () => {
     render(ComponentUnderTest, { props: { checked: 'indeterminate' } })
     expect(screen.getByTestId('control')).toHaveAttribute('data-state', 'indeterminate')
+  })
+})
+
+describe('Checkbox / Field', () => {
+  it('should set checkbox as required', async () => {
+    render(WithField, { props: { required: true } })
+    expect(screen.getByRole('checkbox', { name: /label/i })).toBeRequired()
+  })
+
+  it('should set input as disabled', async () => {
+    render(WithField, { props: { disabled: true } })
+    expect(screen.getByRole('checkbox', { name: /label/i })).toBeDisabled()
+  })
+
+  it('should set input as readonly', async () => {
+    render(WithField, { props: { readOnly: true } })
+    expect(screen.getByText('Label')).toHaveAttribute('data-readonly')
+  })
+
+  it('should display helper text', async () => {
+    render(WithField)
+    expect(screen.getByText('Additional Info')).toBeInTheDocument()
+  })
+
+  it('should display error text when error is present', async () => {
+    render(WithField, { props: { invalid: true } })
+    expect(screen.getByText('Error Info')).toBeInTheDocument()
+  })
+
+  it('should focus on input when label is clicked', async () => {
+    render(WithField)
+    await user.click(screen.getByText(/label/i))
+    expect(screen.getByRole('checkbox', { name: /label/i })).toHaveFocus()
+  })
+
+  it('should not display error text when no error is present', async () => {
+    render(WithField)
+    expect(screen.queryByText('Error Info')).not.toBeInTheDocument()
   })
 })
