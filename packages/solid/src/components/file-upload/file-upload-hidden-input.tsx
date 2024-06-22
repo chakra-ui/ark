3754358a9b15d62c96e219b38b3f72368950fb80
Christian Schröter <dev@grizzly.codes>
2024-06-22 21:18:09 +0200

    chore: missing aria describedby

--- packages/solid/src/components/file-upload/file-upload-hidden-input.tsx
+++ packages/solid/src/components/file-upload/file-upload-hidden-input.tsx
@@ -1,5 +1,6 @@
 import { mergeProps } from '@zag-js/solid'
 import { type HTMLProps, type PolymorphicProps, ark } from '../factory'
+import { useFieldContext } from '../field'
 import { useFileUploadContext } from './use-file-upload-context'
 
 export interface FileUploadHiddenInputBaseProps extends PolymorphicProps<'input'> {}
@@ -10,6 +11,7 @@ export interface FileUploadHiddenInputProps
 export const FileUploadHiddenInput = (props: FileUploadHiddenInputProps) => {
   const fileUpload = useFileUploadContext()
   const mergedProps = mergeProps(() => fileUpload().getHiddenInputProps(), props)
+  const field = useFieldContext()
 
-  return <ark.input {...mergedProps} />
+  return <ark.input aria-describedby={field?.ariaDescribedby} {...mergedProps} />
 }
