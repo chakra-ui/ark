7c2f1f3173e74237ca8f9f724026d878a75dbf01
Christian Schröter <dev@grizzly.codes>
2024-06-22 20:47:54 +0200

    chore: number input solid

--- packages/solid/src/components/number-input/tests/number-input.test.tsx
+++ packages/solid/src/components/number-input/tests/number-input.test.tsx
@@ -3,6 +3,7 @@ import { fireEvent, render, screen, waitFor } from '@solidjs/testing-library'
 import user from '@testing-library/user-event'
 import { NumberInput } from '../'
 import { getExports, getParts } from '../../../setup-test'
+import { WithField } from '../examples/with-field'
 import { ComponentUnderTest } from './basic'
 
 describe('NumberInput', () => {
@@ -89,5 +90,43 @@ describe('NumberInput', () => {
     await waitFor(() => {
       expect(input).toHaveValue('1.123')
     })
+  })
+})
+
+describe('NumberInput / Field', () => {
+  it('should set input as required', async () => {
+    render(() => <WithField required />)
+    expect(screen.getByRole('spinbutton', { name: /label/i })).toBeRequired()
+  })
+
+  it('should set input as disabled', async () => {
+    render(() => <WithField disabled />)
+    expect(screen.getByRole('spinbutton', { name: /label/i })).toBeDisabled()
+  })
+
+  it('should set input as readonly', async () => {
+    render(() => <WithField readOnly />)
+    expect(screen.getByRole('spinbutton', { name: /label/i })).toHaveAttribute('readonly')
+  })
+
+  it('should display helper text', async () => {
+    render(() => <WithField />)
+    expect(screen.getByText('Additional Info')).toBeInTheDocument()
+  })
+
+  it('should display error text when error is present', async () => {
+    render(() => <WithField invalid />)
+    expect(screen.getByText('Error Info')).toBeInTheDocument()
+  })
+
+  it('should focus on input when label is clicked', async () => {
+    render(() => <WithField />)
+    await user.click(screen.getByText(/label/i))
+    expect(screen.getByRole('spinbutton', { name: /label/i })).toHaveFocus()
+  })
+
+  it('should not display error text when no error is present', async () => {
+    render(() => <WithField />)
+    expect(screen.queryByText('Error Info')).not.toBeInTheDocument()
   })
 })
