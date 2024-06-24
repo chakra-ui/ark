8f41107e49e4fb742d4b483a47a53772af5468a4
Christian Schröter <dev@grizzly.codes>
2024-06-24 22:05:22 +0200

    chore: docs

--- packages/vue/src/components/field/use-field.ts
+++ packages/vue/src/components/field/use-field.ts
@@ -6,9 +6,21 @@ const parts = fieldAnatomy.build()
 
 export interface UseFieldProps {
   id?: string
+  /**
+   * Indicates whether the field is required.
+   */
   required?: boolean
+  /**
+   * Indicates whether the field is disabled.
+   */
   disabled?: boolean
+  /**
+   * Indicates whether the field is invalid.
+   */
   invalid?: boolean
+  /**
+   * Indicates whether the field is read-only.
+   */
   readOnly?: boolean
 }
 
