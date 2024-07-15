1b26360e26e76ea1e4038459d7d1ffd22f7401b5
Segun Adebayo <joseshegs@gmail.com>
2024-07-15 19:47:03 +0100

    docs: fix toggle group example

--- packages/solid/src/components/toggle-group/examples/multiple.tsx
+++ packages/solid/src/components/toggle-group/examples/multiple.tsx
@@ -2,7 +2,7 @@ import { ToggleGroup } from '../..'
 
 export const Multiple = () => {
   return (
-    <ToggleGroup.Root>
+    <ToggleGroup.Root multiple>
       <ToggleGroup.Item value="a">A</ToggleGroup.Item>
       <ToggleGroup.Item value="b">B</ToggleGroup.Item>
       <ToggleGroup.Item value="c">C</ToggleGroup.Item>
