e44280821583aefcaec9b29630130d5107340150
Sybren W <sybren.willemot@gmail.com>
2024-07-20 23:39:37 +0200

    fix(docs): fix z-index of docs navbar to 2

--- website/src/components/navigation/docs/docs-navbar.tsx
+++ website/src/components/navigation/docs/docs-navbar.tsx
@@ -20,7 +20,7 @@ export const DocsNavbar = () => {
       right="0"
       top="16"
       bg="bg.canvas"
-      zIndex="1"
+      zIndex="2"
     >
       <MobileSidebarContainer>
         <DocsSidebar groups={groups} />
