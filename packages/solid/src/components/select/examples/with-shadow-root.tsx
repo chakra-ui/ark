3ceb554bf44f65dba0c082a68291879204e6bb8c
Segun Adebayo <joseshegs@gmail.com>
2024-07-24 15:30:35 +0100

    chore(solid): add shadow root example

---
+++ packages/solid/src/components/select/examples/with-shadow-root.tsx
@@ -0,0 +1,39 @@
+import { EnvironmentProvider, Select } from '@ark-ui/solid'
+import { Index, Portal } from 'solid-js/web'
+
+const items = ['React', 'Solid', 'Vue']
+
+export const WithShadowRoot = () => {
+  let portalNode: (HTMLDivElement & { shadowRoot: ShadowRoot }) | undefined
+  return (
+    <Portal ref={portalNode} useShadow={true}>
+      <EnvironmentProvider value={() => portalNode?.shadowRoot ?? document}>
+        <Select.Root items={items}>
+          <Select.Label>Framework</Select.Label>
+          <Select.Control>
+            <Select.Trigger>
+              <Select.ValueText placeholder="Select a Framework" />
+              <Select.Indicator>▼</Select.Indicator>
+            </Select.Trigger>
+            <Select.ClearTrigger>Clear</Select.ClearTrigger>
+          </Select.Control>
+          <Select.Positioner>
+            <Select.Content>
+              <Select.ItemGroup>
+                <Index each={items}>
+                  {(item) => (
+                    <Select.Item item={item()}>
+                      <Select.ItemText>{item()}</Select.ItemText>
+                      <Select.ItemIndicator>✓</Select.ItemIndicator>
+                    </Select.Item>
+                  )}
+                </Index>
+              </Select.ItemGroup>
+            </Select.Content>
+          </Select.Positioner>
+          <Select.HiddenSelect />
+        </Select.Root>
+      </EnvironmentProvider>
+    </Portal>
+  )
+}
