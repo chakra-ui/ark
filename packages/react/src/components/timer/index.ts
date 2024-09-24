f9025e843afae6beb0bb2773bcb6178edd892b07
Segun Adebayo <joseshegs@gmail.com>
2024-09-24 17:48:31 +0100

    fix: exports

--- packages/react/src/components/timer/index.ts
+++ packages/react/src/components/timer/index.ts
@@ -3,20 +3,11 @@ export {
   type TimerActionTriggerBaseProps,
   type TimerActionTriggerProps,
 } from './timer-action-trigger'
-export {
-  TimerContext,
-  type TimerContextProps,
-} from './timer-context'
-export {
-  TimerItem,
-  type TimerItemBaseProps,
-  type TimerItemProps,
-} from './timer-item'
-export {
-  TimerRoot,
-  type TimerRootBaseProps,
-  type TimerRootProps,
-} from './timer-root'
+export { TimerContext, type TimerContextProps } from './timer-context'
+export { TimerItem, type TimerItemBaseProps, type TimerItemProps } from './timer-item'
+export { TimerControl, type TimerControlBaseProps, type TimerControlProps } from './timer-control'
+export { TimerArea, type TimerAreaBaseProps, type TimerAreaProps } from './timer-area'
+export { TimerRoot, type TimerRootBaseProps, type TimerRootProps } from './timer-root'
 export {
   TimerRootProvider,
   type TimerRootProviderBaseProps,
