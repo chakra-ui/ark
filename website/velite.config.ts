bc111f6a34719619c0461d7b5fdaf5f0cc96228e
Christian Schröter <dev@grizzly.codes>
2024-11-02 14:13:33 +0100

    chore: bump deps

--- website/velite.config.ts
+++ website/velite.config.ts
@@ -1,11 +1,11 @@
-import path from 'node:path'
+import { join } from 'node:path'
 import rehypeAutolinkHeadings from 'rehype-autolink-headings'
 import rehypeSlug from 'rehype-slug'
 import { defineCollection, defineConfig, s } from 'velite'
 
 const pages = defineCollection({
   name: 'Pages',
-  pattern: ['website/src/content/pages/**/*.mdx', 'packages/*/CHANGELOG.md'],
+  pattern: ['pages/**/*.mdx', '../../../packages/*/CHANGELOG.md'],
   schema: s
     .object({
       id: s.string(),
@@ -38,7 +38,7 @@ const pages = defineCollection({
 
 const showcases = defineCollection({
   name: 'Showcases',
-  pattern: 'website/src/content/showcases.json',
+  pattern: 'showcases.json',
   schema: s.object({
     title: s.string(),
     description: s.string(),
@@ -56,7 +56,7 @@ const PropDefintion = s.object({
 
 const types = defineCollection({
   name: 'Types',
-  pattern: 'website/src/content/types/**/*.json',
+  pattern: 'types/**/*.json',
   schema: s
     .record(
       s.string(),
@@ -75,7 +75,7 @@ const types = defineCollection({
 })
 
 export default defineConfig({
-  root: path.join(process.cwd(), '../'),
+  root: join(process.cwd(), './src/content'),
   collections: { pages, types, showcases },
   mdx: {
     rehypePlugins: [
