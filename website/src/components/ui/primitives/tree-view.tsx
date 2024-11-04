2aef6175465c661d96f9122da0f32c948ecaccea
Christian Schröter <dev@grizzly.codes>
2024-11-04 23:54:48 +0100

    fix website build

--- website/src/components/ui/primitives/tree-view.tsx
+++ website/src/components/ui/primitives/tree-view.tsx
@@ -1,6 +1,6 @@
 'use client'
 import type { Assign } from '@ark-ui/react'
-import { TreeView } from '@ark-ui/react/tree-view'
+import { type TreeNode, TreeView } from '@ark-ui/react/tree-view'
 import { type TreeViewVariantProps, treeView } from 'styled-system/recipes'
 import type { ComponentProps, HTMLStyledProps } from 'styled-system/types'
 import { createStyleContext } from '~/lib/create-style-context'
@@ -10,19 +10,28 @@ const { withProvider, withContext } = createStyleContext(treeView)
 export type RootProviderProps = ComponentProps<typeof RootProvider>
 export const RootProvider = withProvider<
   HTMLDivElement,
-  Assign<Assign<HTMLStyledProps<'div'>, TreeView.RootProviderBaseProps>, TreeViewVariantProps>
+  Assign<
+    Assign<HTMLStyledProps<'div'>, TreeView.RootProviderBaseProps<TreeNode>>,
+    TreeViewVariantProps
+  >
 >(TreeView.RootProvider, 'root')
 
 export type RootProps = ComponentProps<typeof Root>
 export const Root = withProvider<
   HTMLDivElement,
-  Assign<Assign<HTMLStyledProps<'div'>, TreeView.RootBaseProps>, TreeViewVariantProps>
+  Assign<Assign<HTMLStyledProps<'div'>, TreeView.RootBaseProps<TreeNode>>, TreeViewVariantProps>
 >(TreeView.Root, 'root')
 
 export const BranchContent = withContext<
-  HTMLUListElement,
-  Assign<HTMLStyledProps<'ul'>, TreeView.BranchContentBaseProps>
+  HTMLDivElement,
+  Assign<HTMLStyledProps<'div'>, TreeView.BranchContentBaseProps>
 >(TreeView.BranchContent, 'branchContent')
+
+export const BranchIndentGuide = withContext<
+  HTMLDivElement,
+  Assign<HTMLStyledProps<'div'>, TreeView.BranchIndentGuideBaseProps>
+  // @ts-expect-error
+>(TreeView.BranchIndentGuide, 'branchIndentGuide')
 
 export const BranchControl = withContext<
   HTMLDivElement,
@@ -35,8 +44,8 @@ export const BranchIndicator = withContext<
 >(TreeView.BranchIndicator, 'branchIndicator')
 
 export const Branch = withContext<
-  HTMLLIElement,
-  Assign<HTMLStyledProps<'li'>, TreeView.BranchBaseProps>
+  HTMLDivElement,
+  Assign<HTMLStyledProps<'div'>, TreeView.BranchBaseProps>
 >(TreeView.Branch, 'branch')
 
 export const BranchText = withContext<
@@ -55,8 +64,8 @@ export const ItemIndicator = withContext<
 >(TreeView.ItemIndicator, 'itemIndicator')
 
 export const Item = withContext<
-  HTMLLIElement,
-  Assign<HTMLStyledProps<'li'>, TreeView.ItemBaseProps>
+  HTMLDivElement,
+  Assign<HTMLStyledProps<'div'>, TreeView.ItemBaseProps>
 >(TreeView.Item, 'item')
 
 export const ItemText = withContext<
@@ -70,8 +79,11 @@ export const Label = withContext<
 >(TreeView.Label, 'label')
 
 export const Tree = withContext<
-  HTMLUListElement,
-  Assign<HTMLStyledProps<'ul'>, TreeView.TreeBaseProps>
+  HTMLDivElement,
+  Assign<HTMLStyledProps<'div'>, TreeView.TreeBaseProps>
 >(TreeView.Tree, 'tree')
 
 export const Context = TreeView.Context
+
+export type NodeProviderProps = TreeView.NodeProviderProps<TreeNode>
+export const NodeProvider = TreeView.NodeProvider
--- website/src/components/ui/tree-view.tsx
+++ website/src/components/ui/tree-view.tsx
@@ -1,66 +1,62 @@
 'use client'
+import { CheckSquareIcon, ChevronRightIcon, FileIcon, FolderIcon } from 'lucide-react'
 import { forwardRef } from 'react'
 import * as ArkTreeView from './primitives/tree-view'
 
-interface Child {
-  value: string
+interface Node {
+  id: string
   name: string
-  children?: Child[]
+  children?: Node[]
 }
 
-export interface TreeViewData {
-  label: string
-  children: Child[]
-}
-
-export interface TreeViewProps extends ArkTreeView.RootProps {
-  data: TreeViewData
-}
-
-export const TreeView = forwardRef<HTMLDivElement, TreeViewProps>((props, ref) => {
-  const { data, ...rootProps } = props
-
-  const renderChild = (child: Child) => (
-    <ArkTreeView.Branch key={child.value} value={child.value}>
-      <ArkTreeView.BranchControl>
-        <ArkTreeView.BranchIndicator>
-          <ChevronRightIcon />
-        </ArkTreeView.BranchIndicator>
-        <ArkTreeView.BranchText>{child.name}</ArkTreeView.BranchText>
-      </ArkTreeView.BranchControl>
-      <ArkTreeView.BranchContent>
-        {child.children?.map((child) =>
-          child.children ? (
-            renderChild(child)
-          ) : (
-            <ArkTreeView.Item key={child.value} value={child.value}>
-              <ArkTreeView.ItemText>{child.name}</ArkTreeView.ItemText>
-            </ArkTreeView.Item>
-          ),
-        )}
-      </ArkTreeView.BranchContent>
-    </ArkTreeView.Branch>
-  )
-
+export const TreeView = forwardRef<HTMLDivElement, ArkTreeView.RootProps>((props, ref) => {
   return (
-    <ArkTreeView.Root ref={ref} aria-label={data.label} {...rootProps}>
-      <ArkTreeView.Tree>{data.children.map(renderChild)}</ArkTreeView.Tree>
+    <ArkTreeView.Root ref={ref} {...props}>
+      <ArkTreeView.Tree>
+        {/* @ts-expect-error */}
+        {props.collection.rootNode.children.map((node, index) => (
+          <TreeNode key={node.id} node={node} indexPath={[index]} />
+        ))}
+      </ArkTreeView.Tree>
     </ArkTreeView.Root>
   )
 })
 
 TreeView.displayName = 'TreeView'
 
-const ChevronRightIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-    <title>Chevron Right Icon</title>
-    <path
-      fill="none"
-      stroke="currentColor"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth="2"
-      d="m9 18l6-6l-6-6"
-    />
-  </svg>
-)
+const TreeNode = (props: ArkTreeView.NodeProviderProps) => {
+  const { node, indexPath } = props
+  return (
+    <ArkTreeView.NodeProvider key={node.id} node={node} indexPath={indexPath}>
+      {node.children ? (
+        <ArkTreeView.Branch>
+          <ArkTreeView.BranchControl>
+            <ArkTreeView.BranchText>
+              <FolderIcon /> {node.name}
+            </ArkTreeView.BranchText>
+            <ArkTreeView.BranchIndicator>
+              <ChevronRightIcon />
+            </ArkTreeView.BranchIndicator>
+          </ArkTreeView.BranchControl>
+          <ArkTreeView.BranchContent>
+            <ArkTreeView.BranchIndentGuide />
+            {/* @ts-expect-error */}
+            {node.children.map((child, index) => (
+              <TreeNode key={child.id} node={child} indexPath={[...indexPath, index]} />
+            ))}
+          </ArkTreeView.BranchContent>
+        </ArkTreeView.Branch>
+      ) : (
+        <ArkTreeView.Item>
+          <ArkTreeView.ItemIndicator>
+            <CheckSquareIcon />
+          </ArkTreeView.ItemIndicator>
+          <ArkTreeView.ItemText>
+            <FileIcon />
+            {node.name}
+          </ArkTreeView.ItemText>
+        </ArkTreeView.Item>
+      )}
+    </ArkTreeView.NodeProvider>
+  )
+}
