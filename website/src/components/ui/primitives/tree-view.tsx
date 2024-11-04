b1731ba11b8f3de9fa81574da47d7eb45e871a0d
Christian Schröter <dev@grizzly.codes>
2024-11-05 00:25:36 +0100

    fix(website): tweak styles of treeview demo

--- website/src/components/ui/primitives/tree-view.tsx
+++ website/src/components/ui/primitives/tree-view.tsx
@@ -1,9 +1,148 @@
 'use client'
+import { treeViewAnatomy } from '@ark-ui/anatomy'
 import type { Assign } from '@ark-ui/react'
 import { type TreeNode, TreeView } from '@ark-ui/react/tree-view'
-import { type TreeViewVariantProps, treeView } from 'styled-system/recipes'
+import { sva } from 'styled-system/css'
+import type { TreeViewVariantProps } from 'styled-system/recipes'
 import type { ComponentProps, HTMLStyledProps } from 'styled-system/types'
 import { createStyleContext } from '~/lib/create-style-context'
+
+export const treeView = sva({
+  className: 'treeView',
+  slots: treeViewAnatomy.keys(),
+  base: {
+    root: {
+      width: 'full',
+    },
+    branch: {
+      "&[data-depth='1'] > [data-part='branch-content']": {
+        _before: {
+          bg: 'border.default',
+          content: '""',
+          height: 'full',
+          left: '3',
+          position: 'absolute',
+          width: '1px',
+          zIndex: '1',
+        },
+      },
+    },
+    branchContent: {
+      position: 'relative',
+      overflow: 'hidden',
+      transitionProperty: 'padding-bottom',
+      transitionDuration: 'normal',
+      transitionTimingFunction: 'default',
+      _open: {
+        animation: 'collapse-in',
+      },
+      _closed: {
+        animation: 'collapse-out',
+      },
+    },
+    branchControl: {
+      alignItems: 'center',
+      borderRadius: 'l2',
+      display: 'flex',
+      fontWeight: 'medium',
+      gap: '1.5',
+      ps: 'calc((var(--depth) - 1) * 22px)',
+      py: '1.5',
+      justifyContent: 'space-between',
+      transitionDuration: 'normal',
+      transitionProperty: 'background, color',
+      transitionTimingFunction: 'default',
+      "&[data-depth='1']": {
+        ps: '1',
+      },
+      "&[data-depth='1'] > [data-part='branch-text'] ": {
+        fontWeight: 'semibold',
+        color: 'fg.default',
+      },
+      _hover: {
+        background: 'gray.a2',
+        color: 'fg.default',
+      },
+    },
+    branchIndicator: {
+      color: 'accent.default',
+      transformOrigin: 'center',
+      transitionDuration: 'normal',
+      transitionProperty: 'transform',
+      transitionTimingFunction: 'default',
+      '& svg': {
+        fontSize: 'md',
+        width: '4',
+        height: '4',
+      },
+      _open: {
+        transform: 'rotate(90deg)',
+      },
+    },
+    item: {
+      display: 'flex',
+      alignItems: 'center',
+      gap: '2',
+      borderRadius: 'l2',
+      cursor: 'pointer',
+      fontWeight: 'medium',
+      position: 'relative',
+      ps: 'calc(((var(--depth) - 1) * 22px) + 22px)',
+      py: '1.5',
+      transitionDuration: 'normal',
+      transitionProperty: 'background, color',
+      transitionTimingFunction: 'default',
+      "&[data-depth='1']": {
+        ps: '6',
+        fontWeight: 'semibold',
+        color: 'fg.default',
+        _selected: {
+          _before: {
+            bg: 'transparent',
+          },
+        },
+      },
+      _hover: {
+        background: 'gray.a2',
+        color: 'fg.default',
+      },
+      _selected: {
+        background: 'accent.a2',
+        color: 'accent.text',
+        _hover: {
+          background: 'accent.a2',
+          color: 'accent.text',
+        },
+        _before: {
+          content: '""',
+          position: 'absolute',
+          left: '3',
+          top: '0',
+          width: '2px',
+          height: 'full',
+          bg: 'accent.default',
+          zIndex: '1',
+        },
+      },
+    },
+    itemText: {
+      display: 'flex',
+      alignItems: 'center',
+      gap: '2',
+    },
+    branchText: {
+      display: 'flex',
+      alignItems: 'center',
+      gap: '2',
+    },
+    tree: {
+      display: 'flex',
+      flexDirection: 'column',
+      gap: '3',
+      textStyle: 'sm',
+    },
+  },
+})
 
 const { withProvider, withContext } = createStyleContext(treeView)
 
@@ -30,7 +169,6 @@ export const BranchContent = withContext<
 export const BranchIndentGuide = withContext<
   HTMLDivElement,
   Assign<HTMLStyledProps<'div'>, TreeView.BranchIndentGuideBaseProps>
-  // @ts-expect-error
 >(TreeView.BranchIndentGuide, 'branchIndentGuide')
 
 export const BranchControl = withContext<
