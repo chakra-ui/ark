7d25bdbbc3e408cc4e36fb827c4c2c62f03a1ba3
Segun Adebayo <joseshegs@gmail.com>
2024-11-28 14:18:40 +0000

    revert: controlled tabs example

--- packages/react/src/components/tabs/examples/controlled.tsx
+++ packages/react/src/components/tabs/examples/controlled.tsx
@@ -1,22 +1,11 @@
 import { Tabs } from '@ark-ui/react/tabs'
 import { useState } from 'react'
 
-let i = 0
-
 export const Controlled = () => {
   const [value, setValue] = useState<string | null>('react')
 
   return (
-    <Tabs.Root
-      value={value}
-      onValueChange={(e) => {
-        if (i === 0) {
-          i++
-        } else {
-          setValue(e.value)
-        }
-      }}
-    >
+    <Tabs.Root value={value} onValueChange={(e) => setValue(e.value)}>
       <Tabs.List>
         <Tabs.Trigger value="react">React</Tabs.Trigger>
         <Tabs.Trigger value="vue">Vue</Tabs.Trigger>
