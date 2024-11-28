bd35837e10cd337bcd78736a3b8175ee4fc1b379
Segun Adebayo <joseshegs@gmail.com>
2024-11-28 14:21:40 +0000

    chore: update

--- packages/react/src/components/tabs/examples/controlled.tsx
+++ packages/react/src/components/tabs/examples/controlled.tsx
@@ -3,7 +3,6 @@ import { useState } from 'react'
 
 export const Controlled = () => {
   const [value, setValue] = useState<string | null>('react')
-
   return (
     <Tabs.Root value={value} onValueChange={(e) => setValue(e.value)}>
       <Tabs.List>
