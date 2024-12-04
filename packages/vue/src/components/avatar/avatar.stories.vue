a3e5e31cbf372773998792ce6bdbd435e4e2d118
Sybren W <sybren.willemot@gmail.com>
2024-12-04 13:55:33 +0100

    refactor(avatar): add id for story and variants for clean Histoire url

--- packages/vue/src/components/avatar/avatar.stories.vue
+++ packages/vue/src/components/avatar/avatar.stories.vue
@@ -4,22 +4,23 @@ import Avatar from './examples/closed.vue'
 import Events from './examples/events.vue'
 import RootProvider from './examples/root-provider.vue'
 </script>
+
 <template>
-  <Story title="Avatar">
-    <Variant title="Basic">
+  <Story id="avatar" title="Avatar">
+    <Variant id="basic" title="Basic">
       <Basic />
     </Variant>
-    <Variant title="Events">
+    <Variant id="events" title="Events">
       <Events />
     </Variant>
-    <Variant title="Closed">
+    <Variant id="closed" title="Closed">
       <Avatar
         name="Christian"
         src="https://avatars.githubusercontent.com/u/1846056?v=4"
         @status-change="(e) => console.log(e.status)"
       />
     </Variant>
-    <Variant title="RootProvider">
+    <Variant id="root-provider" title="RootProvider">
       <RootProvider />
     </Variant>
   </Story>
