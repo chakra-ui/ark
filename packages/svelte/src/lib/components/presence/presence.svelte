56700c51e841da53c6cecafef746a0ec82b80177
Christian Schröter <dev@grizzly.codes>
2024-12-05 13:22:26 +0100

    fix(svelte): lazy mount animation

--- packages/svelte/src/lib/components/presence/presence.svelte
+++ packages/svelte/src/lib/components/presence/presence.svelte
@@ -1,24 +1,29 @@
 <script module lang="ts">
-  import type { HTMLProps, PolymorphicProps } from '$lib/types'
-  import { type UsePresenceProps, usePresence } from './use-presence.svelte'
+import type { HTMLProps, PolymorphicProps } from '$lib/types'
+import { type UsePresenceProps, usePresence } from './use-presence.svelte'
 
-  export interface PresenceBaseProps extends UsePresenceProps, PolymorphicProps<'div'> {}
-  export interface PresenceProps extends HTMLProps<'div'>, PresenceBaseProps {}
+export interface PresenceBaseProps extends UsePresenceProps, PolymorphicProps<'div'> {}
+export interface PresenceProps extends HTMLProps<'div'>, PresenceBaseProps {}
 </script>
 
 <script lang="ts">
   import { mergeProps, reflect } from '@zag-js/svelte'
-  import { onMount } from 'svelte'
   import { splitPresenceProps } from './split-presence-props.svelte'
 
   let props: PresenceProps = $props()
   let ref: HTMLDivElement | null = $state(null)
+  let called = $state(false)
 
   const [presenceProps, localProps] = $derived(splitPresenceProps(props))
   const presence = usePresence(reflect(() => presenceProps))
   const mergedProps = $derived(mergeProps(presence().getPresenceProps(), localProps))
 
-  onMount(() => presence().setNode(ref))
+  $effect(() => {
+    if (!called && ref) {
+      called = true
+      presence().setNode(ref)
+    }
+  })
 </script>
 
 {#if !presence().unmounted}
