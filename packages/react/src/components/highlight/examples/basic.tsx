63c77c6906437aea4c253826a9986ae132f69745
Abraham <anubra266@gmail.com>
2024-12-27 14:42:41 -0600

    chore: fix typos in docs (#3178)
    * chore: fix typos in docs

--- packages/react/src/components/highlight/examples/basic.tsx
+++ packages/react/src/components/highlight/examples/basic.tsx
@@ -3,7 +3,7 @@ import { Highlight } from '@ark-ui/react/highlight'
 export const Basic = () => {
   return (
     <Highlight
-      query=" ipsum"
+      query="ipsum"
       text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt"
     />
   )
