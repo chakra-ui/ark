1f1515ff8941b5757ef947f7cb37768308d39abc
Michał Korczak <michal.k2001@gmail.com>
2025-01-06 01:50:40 +0100

    fix: types

--- packages/svelte/src/lib/components/format/format-number.svelte
+++ packages/svelte/src/lib/components/format/format-number.svelte
@@ -7,6 +7,7 @@
     compactDisplay?: 'short' | 'long' | undefined
     notation?: 'standard' | 'scientific' | 'engineering' | 'compact' | undefined
     signDisplay?: 'auto' | 'never' | 'always' | 'exceptZero' | undefined
+    style?: string | undefined
     unit?: string | undefined
     unitDisplay?: 'short' | 'long' | 'narrow' | undefined
     currencyDisplay?: string | undefined
