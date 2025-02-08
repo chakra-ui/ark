eeb1ab62aa3c3fea651cc6f5137c5add5fe8c820
Sybren W <sybren.willemot@gmail.com>
2025-02-08 02:20:22 +0100

    feat(playwrite): update configs

--- packages/vue/playwright.config.ts
+++ packages/vue/playwright.config.ts
@@ -76,9 +76,11 @@ export default defineConfig({
     // },
   ],
   // Run your local dev server before starting the tests.
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'bun run test:serve:story',
+    url: 'http://localhost:6006',
+    stdout: 'pipe',
+    reuseExistingServer: !process.env.CI,
+    timeout: 240 * 1000,
+  },
 })
