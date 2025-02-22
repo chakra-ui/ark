396e1952592d2d4e29d052e3bf3cd5fd2c161dd4
Christian Schröter <dev@grizzly.codes>
2025-02-22 23:35:40 +0100

    wip

--- packages/solid/src/components/toast/create-toaster.tsx
+++ packages/solid/src/components/toast/create-toaster.tsx
@@ -1,11 +1,11 @@
 import * as toast from '@zag-js/toast'
 import type { JSX } from 'solid-js'
 
-export interface CreateToasterProps extends Partial<toast.ToastStoreProps> {
+export interface CreateToasterProps extends Partial<toast.StoreProps> {
   placement: toast.Placement
 }
 
-export interface CreateToasterReturn extends toast.ToastStore<JSX.Element> {}
+export interface CreateToasterReturn extends toast.Store<JSX.Element> {}
 
 export const createToaster = (props: CreateToasterProps): CreateToasterReturn => {
   return toast.createStore(props)
