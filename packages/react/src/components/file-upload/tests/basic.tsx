d358e3f0255c760883d34edc2aca41e0875adaa2
Segun Adebayo <joseshegs@gmail.com>
2025-02-26 18:46:27 +0000

    chore: update

--- packages/react/src/components/file-upload/tests/basic.tsx
+++ packages/react/src/components/file-upload/tests/basic.tsx
@@ -1,37 +1,36 @@
-// import { FileUpload } from '../'
+import { FileUpload } from '../'
 
-export const ComponentUnderTest = () => (
-  <div />
-  // <FileUpload.Root {...props}>
-  //   <FileUpload.Context>
-  //     {(fileUpload) => {
-  //       if (fileUpload.acceptedFiles.length < 1)
-  //         fileUpload.setFiles([new File([''], 'test.jpg', { type: 'image/jpg' })])
+export const ComponentUnderTest = (props: FileUpload.RootProps) => (
+  <FileUpload.Root {...props}>
+    <FileUpload.Context>
+      {(fileUpload) => {
+        // if (fileUpload.acceptedFiles.length < 1)
+        //   fileUpload.setFiles([new File([''], 'test.jpg', { type: 'image/jpg' })])
 
-  //       return (
-  //         <>
-  //           <FileUpload.Label>Drag your file(s) here</FileUpload.Label>
-  //           <FileUpload.Dropzone />
-  //           <FileUpload.ClearTrigger>Clear</FileUpload.ClearTrigger>
-  //           <FileUpload.Trigger>Choose file(s)</FileUpload.Trigger>
-  //           <FileUpload.ItemGroup>
-  //             {fileUpload.acceptedFiles.map((file) => (
-  //               <FileUpload.Item key={file.name} file={file}>
-  //                 <FileUpload.ItemPreview type="image/*">
-  //                   <FileUpload.ItemPreviewImage />
-  //                 </FileUpload.ItemPreview>
-  //                 <FileUpload.ItemPreview>FileIcon</FileUpload.ItemPreview>
-  //                 <FileUpload.ItemPreviewImage />
-  //                 <FileUpload.ItemName />
-  //                 <FileUpload.ItemSizeText />
-  //                 <FileUpload.ItemDeleteTrigger>Remove</FileUpload.ItemDeleteTrigger>
-  //               </FileUpload.Item>
-  //             ))}
-  //           </FileUpload.ItemGroup>
-  //         </>
-  //       )
-  //     }}
-  //   </FileUpload.Context>
-  //   <FileUpload.HiddenInput />
-  // </FileUpload.Root>
+        return (
+          <>
+            <FileUpload.Label>Drag your file(s) here</FileUpload.Label>
+            <FileUpload.Dropzone />
+            <FileUpload.ClearTrigger>Clear</FileUpload.ClearTrigger>
+            <FileUpload.Trigger>Choose file(s)</FileUpload.Trigger>
+            <FileUpload.ItemGroup>
+              {fileUpload.acceptedFiles.map((file) => (
+                <FileUpload.Item key={file.name} file={file}>
+                  <FileUpload.ItemPreview type="image/*">
+                    <FileUpload.ItemPreviewImage />
+                  </FileUpload.ItemPreview>
+                  <FileUpload.ItemPreview>FileIcon</FileUpload.ItemPreview>
+                  <FileUpload.ItemPreviewImage />
+                  <FileUpload.ItemName />
+                  <FileUpload.ItemSizeText />
+                  <FileUpload.ItemDeleteTrigger>Remove</FileUpload.ItemDeleteTrigger>
+                </FileUpload.Item>
+              ))}
+            </FileUpload.ItemGroup>
+          </>
+        )
+      }}
+    </FileUpload.Context>
+    <FileUpload.HiddenInput />
+  </FileUpload.Root>
 )
