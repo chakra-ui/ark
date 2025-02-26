9d41e9e925d6d16d9cb8790b121c0f2ff3ddfa9d
Segun Adebayo <joseshegs@gmail.com>
2025-02-26 22:33:28 +0000

    refactor: toggle to machine

--- packages/react/src/components/toggle/use-toggle.ts
+++ packages/react/src/components/toggle/use-toggle.ts
@@ -1,81 +1,11 @@
-import { dataAttr } from '@zag-js/dom-query'
-import { useControllableState } from '../../utils/use-controllable-state'
-import { parts } from './toggle.anatomy'
+import { type PropTypes, normalizeProps, useMachine } from '@zag-js/react'
+import * as toggle from '@zag-js/toggle'
 
-export interface UseToggleProps {
-  /**
-   * Whether the toggle is disabled.
-   */
-  disabled?: boolean
-  /**
-   * The default pressed state of the toggle.
-   */
-  defaultPressed?: boolean
-  /**
-   * The pressed state of the toggle.
-   */
-  pressed?: boolean
-  /**
-   * Event handler called when the pressed state of the toggle changes.
-   */
-  onPressedChange?: (pressed: boolean) => void
-}
+export interface UseToggleProps extends toggle.Props {}
 
-export interface UseToggleReturn {
-  /**
-   * Whether the toggle is pressed.
-   */
-  pressed: boolean
-  /**
-   * Whether the toggle is disabled.
-   */
-  disabled: boolean
-  /**
-   * Sets the pressed state of the toggle.
-   */
-  setPressed(pressed: boolean): void
-  getRootProps(): React.ComponentProps<'button'>
-  getIndicatorProps(): React.ComponentProps<'div'>
-}
+export interface UseToggleReturn extends toggle.Api<PropTypes> {}
 
 export function useToggle(props: UseToggleProps): UseToggleReturn {
-  const { defaultPressed, pressed, onPressedChange, disabled } = props
-
-  const [pressedState, setPressedState] = useControllableState({
-    defaultValue: !!defaultPressed,
-    value: pressed,
-    onChange: onPressedChange,
-  })
-
-  return {
-    pressed: pressedState,
-    disabled: !!disabled,
-    setPressed: setPressedState,
-
-    getRootProps() {
-      return {
-        ...parts.root.attrs,
-        type: 'button',
-        disabled,
-        'aria-pressed': pressedState,
-        'data-state': pressedState ? 'on' : 'off',
-        'data-pressed': dataAttr(pressedState),
-        'data-disabled': dataAttr(disabled),
-        onClick(event) {
-          if (event.defaultPrevented) return
-          if (disabled) return
-          setPressedState(!pressedState)
-        },
-      }
-    },
-
-    getIndicatorProps() {
-      return {
-        ...(parts.indicator.attrs as React.HTMLAttributes<HTMLDivElement>),
-        'data-disabled': dataAttr(disabled),
-        'data-pressed': dataAttr(pressedState),
-        'data-state': pressedState ? 'on' : 'off',
-      }
-    },
-  }
+  const service = useMachine(toggle.machine, props)
+  return toggle.connect(service, normalizeProps)
 }
