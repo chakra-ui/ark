5b16ac4f4eb8f97a5ad791ccd99927d12c17c1a6
Segun Adebayo <joseshegs@gmail.com>
2025-02-27 13:14:03 +0000

    fix: vue presence

--- packages/vue/src/components/presence/use-presence.ts
+++ packages/vue/src/components/presence/use-presence.ts
@@ -1,6 +1,6 @@
 import * as presence from '@zag-js/presence'
 import { normalizeProps, useMachine } from '@zag-js/vue'
-import { type MaybeRef, type VNodeRef, computed, ref, watch } from 'vue'
+import { type MaybeRef, type VNodeRef, computed, ref, toValue, watch } from 'vue'
 import type { EmitFn, Optional } from '../../types'
 import type { RootEmits } from './presence.types'
 
@@ -20,14 +20,15 @@ export interface UsePresenceProps extends Optional<presence.Props, 'present'> {
 export type UsePresenceReturn = ReturnType<typeof usePresence>
 
 export const usePresence = (props: MaybeRef<UsePresenceProps>, emit?: EmitFn<RootEmits>) => {
-  const context = ref(props)
   const wasEverPresent = ref(false)
   const nodeRef = ref<VNodeRef | null>(null)
 
-  const service = useMachine(presence.machine, {
-    ...context.value,
+  const machineProps = computed(() => ({
+    present: toValue(props).present,
     onExitComplete: () => emit?.('exitComplete'),
-  })
+  }))
+
+  const service = useMachine(presence.machine, machineProps)
   const api = computed(() => presence.connect(service, normalizeProps))
 
   watch(
@@ -47,15 +48,18 @@ export const usePresence = (props: MaybeRef<UsePresenceProps>, emit?: EmitFn<Roo
     }
   })
 
-  return computed(() => ({
-    present: api.value.present,
-    unmounted:
-      (!api.value.present && !wasEverPresent.value && context.value.lazyMount) ||
-      (context.value?.unmountOnExit && !api.value?.present && wasEverPresent.value),
-    presenceProps: {
-      ref: nodeRef,
-      hidden: !api.value.present,
-      'data-state': context.value?.present ? 'open' : 'closed',
-    },
-  }))
+  return computed(() => {
+    const localProps = toValue(props)
+    return {
+      present: api.value.present,
+      unmounted:
+        (!api.value.present && !wasEverPresent.value && localProps.lazyMount) ||
+        (localProps?.unmountOnExit && !api.value?.present && wasEverPresent.value),
+      presenceProps: {
+        ref: nodeRef,
+        hidden: !api.value.present,
+        'data-state': localProps?.present ? 'open' : 'closed',
+      },
+    }
+  })
 }
