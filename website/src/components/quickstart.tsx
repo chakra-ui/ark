4d87d6679b8c64809489495107d4be6d0c5fc591
Christian Schröter <dev@grizzly.codes>
2025-03-06 14:06:06 +0100

    docs: changelog

--- website/src/components/quickstart.tsx
+++ website/src/components/quickstart.tsx
@@ -4,14 +4,14 @@ import { NextJsIcon, NuxtIcon, SolidStartIcon } from './marketing/icons'
 
 export const Quickstart = () => {
   const frameworks = [
-    { name: 'Next.js', icon: NextJsIcon, slug: 'templates/react/next-js' },
-    { name: 'Solid Start', icon: SolidStartIcon, slug: 'templates/solid/solid-start' },
-    { name: 'Nuxt', icon: NuxtIcon, slug: 'templates/vue/nuxt' },
+    { name: 'Next.js', icon: NextJsIcon, href: 'https://stackblitz.com/edit/github-qcm2dskf' },
+    { name: 'Solid Start', icon: SolidStartIcon, href: 'https://stackblitz.com/edit/github-1hgkbbln' },
+    { name: 'Nuxt', icon: NuxtIcon, shreflug: 'https://stackblitz.com/edit/github-s3sg6syq' },
   ]
   return (
     <Grid gap={{ base: '4', md: '6' }} columns={{ base: 2, sm: 3, xl: 5 }} className="not-prose">
-      {frameworks.map(({ name, icon: Icon, slug }) => (
-        <a key={name} href={`https://github.com/chakra-ui/ark/tree/main/${slug}`} target="_blank" rel="noreferrer">
+      {frameworks.map(({ name, icon: Icon, href }) => (
+        <a key={name} href={href} target="_blank" rel="noreferrer">
           <Flex
             borderRadius="l3"
             borderWidth="1px"
