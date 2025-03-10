535500ba4312bfe58ee050a1ff383a41f76afb80
Sybren W <sybren.willemot@gmail.com>
2025-03-10 12:26:03 +0100

    test(avatar): add pw test for root providers

--- packages/vue/src/components/avatar/avatar.stories.ts
+++ packages/vue/src/components/avatar/avatar.stories.ts
@@ -1,6 +1,7 @@
 import type { Meta } from '@storybook/react'
 
 import BasicExample from './examples/basic.vue'
+import RootProviderExample from './examples/root-provider.vue'
 
 const meta = {
   title: 'Components / Avatar',
@@ -12,5 +13,12 @@ export const Basic = {
   render: () => ({
     components: { BasicExample },
     template: '<BasicExample />',
+  }),
+}
+
+export const RootProvider = {
+  render: () => ({
+    components: { RootProviderExample },
+    template: '<RootProviderExample />',
   }),
 }
--- tests/playwright/tests/packages/components/avatar/avatar.pw.test.ts
+++ tests/playwright/tests/packages/components/avatar/avatar.pw.test.ts
@@ -21,4 +21,34 @@ for (const packageName of packages) {
       await expect(accessibilityScanResults.violations).toEqual([])
     })
   })
+
+  test.describe(`${packageName}: root-provider variant`, () => {
+    test.beforeEach(async ({ page }) => {
+      await gotoStory('avatar', 'root-provider', page, packageName)
+      await page.getByAltText('avatar').waitFor()
+    })
+    test('has avatar image', async ({ page }) => {
+      await expect(page.getByAltText('avatar')).toHaveAttribute(
+        'src',
+        'https://i.pravatar.cc/300?u=a042581f4e29026704d',
+      )
+      await expect(page.locator('#storybook-root')).toHaveScreenshot()
+    })
+    test('has no a11y violations', async ({ page }, testInfo) => {
+      const accessibilityScanResults = await testA11yWithAttachedResults(page, testInfo, 'avatar')
+      await expect(accessibilityScanResults.violations).toEqual([])
+    })
+    test('changes avatar source when out of component button is clicked', async ({ page }) => {
+      await expect(page.getByAltText('avatar')).toHaveAttribute(
+        'src',
+        'https://i.pravatar.cc/300?u=a042581f4e29026704d',
+      )
+      await page.getByRole('button', { name: 'Change Source' }).click()
+      await expect(page.getByAltText('avatar')).toHaveAttribute(
+        'src',
+        'https://avatars.githubusercontent.com/u/6916170?v=4',
+      )
+      await expect(page.locator('#storybook-root')).toHaveScreenshot()
+    })
+  })
 }
