b5a2f293c2a90140d173dc5adfca7d79dae9b698
Christian Schröter <dev@grizzly.codes>
2025-03-13 19:55:25 +0100

    docs(website): add architecture diagram (#3388)

--- website/src/mdx-content.tsx
+++ website/src/mdx-content.tsx
@@ -14,6 +14,7 @@ import { Quickstart } from './components/quickstart'
 import { Story } from './components/story'
 
 import { Step, Steps } from '~/components/ui/stepper'
+import { ThemeImage } from './components/theme-image'
 
 const sharedComponents = {
   a: Link,
@@ -31,6 +32,7 @@ const sharedComponents = {
   Step,
   Steps,
   Story,
+  ThemeImage,
 }
 
 const useMDXComponent = (code: string) => {
