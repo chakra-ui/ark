b09310b42f555b887f082c117401d149cda2256d
Sybren W <sybren.willemot@gmail.com>
2025-03-18 23:20:23 +0100

    feat(vue): migrate stories to storybook for frame

---
+++ packages/vue/src/components/frame/frame.stories.ts
@@ -0,0 +1,32 @@
+import type { Meta } from '@storybook/vue3'
+
+import BasicExample from './examples/basic.vue'
+import ScriptExample from './examples/script.vue'
+import SrcDocExample from './examples/src-doc.vue'
+
+const meta = {
+  title: 'Frame',
+} as Meta
+
+export default meta
+
+export const Basic = {
+  render: () => ({
+    components: { BasicExample },
+    template: '<BasicExample />',
+  }),
+}
+
+export const SrcDoc = {
+  render: () => ({
+    components: { SrcDocExample },
+    template: '<SrcDocExample />',
+  }),
+}
+
+export const Script = {
+  render: () => ({
+    components: { ScriptExample },
+    template: '<ScriptExample />',
+  }),
+}
