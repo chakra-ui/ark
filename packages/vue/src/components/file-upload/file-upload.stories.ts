a4d907aaa6a6b36f2ec9cb940502eeb569441d96
Sybren W <sybren.willemot@gmail.com>
2025-03-18 23:14:52 +0100

    feat(vue): migrate stories to storybook for file upload

---
+++ packages/vue/src/components/file-upload/file-upload.stories.ts
@@ -0,0 +1,28 @@
+import BasicExample from './examples/basic.vue'
+import WithFieldExample from './examples/with-field.vue'
+import RootProviderExample from './examples/root-provider.vue'
+
+export default {
+  title: 'File Upload',
+};
+
+export const Basic = {
+  render: () => ({
+    components: { BasicExample },
+    template: '<BasicExample />',
+  }),
+};
+
+export const WithField = {
+  render: () => ({
+    components: { WithFieldExample },
+    template: '<WithFieldExample />',
+  }),
+};
+
+export const RootProvider = {
+  render: () => ({
+    components: { RootProviderExample },
+    template: '<RootProviderExample />',
+  }),
+};
