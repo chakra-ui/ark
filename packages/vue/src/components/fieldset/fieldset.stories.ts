f7df93273879534d383d6fb8581f5fd537d2a520
Sybren W <sybren.willemot@gmail.com>
2025-03-18 23:14:34 +0100

    feat(vue): migrate stories to storybook for fieldset

---
+++ packages/vue/src/components/fieldset/fieldset.stories.ts
@@ -0,0 +1,28 @@
+import BasicExample from './examples/basic.vue'
+import WithCheckboxExample from './examples/with-checkbox.vue'
+import WithFieldExample from './examples/with-field.vue'
+
+export default {
+  title: 'Fieldset',
+};
+
+export const Basic = {
+  render: () => ({
+    components: { BasicExample },
+    template: '<BasicExample />',
+  }),
+};
+
+export const WithField = {
+  render: () => ({
+    components: { WithFieldExample },
+    template: '<WithFieldExample />',
+  }),
+};
+
+export const WithCheckbox = {
+  render: () => ({
+    components: { WithCheckboxExample },
+    template: '<WithCheckboxExample />',
+  }),
+};
