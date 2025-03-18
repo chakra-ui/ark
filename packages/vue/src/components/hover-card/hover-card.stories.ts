945f1420f513985c5c7c65a10a756b353fd09a88
Sybren W <sybren.willemot@gmail.com>
2025-03-18 23:24:20 +0100

    feat(vue): migrate stories to storybook for hover card

---
+++ packages/vue/src/components/hover-card/hover-card.stories.ts
@@ -0,0 +1,48 @@
+import type { Meta } from '@storybook/vue3'
+
+import BasicExample from './examples/basic.vue'
+import ControlledExample from './examples/controlled.vue'
+import PositioningExample from './examples/positioning.vue'
+import RenderPropExample from './examples/render-prop.vue'
+import RootProviderExample from './examples/root-provider.vue'
+
+const meta = {
+  title: 'HoverCard',
+} as Meta
+
+export default meta
+
+export const Basic = {
+  render: () => ({
+    components: { BasicExample },
+    template: '<BasicExample />',
+  }),
+}
+
+export const Controlled = {
+  render: () => ({
+    components: { ControlledExample },
+    template: '<ControlledExample />',
+  }),
+}
+
+export const Positioning = {
+  render: () => ({
+    components: { PositioningExample },
+    template: '<PositioningExample />',
+  }),
+}
+
+export const RenderProp = {
+  render: () => ({
+    components: { RenderPropExample },
+    template: '<RenderPropExample />',
+  }),
+}
+
+export const RootProvider = {
+  render: () => ({
+    components: { RootProviderExample },
+    template: '<RootProviderExample />',
+  }),
+}
