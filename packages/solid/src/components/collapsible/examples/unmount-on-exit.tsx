210c89e45c6347e1e51bc2d396ba2080f71d40eb
Christian Schröter <dev@grizzly.codes>
2025-03-24 21:55:22 +0100

    docs: update changelog

--- packages/solid/src/components/collapsible/examples/unmount-on-exit.tsx
+++ packages/solid/src/components/collapsible/examples/unmount-on-exit.tsx
@@ -1,8 +1,14 @@
 import { Collapsible } from '@ark-ui/solid/collapsible'
+import { ChevronDownIcon } from 'lucide-solid'
 
 export const UnmountOnExit = () => (
   <Collapsible.Root unmountOnExit>
-    <Collapsible.Trigger>Toggle</Collapsible.Trigger>
+    <Collapsible.Trigger>
+      Toggle
+      <Collapsible.Indicator>
+        <ChevronDownIcon />
+      </Collapsible.Indicator>
+    </Collapsible.Trigger>
     <Collapsible.Content>Content</Collapsible.Content>
   </Collapsible.Root>
 )
