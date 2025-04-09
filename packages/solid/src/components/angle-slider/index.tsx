3c9d15808d91ec796e4f3a3328939dd7ccbe23cb
Segun Adebayo <joseshegs@gmail.com>
2025-04-09 20:01:05 +0100

    chore: implement vue

--- packages/solid/src/components/angle-slider/index.tsx
+++ packages/solid/src/components/angle-slider/index.tsx
@@ -1,33 +1,34 @@
-export { angleSliderAnatomy } from './angle-slider.anatomy'
-export { AngleSliderRoot, type AngleSliderRootProps, type AngleSliderRootBaseProps } from './angle-slider-root'
-export {
-  AngleSliderRootProvider,
-  type AngleSliderRootProviderProps,
-  type AngleSliderRootProviderBaseProps,
-} from './angle-slider-root-provider'
-export { AngleSliderLabel, type AngleSliderLabelBaseProps, type AngleSliderLabelProps } from './angle-slider-label'
+export { AngleSliderContext, type AngleSliderContextProps } from './angle-slider-context'
 export {
   AngleSliderControl,
   type AngleSliderControlBaseProps,
   type AngleSliderControlProps,
 } from './angle-slider-control'
-export { AngleSliderThumb, type AngleSliderThumbBaseProps, type AngleSliderThumbProps } from './angle-slider-thumb'
-export { AngleSliderMarker, type AngleSliderMarkerBaseProps, type AngleSliderMarkerProps } from './angle-slider-marker'
-export {
-  AngleSliderMarkerGroup,
-  type AngleSliderMarkerGroupProps,
-  type AngleSliderMarkerGroupBaseProps,
-} from './angle-slider-marker-group'
-export {
-  AngleSliderValueText,
-  type AngleSliderValueTextProps,
-  type AngleSliderValueTextBaseProps,
-} from './angle-slider-value-text'
 export {
   AngleSliderHiddenInput,
   type AngleSliderHiddenInputBaseProps,
   type AngleSliderHiddenInputProps,
 } from './angle-slider-hidden-input'
+export { AngleSliderLabel, type AngleSliderLabelBaseProps, type AngleSliderLabelProps } from './angle-slider-label'
+export { AngleSliderMarker, type AngleSliderMarkerBaseProps, type AngleSliderMarkerProps } from './angle-slider-marker'
+export {
+  AngleSliderMarkerGroup,
+  type AngleSliderMarkerGroupBaseProps,
+  type AngleSliderMarkerGroupProps,
+} from './angle-slider-marker-group'
+export { AngleSliderRoot, type AngleSliderRootBaseProps, type AngleSliderRootProps } from './angle-slider-root'
+export {
+  AngleSliderRootProvider,
+  type AngleSliderRootProviderBaseProps,
+  type AngleSliderRootProviderProps,
+} from './angle-slider-root-provider'
+export { AngleSliderThumb, type AngleSliderThumbBaseProps, type AngleSliderThumbProps } from './angle-slider-thumb'
+export {
+  AngleSliderValueText,
+  type AngleSliderValueTextBaseProps,
+  type AngleSliderValueTextProps,
+} from './angle-slider-value-text'
+export { angleSliderAnatomy } from './angle-slider.anatomy'
 export { useAngleSlider, type UseAngleSliderProps, type UseAngleSliderReturn } from './use-angle-slider'
 export { useAngleSliderContext, type UseAngleSliderContext } from './use-angle-slider-context'
 
---
+++ packages/vue/src/components/angle-slider/angle-slider.ts
@@ -0,0 +1,34 @@
+export { default as Context, type AngleSliderContextProps } from './angle-slider-context.vue'
+export { default as Root, type AngleSliderRootProps, type AngleSliderRootBaseProps } from './angle-slider-root.vue'
+export {
+  default as RootProvider,
+  type AngleSliderRootProviderProps,
+  type AngleSliderRootProviderBaseProps,
+} from './angle-slider-root-provider.vue'
+export { default as Thumb, type AngleSliderThumbProps, type AngleSliderThumbBaseProps } from './angle-slider-thumb.vue'
+export {
+  default as ValueText,
+  type AngleSliderValueTextProps,
+  type AngleSliderValueTextBaseProps,
+} from './angle-slider-value-text.vue'
+export { default as Label, type AngleSliderLabelProps, type AngleSliderLabelBaseProps } from './angle-slider-label.vue'
+export {
+  default as Control,
+  type AngleSliderControlProps,
+  type AngleSliderControlBaseProps,
+} from './angle-slider-control.vue'
+export {
+  default as MarkerGroup,
+  type AngleSliderMarkerGroupProps,
+  type AngleSliderMarkerGroupBaseProps,
+} from './angle-slider-marker-group.vue'
+export {
+  default as Marker,
+  type AngleSliderMarkerProps,
+  type AngleSliderMarkerBaseProps,
+} from './angle-slider-marker.vue'
+export {
+  default as HiddenInput,
+  type AngleSliderHiddenInputProps,
+  type AngleSliderHiddenInputBaseProps,
+} from './angle-slider-hidden-input.vue'
