4ddf23b0a52d66463eb3e851dfe2561c9b603c9f
Sybren W <sybren.willemot@gmail.com>
2025-06-18 22:39:09 +0200

    feat(tree-view): add all examples to stories

--- packages/vue/src/components/tree-view/tree-view.stories.ts
+++ packages/vue/src/components/tree-view/tree-view.stories.ts
@@ -1,9 +1,15 @@
 import type { Meta } from '@storybook/vue3'
 
+import AsyncLoadingExample from './examples/async-loading.vue'
+import AsyncTreeNodeExample from './examples/async-tree-node.vue'
 import BasicExample from './examples/basic.vue'
+import ControlledExpandedExample from './examples/controlled-expanded.vue'
+import ControlledSelectedExample from './examples/controlled-selected.vue'
+import RootProviderExample from './examples/root-provider.vue'
+import TreeNodeExample from './examples/tree-node.vue'
 
 const meta = {
-  title: 'Components / TreeView',
+  title: 'Components / Tree View',
 } as Meta
 
 export default meta
@@ -12,5 +18,47 @@ export const Basic = {
   render: () => ({
     components: { BasicExample },
     template: '<BasicExample />',
+  }),
+}
+
+export const TreeNode = {
+  render: () => ({
+    components: { TreeNodeExample },
+    template: '<TreeNodeExample />',
+  }),
+}
+
+export const AsyncLoading = {
+  render: () => ({
+    components: { AsyncLoadingExample },
+    template: '<AsyncLoadingExample />',
+  }),
+}
+
+export const AsyncTreeNode = {
+  render: () => ({
+    components: { AsyncTreeNodeExample },
+    template: '<AsyncTreeNodeExample />',
+  }),
+}
+
+export const ControlledExpanded = {
+  render: () => ({
+    components: { ControlledExpandedExample },
+    template: '<ControlledExpandedExample />',
+  }),
+}
+
+export const ControlledSelected = {
+  render: () => ({
+    components: { ControlledSelectedExample },
+    template: '<ControlledSelectedExample />',
+  }),
+}
+
+export const RootProvider = {
+  render: () => ({
+    components: { RootProviderExample },
+    template: '<RootProviderExample />',
   }),
 }
