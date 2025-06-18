212d766ebcab3ed7580aeb6dce6b05deabc45ec5
Sybren W <sybren.willemot@gmail.com>
2025-06-18 22:48:14 +0200

    feat(file-upload): add all examples to stories

--- packages/vue/src/components/file-upload/file-upload.stories.ts
+++ packages/vue/src/components/file-upload/file-upload.stories.ts
@@ -1,28 +1,48 @@
-import BasicExample from './examples/basic.vue'
-import WithFieldExample from './examples/with-field.vue'
-import RootProviderExample from './examples/root-provider.vue'
+import type { Meta } from '@storybook/vue3'
 
-export default {
+import BasicExample from './examples/basic.vue'
+import DirectoryUploadExample from './examples/directory-upload.vue'
+import RootProviderExample from './examples/root-provider.vue'
+import ValidationExample from './examples/validation.vue'
+import WithFieldExample from './examples/with-field.vue'
+
+const meta = {
   title: 'Components / File Upload',
-};
+} as Meta
+
+export default meta
 
 export const Basic = {
   render: () => ({
     components: { BasicExample },
     template: '<BasicExample />',
   }),
-};
+}
+
+export const DirectoryUpload = {
+  render: () => ({
+    components: { DirectoryUploadExample },
+    template: '<DirectoryUploadExample />',
+  }),
+}
+
+export const Validation = {
+  render: () => ({
+    components: { ValidationExample },
+    template: '<ValidationExample />',
+  }),
+}
 
 export const WithField = {
   render: () => ({
     components: { WithFieldExample },
     template: '<WithFieldExample />',
   }),
-};
+}
 
 export const RootProvider = {
   render: () => ({
     components: { RootProviderExample },
     template: '<RootProviderExample />',
   }),
-};
+}
