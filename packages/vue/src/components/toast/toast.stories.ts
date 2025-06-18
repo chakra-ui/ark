51a470c0942c14215bed6215c5f45dcd3741b0a6
Sybren W <sybren.willemot@gmail.com>
2025-06-18 22:36:45 +0200

    feat(toast): add all examples to stories

--- packages/vue/src/components/toast/toast.stories.ts
+++ packages/vue/src/components/toast/toast.stories.ts
@@ -1,6 +1,8 @@
 import type { Meta } from '@storybook/vue3'
 
+import ActionExample from './examples/action.vue'
 import BasicExample from './examples/basic.vue'
+import UpdateExample from './examples/update.vue'
 
 const meta = {
   title: 'Components / Toast',
@@ -12,5 +14,19 @@ export const Basic = {
   render: () => ({
     components: { BasicExample },
     template: '<BasicExample />',
+  }),
+}
+
+export const Action = {
+  render: () => ({
+    components: { ActionExample },
+    template: '<ActionExample />',
+  }),
+}
+
+export const Update = {
+  render: () => ({
+    components: { UpdateExample },
+    template: '<UpdateExample />',
   }),
 }
