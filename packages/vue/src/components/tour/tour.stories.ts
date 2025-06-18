a673fffea77d8d4a79dbbca81088d63f2e327e04
Sybren W <sybren.willemot@gmail.com>
2025-06-18 22:38:59 +0200

    feat(tour): add all examples to stories

--- packages/vue/src/components/tour/tour.stories.ts
+++ packages/vue/src/components/tour/tour.stories.ts
@@ -1,6 +1,7 @@
 import type { Meta } from '@storybook/vue3'
 
 import BasicExample from './examples/basic.vue'
+import TourExample from './examples/tour.vue'
 
 const meta = {
   title: 'Components / Tour',
@@ -12,5 +13,12 @@ export const Basic = {
   render: () => ({
     components: { BasicExample },
     template: '<BasicExample />',
+  }),
+}
+
+export const Tour = {
+  render: () => ({
+    components: { TourExample },
+    template: '<TourExample />',
   }),
 }
