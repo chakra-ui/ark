9b4202aa00fe755a951217691ba32941b539dbea
Sybren W <sybren.willemot@gmail.com>
2025-06-18 22:09:51 +0200

    feat(angle-slider): add storybook stories

---
+++ packages/vue/src/components/angle-slider/angle-slider.stories.ts
@@ -0,0 +1,32 @@
+import type { Meta } from '@storybook/vue3'
+
+import BasicExample from './examples/basic.vue'
+import ControlledExample from './examples/controlled.vue'
+import StepsExample from './examples/steps.vue'
+
+const meta = {
+  title: 'Components / AngleSlider',
+} as Meta
+
+export default meta
+
+export const Basic = {
+  render: () => ({
+    components: { BasicExample },
+    template: '<BasicExample />',
+  }),
+}
+
+export const Steps = {
+  render: () => ({
+    components: { StepsExample },
+    template: '<StepsExample />',
+  }),
+}
+
+export const Controlled = {
+  render: () => ({
+    components: { ControlledExample },
+    template: '<ControlledExample />',
+  }),
+}
