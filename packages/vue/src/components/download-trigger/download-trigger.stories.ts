7a60330fd7146b9c88dceb69eb222bd46cc4bc5b
Sybren W <sybren.willemot@gmail.com>
2025-06-18 22:16:02 +0200

    refactor(download-trigger): change to storybook stories

---
+++ packages/vue/src/components/download-trigger/download-trigger.stories.ts
@@ -0,0 +1,32 @@
+import type { Meta } from '@storybook/vue3'
+
+import BasicExample from './examples/basic.vue'
+import SvgExample from './examples/svg.vue'
+import WithPromiseExample from './examples/with-promise.vue'
+
+const meta = {
+  title: 'Components / DownloadTrigger',
+} as Meta
+
+export default meta
+
+export const Basic = {
+  render: () => ({
+    components: { BasicExample },
+    template: '<BasicExample />',
+  }),
+}
+
+export const Svg = {
+  render: () => ({
+    components: { SvgExample },
+    template: '<SvgExample />',
+  }),
+}
+
+export const WithPromise = {
+  render: () => ({
+    components: { WithPromiseExample },
+    template: '<WithPromiseExample />',
+  }),
+}
