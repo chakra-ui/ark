5e99bbd2f8b438e6900b158ebac51e2fb78ba0b0
Sybren W <sybren.willemot@gmail.com>
2025-06-18 22:41:05 +0200

    feat(locale): add all examples to stories

--- packages/vue/src/providers/locale/locale.stories.ts
+++ packages/vue/src/providers/locale/locale.stories.ts
@@ -1,6 +1,8 @@
 import type { Meta } from '@storybook/vue3'
 
 import BasicExample from './examples/basic.vue'
+import SetupExample from './examples/setup.vue'
+import UsageExample from './examples/usage.vue'
 
 const meta = {
   title: 'Providers / Locale',
@@ -12,5 +14,19 @@ export const Basic = {
   render: () => ({
     components: { BasicExample },
     template: '<BasicExample />',
+  }),
+}
+
+export const Setup = {
+  render: () => ({
+    components: { SetupExample },
+    template: '<SetupExample />',
+  }),
+}
+
+export const Usage = {
+  render: () => ({
+    components: { UsageExample },
+    template: '<UsageExample />',
   }),
 }
