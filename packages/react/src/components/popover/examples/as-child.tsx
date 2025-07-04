59ccbcff5d195bc85de324a2738ddd43791a8f7b
Segun Adebayo <joseshegs@gmail.com>
2025-07-04 16:41:18 +0100

    chore: rm ts-expect-error

--- packages/react/src/components/popover/examples/as-child.tsx
+++ packages/react/src/components/popover/examples/as-child.tsx
@@ -1,11 +1,9 @@
-// @ts-expect-error - Required for examples
-import { Button } from '@acme/ui-lib'
 import { Popover } from '@ark-ui/react/popover'
 
 export const AsChild = () => (
   <Popover.Root>
     <Popover.Trigger asChild>
-      <Button>Open</Button>
+      <button>Open</button>
     </Popover.Trigger>
   </Popover.Root>
 )
--- packages/svelte/src/lib/components/popover/examples/as-child.svelte
+++ packages/svelte/src/lib/components/popover/examples/as-child.svelte
@@ -1,13 +1,11 @@
 <script lang="ts">
-  // @ts-expect-error
-  import { Button } from '@acme/ui-lib'
   import { Popover } from '@ark-ui/svelte/popover'
 </script>
 
 <Popover.Root>
   <Popover.Trigger>
     {#snippet asChild(props)}
-      <Button {...props()}>Open</Button>
+      <button {...props()}>Open</button>
     {/snippet}
   </Popover.Trigger>
 </Popover.Root>
