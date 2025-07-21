702001f9c39393dea324722fc343b17b0acfd795
Segun Adebayo <joseshegs@gmail.com>
2025-07-21 14:36:06 +0100

    fix: check exports

--- packages/vue/src/components/json-tree-view/index.ts
+++ packages/vue/src/components/json-tree-view/index.ts
@@ -1,17 +1,9 @@
-export {
-  default as JsonTreeViewRoot,
-  type JsonTreeViewRootBaseProps,
-  type JsonTreeViewRootProps,
-} from './json-tree-view-root.vue'
+export { default as JsonTreeViewRoot, type JsonTreeViewRootProps } from './json-tree-view-root.vue'
 export {
   default as JsonTreeViewRootProvider,
   type JsonTreeViewRootProviderProps,
 } from './json-tree-view-root-provider.vue'
-export {
-  default as JsonTreeViewTree,
-  type JsonTreeViewTreeBaseProps,
-  type JsonTreeViewTreeProps,
-} from './json-tree-view-tree.vue'
+export { default as JsonTreeViewTree, type JsonTreeViewTreeProps } from './json-tree-view-tree.vue'
 export { useJsonTreeView, type UseJsonTreeViewProps, type UseJsonTreeViewReturn } from './use-json-tree-view'
 
 export * as JsonTreeView from './json-tree-view'
