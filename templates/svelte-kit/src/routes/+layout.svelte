e2b8a03d6fd3f703301b0170ee2b0d0d1ea257a0
Segun Adebayo <joseshegs@gmail.com>
2025-07-29 20:11:23 +0100

    chore: update sveltekit

---
+++ templates/svelte-kit/src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+  let { children } = $props()
+</script>
+
+<nav>
+  <a href="/">Home</a>
+  <a href="/select">Select</a>
+</nav>
+
+<div style="padding: 1rem;">
+  {@render children()}
+</div>
