7cc99e194d8d08ebc757a9c5d00c858b8517b5fc
Segun Adebayo <joseshegs@gmail.com>
2025-07-31 20:29:50 +0100

    refactor(solid): compose refs

--- packages/solid/src/utils/compose-refs.ts
+++ packages/solid/src/utils/compose-refs.ts
@@ -1,19 +1,24 @@
-import { createEffect, createSignal, on } from 'solid-js'
+import { createEffect } from 'solid-js'
 
 type PossibleRef<T> = T | ((el: T) => void) | undefined
 
 const isRefFn = <T>(ref: PossibleRef<T>): ref is (el: T | null) => void => typeof ref === 'function'
 
+const setRefs = <T>(refs: PossibleRef<T>[], node: T | null) => {
+  for (const ref of refs) {
+    if (isRefFn(ref)) {
+      ref(node)
+    }
+  }
+}
+
 export function composeRefs<T>(...refs: PossibleRef<T>[]) {
-  const [node, setNode] = createSignal<T | null>(null)
-  createEffect(
-    on(node, (el) => {
-      for (const ref of refs) {
-        if (isRefFn(ref)) {
-          ref(el)
-        }
-      }
-    }),
-  )
-  return setNode
+  let node: T | null = null
+  createEffect(() => {
+    setRefs(refs, node)
+  })
+  return (el: T) => {
+    node = el
+    setRefs(refs, el)
+  }
 }
