7600341135a00d8f2d39b40ee5788bd7e12d88c2
Segun Adebayo <joseshegs@gmail.com>
2025-08-16 20:18:18 +0100

    chore: update style

--- packages/react/src/components/scroll-area/examples/nested.tsx
+++ packages/react/src/components/scroll-area/examples/nested.tsx
@@ -1,18 +1,17 @@
 import { ScrollArea } from '@ark-ui/react/scroll-area'
 
 export const Nested = () => (
-  <ScrollArea.Root>
-    <ScrollArea.Viewport>
-      <ScrollArea.Content>
+  <ScrollArea.Root style={{ height: '12rem', width: '50%' }}>
+    <ScrollArea.Viewport style={{ height: '100%' }}>
+      <ScrollArea.Content style={{ padding: '1rem 1rem 1.5rem' }}>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore
           magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
           consequat.
         </p>
-
-        <ScrollArea.Root>
-          <ScrollArea.Viewport>
-            <ScrollArea.Content>
+        <ScrollArea.Root style={{ height: '8rem' }}>
+          <ScrollArea.Viewport style={{ height: '100%' }}>
+            <ScrollArea.Content style={{ padding: '1rem 1rem 1.5rem' }}>
               <p>
                 This is a nested scroll area. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
                 dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
@@ -27,13 +26,6 @@ export const Nested = () => (
           </ScrollArea.Scrollbar>
           <ScrollArea.Corner />
         </ScrollArea.Root>
-
-        <p>
-          Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni
-          dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit
-          amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam
-          aliquam quaerat voluptatem.
-        </p>
       </ScrollArea.Content>
     </ScrollArea.Viewport>
     <ScrollArea.Scrollbar>
