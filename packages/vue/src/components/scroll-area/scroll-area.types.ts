fb7f1821cd89fe732a9674b72ef5f70d36b1355c
Segun Adebayo <joseshegs@gmail.com>
2025-08-16 19:36:30 +0100

    fix: build

--- packages/vue/src/components/scroll-area/scroll-area.types.ts
+++ packages/vue/src/components/scroll-area/scroll-area.types.ts
@@ -1,4 +1,12 @@
-import type { UseScrollAreaProps } from './use-scroll-area'
+export interface RootProps {
+  /**
+   * The unique identifier of the machine.
+   */
+  id?: string
+  /**
+   * The ids of the scroll area elements
+   */
+  ids?: Partial<{ root: string; viewport: string; content: string; scrollbar: string; thumb: string }>
+}
 
-export interface RootProps extends UseScrollAreaProps {}
 export type RootEmits = {}
