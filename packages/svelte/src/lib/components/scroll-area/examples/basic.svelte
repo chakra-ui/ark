5f7f78739c9855c0a9c763ad26ebf11afd518316
Segun Adebayo <joseshegs@gmail.com>
2026-01-11 14:55:32 +0200

    refactor: scroll-area

--- packages/svelte/src/lib/components/scroll-area/examples/basic.svelte
+++ packages/svelte/src/lib/components/scroll-area/examples/basic.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
   import { ScrollArea } from '@ark-ui/svelte/scroll-area'
+  import styles from 'styles/scroll-area.module.css'
 </script>
 
-<ScrollArea.Root style="height: 8.5rem;">
-  <ScrollArea.Viewport style="height: 100%;">
-    <ScrollArea.Content style="padding: 1rem 1rem 1.5rem;">
-      <p>
+<ScrollArea.Root class={styles.Root}>
+  <ScrollArea.Viewport class={styles.Viewport}>
+    <ScrollArea.Content class={styles.Content}>
+      <p class={styles.Paragraph}>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore
         magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
         consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
@@ -19,8 +20,8 @@
       </p>
     </ScrollArea.Content>
   </ScrollArea.Viewport>
-  <ScrollArea.Scrollbar>
-    <ScrollArea.Thumb />
+  <ScrollArea.Scrollbar class={styles.Scrollbar}>
+    <ScrollArea.Thumb class={styles.Thumb} />
   </ScrollArea.Scrollbar>
-  <ScrollArea.Corner />
+  <ScrollArea.Corner class={styles.Corner} />
 </ScrollArea.Root>
