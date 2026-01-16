ac3a4e9d02afe1291c527f65ad09da1540f82d1b
Segun Adebayo <joseshegs@gmail.com>
2026-01-16 17:23:45 +0200

    refactor: presence

--- packages/solid/src/components/presence/examples/skip-animation-on-mount.tsx
+++ packages/solid/src/components/presence/examples/skip-animation-on-mount.tsx
@@ -1,17 +1,18 @@
 import { Presence } from '@ark-ui/solid/presence'
 import { createSignal } from 'solid-js'
+import button from 'styles/button.module.css'
+import styles from 'styles/presence.module.css'
 
 export const SkipAnimationOnMount = () => {
   const [present, setPresent] = createSignal(true)
-
   return (
-    <>
-      <button type="button" onClick={() => setPresent(!present)}>
+    <div class="stack">
+      <button class={button.Root} type="button" onClick={() => setPresent(!present())}>
         Toggle
       </button>
-      <Presence present={present()} skipAnimationOnMount>
-        Content
+      <Presence class={styles.Box} present={present()} skipAnimationOnMount>
+        No Initial Animation
       </Presence>
-    </>
+    </div>
   )
 }
