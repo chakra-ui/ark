a7d614e9a82d3c58d98ccf3f3d7e29f4c2d1b357
Segun Adebayo <joseshegs@gmail.com>
2026-01-17 00:54:17 +0200

    fix: test and typecheck

---
+++ packages/vue/src/components/editable/tests/editable-with-field.test.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { Editable } from '../..'
+import { Field } from '../../field'
+import type { RootProps } from '../../field/field.types'
+
+defineProps<RootProps>()
+</script>
+
+<template>
+  <Field.Root :disabled="disabled" :invalid="invalid" :readOnly="readOnly" :required="required">
+    <Editable.Root placeholder="Placeholder" activationMode="dblclick">
+      <Editable.Label>Label</Editable.Label>
+      <Editable.Area>
+        <Editable.Input />
+        <Editable.Preview />
+      </Editable.Area>
+    </Editable.Root>
+    <Field.HelperText>Additional Info</Field.HelperText>
+    <Field.ErrorText>Error Info</Field.ErrorText>
+  </Field.Root>
+</template>
