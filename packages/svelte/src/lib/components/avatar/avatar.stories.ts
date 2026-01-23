e8238120cb5486c29e6c6b4671694a3831c0daa1
Segun Adebayo <joseshegs@gmail.com>
2026-01-23 18:53:00 +0200

    fix: typecheck

--- packages/svelte/src/lib/components/avatar/avatar.stories.ts
+++ packages/svelte/src/lib/components/avatar/avatar.stories.ts
@@ -1,6 +1,5 @@
 import type { Meta } from '@storybook/svelte'
 import BasicExample from './examples/basic.svelte'
-import ClosedExample from './examples/closed.svelte'
 import ContextExample from './examples/context.svelte'
 import EventsExample from './examples/events.svelte'
 import RootProviderExample from './examples/root-provider.svelte'
@@ -14,17 +13,6 @@ export default meta
 export const Basic = {
   render: () => ({
     Component: BasicExample,
-  }),
-}
-
-export const Closed = {
-  name: 'Composed Avatar',
-  render: () => ({
-    Component: ClosedExample,
-    props: {
-      name: 'John Doe',
-      src: 'https://i.pravatar.cc/300?u=johndoe',
-    },
   }),
 }
 
--- packages/svelte/src/lib/components/popover/examples/factory.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-  import { ark } from '@ark-ui/svelte/factory'
-</script>
-
-<ark.span>
-  {#snippet asChild(props)}
-    <a href="#" {...props()}>Ark UI</a>
-  {/snippet}
-</ark.span>
