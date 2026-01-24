c4c037e56fa2497e62489f11d9daff5e3dcae1e8
Segun Adebayo <joseshegs@gmail.com>
2026-01-24 19:32:25 +0200

    fix: anatomy

--- website/src/components/anatomy.tsx
+++ website/src/components/anatomy.tsx
@@ -2,8 +2,9 @@ import { allComponents as Anatomies, createGradient } from '@zag-js/anatomy-icon
 import { css } from 'styled-system/css'
 import { Box } from 'styled-system/jsx'
 
-export const Anatomy = ({ id }: { id: keyof typeof Anatomies }) => {
-  const Anatomy = Anatomies[id]
+export const Anatomy = ({ id }: { id: string }) => {
+  const Anatomy = Anatomies[id as keyof typeof Anatomies]
+  if (!Anatomy) return null
   return (
     <Box my="8" style={{ background: createGradient('red').value }} borderRadius="lg">
       <Anatomy
