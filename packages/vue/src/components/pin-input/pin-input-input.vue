ccf925ccde2006d657bad255e09514277b06f871
Hwacc <mshcccch@gmail.com>
2026-03-10 02:41:09 +0800

    feat(pin-input): enhance input handling by omitting unnecessary props and adding custom input event logic
    (cherry picked from commit 80192b5d3575ef274997b1a3a704fcb278f9fb25)

--- packages/vue/src/components/pin-input/pin-input-input.vue
+++ packages/vue/src/components/pin-input/pin-input-input.vue
@@ -2,6 +2,7 @@
 import type { InputProps } from '@zag-js/pin-input'
 import type { InputHTMLAttributes } from 'vue'
 import type { PolymorphicProps } from '../factory'
+import { omit } from '@zag-js/utils'
 
 export interface PinInputInputBaseProps extends InputProps, PolymorphicProps {}
 export interface PinInputInputProps
@@ -17,13 +18,29 @@ export interface PinInputInputProps
 import { ark } from '../factory'
 import { usePinInputContext } from './use-pin-input-context'
 import { useForwardExpose } from '../../utils/use-forward-expose'
+import { computed } from 'vue'
 
 const props = defineProps<PinInputInputProps>()
 const pinInput = usePinInputContext()
+
+const inputProps = computed(() => pinInput.value.getInputProps(props))
+let lastValue = ''
+
+function onInput(e: Event) {
+  const v = (e.target as HTMLInputElement).value
+
+  if (v === lastValue) return
+
+  lastValue = v
+  inputProps.value.onInput?.(e)
+}
+const omited = omit(inputProps.value, ['onChange', 'onInput'])
 
 useForwardExpose()
 </script>
 
 <template>
-  <ark.input v-bind="pinInput.getInputProps(props)" :as-child="asChild"><slot /></ark.input>
+  <ark.input v-bind="omited" @input="onInput" :as-child="asChild">
+    <slot />
+  </ark.input>
 </template>
