0b298a8bf4aec1da0e80ea355b2d322ba5f93ad5
Hwacc <mshcccch@gmail.com>
2026-03-10 15:33:35 +0800

    fix(pin-input): synchronize input value with machine state and enhance input handling

--- packages/vue/src/components/pin-input/pin-input-input.vue
+++ packages/vue/src/components/pin-input/pin-input-input.vue
@@ -18,22 +18,32 @@ export interface PinInputInputProps
 import { ark } from '../factory'
 import { usePinInputContext } from './use-pin-input-context'
 import { useForwardExpose } from '../../utils/use-forward-expose'
-import { computed } from 'vue'
+import { computed, watch } from 'vue'
 
 const props = defineProps<PinInputInputProps>()
 const pinInput = usePinInputContext()
 
+// ts-plugin(2590)
 const inputProps = computed(() => pinInput.value.getInputProps(props))
+
 let lastValue = ''
 function onInput(e: InputEvent) {
-  const target = e.target as HTMLInputElement
-  const nextValue = target.value
+  const nextValue = (e.target as HTMLInputElement)?.value ?? ''
 
   if (nextValue === lastValue) return
 
   lastValue = nextValue
   inputProps.value.onInput?.(e)
 }
+
+// watch current index input's value in machine state, then sync it to lastValue
+watch(
+  () => pinInput.value.value[props.index],
+  (newVal) => {
+    lastValue = newVal
+  },
+)
+
 const omited = computed(() => omit(inputProps.value, ['onInput']))
 
 useForwardExpose()
