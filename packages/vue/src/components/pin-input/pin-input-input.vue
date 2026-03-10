f0b6ccbf2522ad84b311e910a80d1a5809b5ab01
Hwacc <mshcccch@gmail.com>
2026-03-10 16:03:14 +0800

    fix(pin-input): refine input synchronization and enhance type handling for input properties

--- packages/vue/src/components/pin-input/pin-input-input.vue
+++ packages/vue/src/components/pin-input/pin-input-input.vue
@@ -12,6 +12,8 @@ export interface PinInputInputProps
      * @vue-ignore
      */
     InputHTMLAttributes {}
+
+type InputPropsResult = Record<string, unknown> & { onInput?: (e: InputEvent) => void }
 </script>
 
 <script setup lang="ts">
@@ -23,26 +25,28 @@ import { computed, watch } from 'vue'
 const props = defineProps<PinInputInputProps>()
 const pinInput = usePinInputContext()
 
-// ts-plugin(2590)
-const inputProps = computed(() => pinInput.value.getInputProps(props))
+// we use these splits to break the ts type interface chain to avoid ts-plugin(2590)
+const inputProps = computed(() => {
+  const result = pinInput.value.getInputProps({ index: props.index })
+  return result as InputPropsResult
+})
+const currentValue = computed<string | undefined>(() => {
+  const api = pinInput.value as { value: string[] }
+  const idx = props.index as number
+  return api.value[idx]
+})
 
 let lastValue = ''
+// watch current index input's value in machine state, then sync it to lastValue
+watch(currentValue, (newVal) => {
+  lastValue = newVal ?? ''
+})
 function onInput(e: InputEvent) {
   const nextValue = (e.target as HTMLInputElement)?.value ?? ''
-
   if (nextValue === lastValue) return
-
   lastValue = nextValue
   inputProps.value.onInput?.(e)
 }
-
-// watch current index input's value in machine state, then sync it to lastValue
-watch(
-  () => pinInput.value.value[props.index],
-  (newVal) => {
-    lastValue = newVal
-  },
-)
 
 const omited = computed(() => omit(inputProps.value, ['onInput']))
 
