c806cc3777613b7eee9f09a1bd35cc91c3303684
Hwacc <mshcccch@gmail.com>
2026-03-10 10:23:13 +0800

    fix(pin-input): enhance input handling by implementing custom onInput logic and omitting unnecessary props
    (cherry picked from commit 5ef32ea0ad2a664441efb9e8d022799d76986ad7)

--- packages/solid/src/components/pin-input/pin-input-input.tsx
+++ packages/solid/src/components/pin-input/pin-input-input.tsx
@@ -3,6 +3,7 @@ import { mergeProps } from '@zag-js/solid'
 import { createSplitProps } from '../../utils/create-split-props'
 import { type HTMLProps, type PolymorphicProps, ark } from '../factory'
 import { usePinInputContext } from './use-pin-input-context'
+import { omit } from '@zag-js/utils'
 
 export interface PinInputInputBaseProps extends InputProps, PolymorphicProps<'input'> {}
 export interface PinInputInputProps extends HTMLProps<'input'>, PinInputInputBaseProps {}
@@ -12,5 +13,13 @@ export const PinInputInput = (props: PinInputInputProps) => {
   const api = usePinInputContext()
   const mergedProps = mergeProps(() => api().getInputProps(inputProps), localProps)
 
-  return <ark.input {...mergedProps} />
+  let lastValue = ''
+  function onInput(e: InputEvent & { currentTarget: HTMLInputElement; target: HTMLInputElement }) {
+    const nextValue = e.target.value
+    if (nextValue === lastValue) return
+    lastValue = nextValue
+    ;(mergedProps.onInput as (e: InputEvent) => void)?.(e)
+  }
+
+  return <ark.input {...omit(mergedProps, ['onInput'])} onInput={onInput} />
 }
