2cadc930b2b9e14bc05afe5e6f53cf1c5feb2011
Kevin Dev 🇷🇼 <kevinkalisa84@gmail.com>
2026-04-13 15:31:41 +0200

    fix: lint and test errors

--- packages/solid/src/components/toc/toc-indicator.tsx
+++ packages/solid/src/components/toc/toc-indicator.tsx
@@ -2,11 +2,11 @@ import { mergeProps } from '@zag-js/solid'
 import { type HTMLProps, type PolymorphicProps, ark } from '../factory'
 import { useTocContext } from './use-toc-context'
 
-export interface TocIndicatorBaseProps extends PolymorphicProps {}
+export interface TocIndicatorBaseProps extends PolymorphicProps<'div'> {}
 export interface TocIndicatorProps extends HTMLProps<'div'>, TocIndicatorBaseProps {}
 
 export const TocIndicator = (props: TocIndicatorProps) => {
   const toc = useTocContext()
-  const mergedProps = mergeProps(() => toc.getIndicatorProps(), props)
+  const mergedProps = mergeProps(() => toc.getRootProps(), props)
   return <ark.div {...mergedProps} />
 }
