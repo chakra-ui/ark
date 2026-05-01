9b33696fc58f42b18daef0440f02c4932ca2530f
Segun Adebayo <joseshegs@gmail.com>
2026-05-02 00:30:24 +0200

    fix: style hmr

--- packages/vue/.storybook/main.ts
+++ packages/vue/.storybook/main.ts
@@ -1,8 +1,10 @@
-import { dirname, resolve } from 'node:path'
+import { createHash } from 'node:crypto'
+import { dirname, relative, resolve } from 'node:path'
 import { fileURLToPath } from 'node:url'
 import type { StorybookConfig } from '@storybook/vue3-vite'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
+const stylesDir = resolve(__dirname, '../../../.storybook/modules')
 
 const config: StorybookConfig = {
   framework: {
@@ -20,7 +22,15 @@ const config: StorybookConfig = {
     config.resolve ??= {}
     config.resolve.alias ??= {}
     // @ts-expect-error - alias type mismatch
-    config.resolve.alias['styles'] = resolve(__dirname, '../../../.storybook/modules')
+    config.resolve.alias['styles'] = stylesDir
+    config.css ??= {}
+    config.css.modules = {
+      ...config.css.modules,
+      generateScopedName: (name, filename) => {
+        const hash = createHash('md5').update(relative(stylesDir, filename)).digest('hex').slice(0, 5)
+        return `_${name}_${hash}`
+      },
+    }
     return config
   },
 }
