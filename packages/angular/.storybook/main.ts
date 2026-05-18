26ecd13b17eebbbab8cea57284c47af3fb9459f4
Ryan Mahoney <ryan.mahoney@outlook.com>
2026-05-18 07:10:01 -0400

    chore(angular): include progress and toggle in Storybook story glob (#8)
    The .storybook main config hardcoded each component package's story glob.
    Add progress/**/*.stories.ts and toggle/**/*.stories.ts so the new
    component stories appear alongside Avatar.

--- packages/angular/.storybook/main.ts
+++ packages/angular/.storybook/main.ts
@@ -1,7 +1,12 @@
 import type { StorybookConfig } from '@storybook/angular'
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.stories.ts', '../avatar/**/*.stories.ts'],
+  stories: [
+    '../src/**/*.stories.ts',
+    '../avatar/**/*.stories.ts',
+    '../progress/**/*.stories.ts',
+    '../toggle/**/*.stories.ts',
+  ],
   addons: ['@storybook/addon-a11y'],
   framework: {
     name: '@storybook/angular',
