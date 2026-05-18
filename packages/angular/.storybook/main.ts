b93dd9e9166f7280630194d6cce3a4b0b977c2e8
Ryan Mahoney <ryan.mahoney@outlook.com>
2026-05-17 22:23:43 -0400

    feat(angular): add avatar storybook stories (#5)

--- packages/angular/.storybook/main.ts
+++ packages/angular/.storybook/main.ts
@@ -1,7 +1,7 @@
 import type { StorybookConfig } from '@storybook/angular'
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.stories.ts'],
+  stories: ['../src/**/*.stories.ts', '../avatar/**/*.stories.ts'],
   addons: ['@storybook/addon-a11y'],
   framework: {
     name: '@storybook/angular',
