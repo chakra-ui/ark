b33b5c5ba4fde10d66990865fb10331fba6f99e6
Ryan Mahoney <ryan.mahoney@outlook.com>
2026-05-18 07:29:24 -0400

    refactor(angular): share toggle example icons (#8)

--- packages/angular/toggle/examples/context.ts
+++ packages/angular/toggle/examples/context.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core'
 import { ArkToggleRoot, injectArkToggleContext } from '@ark-ui/angular/toggle'
 import { toggleExampleStyles } from '../toggle-example-styles'
+import { ToggleBoldIcon } from './icons'
 
 @Component({
   selector: 'toggle-context-label',
@@ -16,21 +17,10 @@ export class ToggleContextLabel {
   selector: 'toggle-context-example',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [ArkToggleRoot, ToggleContextLabel],
+  imports: [ArkToggleRoot, ToggleBoldIcon, ToggleContextLabel],
   template: `
     <button arkToggle>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        aria-hidden="true"
-      >
-        <path d="M6 12h9a4 4 0 0 1 0 8H7a1 1 0 0 1-1-1V5a1 1 0 0 1 1-1h7a4 4 0 0 1 0 8H6" />
-      </svg>
+      <toggle-bold-icon />
       <toggle-context-label />
     </button>
   `,
--- packages/angular/toggle/examples/indicator.ts
+++ packages/angular/toggle/examples/indicator.ts
@@ -1,46 +1,17 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core'
 import { ArkToggleIndicator, ArkToggleRoot } from '@ark-ui/angular/toggle'
 import { toggleExampleStyles } from '../toggle-example-styles'
+import { ToggleHeartIcon } from './icons'
 
 @Component({
   selector: 'toggle-indicator-example',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [ArkToggleRoot, ArkToggleIndicator],
+  imports: [ArkToggleRoot, ArkToggleIndicator, ToggleHeartIcon],
   template: `
     <button arkToggle #toggle="arkToggle">
       <span arkToggleIndicator>
-        @if (toggle.api().pressed) {
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            aria-hidden="true"
-          >
-            <path
-              d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.29 1.51 4.04 3 5.5l7 7Z"
-            />
-          </svg>
-        } @else {
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            aria-hidden="true"
-          >
-            <path
-              d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.29 1.51 4.04 3 5.5l7 7Z"
-            />
-          </svg>
-        }
+        <toggle-heart-icon [filled]="toggle.api().pressed" />
       </span>
     </button>
   `,
