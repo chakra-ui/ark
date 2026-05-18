a27604949ceac00cce3d0d895177bc3aaa9bb4b2
Ryan Mahoney <ryan.mahoney@outlook.com>
2026-05-17 21:32:42 -0400

    feat(angular): add avatar anatomy and public type aliases (#5)

---
+++ packages/angular/avatar/avatar.types.ts
@@ -0,0 +1,7 @@
+export type {
+  Api as AvatarApi,
+  ElementIds as AvatarElementIds,
+  LoadStatus as AvatarLoadStatus,
+  Props as AvatarMachineProps,
+  StatusChangeDetails as AvatarStatusChangeDetails,
+} from '@zag-js/avatar'
--- packages/angular/avatar/public-api.ts
+++ packages/angular/avatar/public-api.ts
@@ -1,1 +1,8 @@
-export {}
+export { avatarAnatomy } from './avatar.anatomy'
+export type {
+  AvatarApi,
+  AvatarElementIds,
+  AvatarLoadStatus,
+  AvatarMachineProps,
+  AvatarStatusChangeDetails,
+} from './avatar.types'
