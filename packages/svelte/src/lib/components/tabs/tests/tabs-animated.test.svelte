38a4d4d5e4a3ddd96b6793a594c9c073f8bcd5be
Aaron George <a.george@easygo.io>
2026-09-15 17:45:46 +1000

    fix(svelte): merge the presence props into the tabs content (#4043)
    Co-authored-by: Lope <adebesintolulope80@gmail.com>

---
+++ packages/svelte/src/lib/components/tabs/tests/tabs-animated.test.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { Tabs } from '@ark-ui/svelte/tabs'
+
+  const animated = 'animation-name: exit; animation-duration: 200ms'
+</script>
+
+<Tabs.Root defaultValue="account">
+  <Tabs.List>
+    <Tabs.Trigger value="account">Account</Tabs.Trigger>
+    <Tabs.Trigger value="password">Password</Tabs.Trigger>
+  </Tabs.List>
+  <Tabs.Content value="account" style={animated}>Account panel</Tabs.Content>
+  <Tabs.Content value="password" style={animated}>Password panel</Tabs.Content>
+</Tabs.Root>
