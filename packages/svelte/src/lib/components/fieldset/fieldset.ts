debbdd330f6734761b37a25eb877120bcde9cc8c
Aaron George <a.george@easygo.io>
2026-09-16 15:57:45 +1000

    fix(svelte): alias the fieldset namespace's prop types

--- packages/svelte/src/lib/components/fieldset/fieldset.ts
+++ packages/svelte/src/lib/components/fieldset/fieldset.ts
@@ -1,6 +1,26 @@
-export { default as Root, type FieldsetRootProps } from './fieldset-root.svelte'
-export { default as RootProvider, type FieldsetRootProviderProps } from './fieldset-root-provider.svelte'
-export { default as Context, type FieldsetContextProps } from './fieldset-context.svelte'
-export { default as Legend, type FieldsetLegendProps } from './fieldset-legend.svelte'
-export { default as HelperText, type FieldsetHelperTextProps } from './fieldset-helper-text.svelte'
-export { default as ErrorText, type FieldsetErrorTextProps } from './fieldset-error-text.svelte'
+export {
+  default as Root,
+  type FieldsetRootProps as RootProps,
+  type FieldsetRootBaseProps as RootBaseProps,
+} from './fieldset-root.svelte'
+export {
+  default as RootProvider,
+  type FieldsetRootProviderProps as RootProviderProps,
+  type FieldsetRootProviderBaseProps as RootProviderBaseProps,
+} from './fieldset-root-provider.svelte'
+export { default as Context, type FieldsetContextProps as ContextProps } from './fieldset-context.svelte'
+export {
+  default as Legend,
+  type FieldsetLegendProps as LegendProps,
+  type FieldsetLegendBaseProps as LegendBaseProps,
+} from './fieldset-legend.svelte'
+export {
+  default as HelperText,
+  type FieldsetHelperTextProps as HelperTextProps,
+  type FieldsetHelperTextBaseProps as HelperTextBaseProps,
+} from './fieldset-helper-text.svelte'
+export {
+  default as ErrorText,
+  type FieldsetErrorTextProps as ErrorTextProps,
+  type FieldsetErrorTextBaseProps as ErrorTextBaseProps,
+} from './fieldset-error-text.svelte'
