bf557a29a477155b6f1ea53d6a998914e9f11112
Adebesin Tolulope <adebesintolulope80@gmail.com>
2026-09-17 19:56:57 +0100

    feat(website): dynamic per-page OpenGraph images

--- website/src/app/blog/[slug]/page.tsx
+++ website/src/app/blog/[slug]/page.tsx
@@ -9,6 +9,7 @@ import { Navbar } from '~/components/marketing/navbar'
 import { Heading } from '~/components/ui/heading'
 import { Text } from '~/components/ui/text'
 import { getPublicUrl } from '~/lib/get-public-url'
+import { ogImageUrl } from '~/lib/og-template'
 import { MDXContent } from '~/mdx-content'
 import { blogs } from '~/lib/source'
 
@@ -91,6 +92,7 @@ export async function generateMetadata({ params }: { params: Promise<{ slug: str
   const blog = blogs.find((blog) => blog.slug === slug)
   if (!blog) return {}
 
+  const image = ogImageUrl({ title: blog.title, description: blog.description, category: 'Blog' })
   return {
     title: blog.title,
     description: blog.description,
@@ -99,17 +101,17 @@ export async function generateMetadata({ params }: { params: Promise<{ slug: str
       url: `/blog/${slug}`,
       title: blog.title,
       description: blog.description,
-      images: '/og-image.png',
+      images: [image],
       type: 'article',
       publishedTime: blog.date,
-      authors: [blog.author],
+      authors: blog.author ? [blog.author].flat() : undefined,
       tags: blog.tags,
     },
     twitter: {
       card: 'summary_large_image',
       title: blog.title,
       description: blog.description,
-      images: '/og-image.png',
+      images: [image],
     },
   }
 }
